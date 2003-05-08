8dbbbdc053b0359cdbd13bb834bb85ea6d2cf141
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2003-05-08 13:41:16 +0000

    *** empty log message ***

---
+++ trunk/mevenide-core/src/test/java/org/mevenide/core/pom/AllTests.java
@@ -0,0 +1,34 @@
+/* 
+ * Copyright (C) 2003  Gilles Dodinet (gdodinet@wanadoo.fr)
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ */
+package org.mevenide.core.pom;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * 
+ * @author Gilles Dodinet (gdodinet@wanadoo.fr)
+ * @version $Id: AllTests.java 8 mai 2003 15:32:4913:34:35 Exp gdodinet 
+ * 
+ */
+public class AllTests  {
+    public static Test suite() {
+        TestSuite suite = new TestSuite();
+        
+        suite.addTestSuite(PomHelperTest.class);
+        
+        return suite;
+    }
+}
