5f4c0117bdc77884086358aae4388009f54e9bbb
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2003-05-08 14:05:00 +0000

    *** empty log message ***

--- mevenide-core/src/java/org/mevenide/core/IGoalsManager.java
+++ mevenide-core/src/java/org/mevenide/core/IGoalsManager.java
@@ -23,39 +23,41 @@ import java.io.IOException;
  * 
  */
 public interface IGoalsManager {
-	/**
+	
+    /**
 	 * add the plugin:goal to the list of runnable goals
 	 * if goal is null just add plugin
 	 * 
 	 * @param category
 	 * @param goal 
 	 */
-	public abstract void addGoal(String plugin, String goal)
-		throws GoalNotFoundException;
-	/**
+	public abstract void addGoal(String plugin, String goal) throws GoalNotFoundException;
+	
+    /**
 	 * remove plugin:goal from the list of runnable goals
 	 * if goal is null remove plugin
 	 * @param category
 	 * @param goals   
 	 */
 	public abstract void removeGoal(String plugin, String goal);
-	/**
+	
+    /**
 	 * @return String[] the list of goals to be run
 	 */
 	public abstract String[] getGoalsToRun();
-	/**
-	 * @todo IMPLEMENTME
-	 * 
+	
+    /**
 	 * @param plugin
 	 * @return String[] the selected goals under <code>plugin</code> control 
 	 */
 	public abstract String[] getGoals(String plugin);
-	/**
+	
+    /**
 	 * run the runnableGoals
-	 * @todo FUNCTIONAL add options
 	 */
 	public abstract void runGoals();
-	/**
+	
+    /**
 	 * @throws IOException
 	 */
 	public abstract void save() throws IOException;
