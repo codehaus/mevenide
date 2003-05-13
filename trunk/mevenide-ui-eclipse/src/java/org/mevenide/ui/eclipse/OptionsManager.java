2f707e7d2a04754c1caddc69c92a9ec96e0cc020
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2003-05-13 10:08:02 +0000

    *** empty log message ***

--- trunk/mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/OptionsManager.java
+++ trunk/mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/OptionsManager.java
@@ -1,19 +1,27 @@
-/*
- * Created on 11 mai 2003
+/* 
+ * Copyright (C) 2003  Gilles Dodinet (gdodinet@wanadoo.fr)
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
  */
 package org.mevenide.ui.eclipse;
 
 import org.mevenide.core.AbstractOptionsManager;
 
 /**
- * @author gdodinet
- *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
+ * Created on 01 feb. 03	
+ * 
+ * @author Gilles Dodinet (gdodinet@wanadoo.fr)
+ * @version $Id$
+ */ 
 public class OptionsManager extends AbstractOptionsManager {
 
 }
