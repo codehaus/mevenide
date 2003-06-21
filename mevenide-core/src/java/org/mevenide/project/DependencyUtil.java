71edc91114e4fcf9f8f44edd1d83cd4a73a89319
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2003-06-21 13:54:50 +0000

    *** empty log message ***

---
+++ mevenide-core/src/java/org/mevenide/project/DependencyUtil.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2003  Gilles Dodinet (gdodinet@wanadoo.fr)
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+package org.mevenide.project;
+
+import org.apache.maven.project.Dependency;
+
+/**
+ * 
+ * @author Gilles Dodinet (gdodinet@wanadoo.fr)
+ * @version $Id$
+ * 
+ */
+public class DependencyUtil {
+	public static boolean areEquals(Dependency d1, Dependency d2) {
+		return  (d1 == null && d2 == null) ||
+				(   
+				    d1 != null && d2 != null
+				    && areEquals(d1.getArtifactId(), d2.getArtifactId())
+					&& areEquals(d1.getGroupId(), d2.getGroupId())
+					&& areEquals(d1.getVersion(), d2.getVersion())
+				);
+	}
+	
+	private static boolean areEquals(String s1, String s2) {
+		return  (s1 == null && s2 == null) ||
+				(
+				    s1 != null && s1.equals(s2)
+				);
+	}
+}
