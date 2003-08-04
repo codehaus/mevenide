516d209d8d29ad15270132799b05bec0353dad5e
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2003-08-04 15:36:51 +0000

    *** empty log message ***

--- mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/launch/configuration/MavenRunnerTabGroup.java
+++ mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/launch/configuration/MavenRunnerTabGroup.java
@@ -4,25 +4,27 @@ import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.debug.ui.launchVariables.RefreshTab;
 
 public class MavenRunnerTabGroup extends AbstractLaunchConfigurationTabGroup {
 
-	private MavenOptionsTab tab = new MavenOptionsTab();
+	private MavenOptionsTab optionsTab = new MavenOptionsTab();
 	private MavenMainTab mainTab = new MavenMainTab();
-	
+	private RefreshTab refreshTab = new RefreshTab();
 	/**
 	 * @see org.eclipse.debug.ui.ILaunchConfigurationTabGroup#createTabs(org.eclipse.debug.ui.ILaunchConfigurationDialog, java.lang.String)
 	 */
 	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
 		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {
+			optionsTab,
 			mainTab,
-			tab,
+			refreshTab,
 		};
 		setTabs(tabs);
 	}
 
 
 	public void performApply(ILaunchConfigurationWorkingCopy configuration) {
-		tab.performApply(configuration);
+		optionsTab.performApply(configuration);
 	}
 }
