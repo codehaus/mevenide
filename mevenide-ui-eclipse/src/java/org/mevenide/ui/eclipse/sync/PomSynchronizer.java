fba876f74d7e258a7f978a17ca0923daf615faee
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2003-09-01 23:19:56 +0000

    extracted pom update operations into PomUdapter

--- mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/sync/PomSynchronizer.java
+++ mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/sync/PomSynchronizer.java
@@ -48,36 +48,18 @@
  */
 package org.mevenide.ui.eclipse.sync;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.maven.project.Dependency;
-import org.apache.maven.project.Project;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.jface.preference.PreferenceStore;
-import org.mevenide.ProjectConstants;
-import org.mevenide.project.dependency.DependencyUtil;
-import org.mevenide.project.io.ProjectReader;
-import org.mevenide.project.io.ProjectWriter;
 import org.mevenide.sync.AbstractPomSynchronizer;
 import org.mevenide.sync.ISynchronizer;
-import org.mevenide.ui.eclipse.DefaultPathResolver;
-import org.mevenide.ui.eclipse.IPathResolver;
 import org.mevenide.ui.eclipse.Mevenide;
 import org.mevenide.ui.eclipse.sync.model.dependency.DependencyGroup;
 import org.mevenide.ui.eclipse.sync.model.dependency.DependencyGroupMarshaller;
-import org.mevenide.ui.eclipse.sync.model.dependency.DependencyWrapper;
-import org.mevenide.ui.eclipse.sync.model.source.SourceDirectory;
 import org.mevenide.ui.eclipse.sync.model.source.SourceDirectoryGroup;
 import org.mevenide.ui.eclipse.sync.model.source.SourceDirectoryGroupMarshaller;
 import org.mevenide.ui.eclipse.util.FileUtil;
-import org.mevenide.util.MevenideUtil;
 
 /**
  * 
@@ -94,15 +76,12 @@ public class PomSynchronizer extends AbstractPomSynchronizer implements ISynchro
     /** the POM under control */
 	private IFile pom;
 
-    /** helper instance */
-    private IPathResolver pathResolver;
-    
+   
 
     
     public void initialize() {
 		this.project = Mevenide.getPlugin().getProject();
 		this.pom = project.getFile("project.xml");
-		pathResolver = new DefaultPathResolver(); 
 	}
 
     /**
@@ -115,7 +94,7 @@ public class PomSynchronizer extends AbstractPomSynchronizer implements ISynchro
 				log.debug("About to update pom");
 				DependencyGroup dependencyGroup = DependencyGroupMarshaller.getDependencyGroup(project, Mevenide.getPlugin().getFile("statedDependencies.xml"));
 				SourceDirectoryGroup sourceGroup = SourceDirectoryGroupMarshaller.getSourceDirectoryGroup(project, Mevenide.getPlugin().getFile("sourceTypes.xml"), 0);
-				updatePom(sourceGroup, dependencyGroup, Mevenide.getPlugin().getPom());
+				new PomUpdater(project).updatePom(sourceGroup, dependencyGroup, Mevenide.getPlugin().getPom());
 			}
 			
 		}
@@ -151,126 +130,6 @@ public class PomSynchronizer extends AbstractPomSynchronizer implements ISynchro
 	}
 
 
-	/**
-	 * @todo refactor-me length of this method  is about 100 lines !
-	 * 
-	 */
-	public void updatePom(SourceDirectoryGroup sourceGroup, DependencyGroup dependencyGroup, File pomFile) throws Exception {
-		Mevenide.getPlugin().createProjectProperties();
-		
-		updateDeclaredSourceDirectories(sourceGroup, pomFile);
-		
-		updateDeclaredDependencies(dependencyGroup, pomFile);
-		
-		updateExtend(sourceGroup, dependencyGroup, pomFile);
-		
-		Mevenide.getPlugin().setBuildPath();
-		
-		updateParentPom(sourceGroup, dependencyGroup, pomFile);
-	}
-
-    private void updateParentPom(SourceDirectoryGroup sourceGroup, DependencyGroup dependencyGroup, File pomFile) throws Exception, FileNotFoundException, IOException {
-		PreferenceStore store = new PreferenceStore(Mevenide.getPlugin().getPreferencesFilename());
-		store.load();
-		
-		String resolvedExtend = MevenideUtil.resolve(ProjectReader.getReader().read(pomFile), store.getString("pom." + sourceGroup.getProjectName() + ".parent"));
-		if ( !new File(resolvedExtend).exists() ) {
-			resolvedExtend = new File(new File(project.getFile("project.xml").getLocation().toOSString()).getParentFile(), resolvedExtend).getAbsolutePath();
-		}			
-
-		if ( new File(resolvedExtend).exists() && new File(resolvedExtend).isFile() ) {
-			updateParentDependencies(dependencyGroup, resolvedExtend);
 	
-			updateParentSourceDirectories(sourceGroup, resolvedExtend);
-			//donot refresh parent pom since we're not sure if it is present in workspace
-			
-		}
-    }
-
-    private void updateParentSourceDirectories(SourceDirectoryGroup sourceGroup, String resolvedExtend) throws Exception {
-        List inheritedSourceDirectories = sourceGroup.getInheritedSourceDirectories();
-        for (int i = 0; i < inheritedSourceDirectories.size(); i++) {
-            SourceDirectory directory = (SourceDirectory) inheritedSourceDirectories.get(i);
-        	if ( directory.isSource() ) {
-        		ProjectWriter.getWriter().addSource(directory.getDirectoryPath(), new File(resolvedExtend), directory.getDirectoryType());
-        	}
-        	if ( directory.getDirectoryType().equals(ProjectConstants.MAVEN_RESOURCE ) ) {
-        		ProjectWriter.getWriter().addResource(directory.getDirectoryPath(), new File(resolvedExtend));
-        	}
-        	if ( directory.getDirectoryType().equals(ProjectConstants.MAVEN_TEST_RESOURCE ) ) {
-        		ProjectWriter.getWriter().addUnitTestResource(directory.getDirectoryPath(), new File(resolvedExtend));
-        	}   
-        }
-    }
-
-    private void updateParentDependencies(DependencyGroup dependencyGroup, String resolvedExtend) throws FileNotFoundException, Exception, IOException {
-        List inheritedDependencies = new ArrayList();
-        List inheritedDependencyWrappers = dependencyGroup.getInheritedDependencyWrappers();
-        for (int i = 0; i < inheritedDependencyWrappers.size(); i++) {
-        	DependencyWrapper wrapper = (DependencyWrapper) inheritedDependencyWrappers.get(i); 
-        	inheritedDependencies.add(wrapper.getDependency());   
-        }
-        if ( inheritedDependencies.size() > 0 ) {
-        	Project parentProject = ProjectReader.getReader().read(new File(resolvedExtend));
-        	List parentDependencies = parentProject.getDependencies();
-        	for (int i = 0; i < parentDependencies.size(); i++) {
-        		boolean shouldAddDependency = true;
-                for (int j = 0; j < inheritedDependencies.size(); j++) {
-        			if ( DependencyUtil.areEquals((Dependency) inheritedDependencies.get(j), (Dependency) parentDependencies.get(i)) ) {
-                        shouldAddDependency = false;
-        			}
-                }
-                if ( shouldAddDependency ) {
-        			inheritedDependencies.add(parentDependencies.get(i));
-                }
-            }
-        	ProjectWriter.getWriter().setDependencies(inheritedDependencies, new File(resolvedExtend));
-        }
-    }
-
-    private void updateExtend(SourceDirectoryGroup sourceGroup, DependencyGroup dependencyGroup, File pomFile) throws IOException, Exception {
-        PreferenceStore store = new PreferenceStore(Mevenide.getPlugin().getPreferencesFilename());
-		store.load();
-		
-		log.debug("isInherited = " + (dependencyGroup.isInherited()) + " ; parentPom = " + store.getString("pom." + dependencyGroup.getProjectName() + ".parent"));
-		ProjectWriter.getWriter().updateExtend(pomFile, dependencyGroup.isInherited(), store.getString("pom." + dependencyGroup.getProjectName() + ".parent"));
-        
-    }
-
-    private void updateDeclaredDependencies(DependencyGroup dependencyGoup, File pomFile) throws Exception {
-        //List dependencies = dependencyGoup.getDependencies();
-		List dependencies = dependencyGoup.getNonInheritedDependencyWrappers();
-		log.debug("Writing back " + dependencies.size() + " dependencies to file '" + pomFile.getName() +"'");
-		//dependencies.addAll(ProjectUtil.getCrossProjectDependencies());
-		
-		List nonInheritedDependencies = new ArrayList();
-		for (int i = 0; i < dependencies.size(); i++) {
-			nonInheritedDependencies.add(((DependencyWrapper) dependencies.get(i)).getDependency());
-		}
-		ProjectWriter.getWriter().setDependencies(nonInheritedDependencies, pomFile);
-    }
-
-    private void updateDeclaredSourceDirectories(SourceDirectoryGroup sourceGroup, File pomFile) throws Exception {
-		ProjectWriter pomWriter = ProjectWriter.getWriter();
-		
-		pomWriter.resetSourceDirectories(pomFile);
-		
-		//WICKED if/else -- to be removed when SourceDirectoryBatchUpdate is done - tho this will only move the problem backward
-		log.debug("Writing back " + sourceGroup.getNonInheritedSourceDirectories().size());
-		
-		for (int i = 0; i < sourceGroup.getNonInheritedSourceDirectories().size(); i++) {
-			SourceDirectory directory = (SourceDirectory) sourceGroup.getNonInheritedSourceDirectories().get(i);
-			if ( directory.isSource() ) {
-				pomWriter.addSource(directory.getDirectoryPath(), pomFile, directory.getDirectoryType());
-			}
-			if ( directory.getDirectoryType().equals(ProjectConstants.MAVEN_RESOURCE ) ) {
-				pomWriter.addResource(directory.getDirectoryPath(), pomFile);
-			}
-			if ( directory.getDirectoryType().equals(ProjectConstants.MAVEN_TEST_RESOURCE ) ) {
-				pomWriter.addUnitTestResource(directory.getDirectoryPath(), pomFile);
-			}
-					
-		}
-    }
 	
 }
