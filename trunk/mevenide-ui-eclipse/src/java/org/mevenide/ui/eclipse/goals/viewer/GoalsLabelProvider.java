28535e06a9c8d15572dcf4d82d2d75c20b63c028
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2003-09-08 22:17:31 +0000

    added goals picker ui - no logic yet, so its not yet linked to the goals textfield and prereqs vizualisation is not yet managed

---
+++ trunk/mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/goals/viewer/GoalsLabelProvider.java
@@ -0,0 +1,75 @@
+/* ====================================================================
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003 Gilles Dodinet (rhill@wanadoo.fr).  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:
+ *       "This product includes software licensed under 
+ *        Apache Software License (http://www.apache.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The names "Apache" and "Apache Software Foundation" and
+ *    "Mevenide" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact mevenide-general-dev@lists.sourceforge.net.
+ *
+ * 5. Products derived from this software may not be called "Apache",
+ *    "Mevenide", nor may "Apache" or "Mevenide" appear in their name, without
+ *    prior written permission of the Mevenide Team and the ASF.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ */
+package org.mevenide.ui.eclipse.goals.viewer;
+
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.graphics.Image;
+import org.mevenide.ui.eclipse.Mevenide;
+import org.mevenide.ui.eclipse.goals.model.Goal;
+import org.mevenide.ui.eclipse.goals.model.Plugin;
+
+/**  
+ * 
+ * needed to display nice icons
+ * 
+ * @author Gilles Dodinet (gdodinet@wanadoo.fr)
+ * @version $Id: GoalsLabelProvider.java,v 1.1 7 sept. 2003 Exp gdodinet 
+ * 
+ */
+public class GoalsLabelProvider extends LabelProvider {
+	
+    public Image getImage(Object arg0) {
+    	if ( arg0 instanceof Plugin )
+			return Mevenide.getImageDescriptor("plugin-16.gif").createImage();
+        if ( arg0 instanceof Goal )
+			return Mevenide.getImageDescriptor("goal-16.gif").createImage();
+        return null;
+    }
+
+}
