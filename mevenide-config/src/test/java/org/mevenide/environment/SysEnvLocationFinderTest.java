d88814f6f2931e11cf3fef56b55772fe5e50807a
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2004-01-26 00:34:21 +0000

    refined testcase

--- mevenide-config/src/test/java/org/mevenide/environment/SysEnvLocationFinderTest.java
+++ mevenide-config/src/test/java/org/mevenide/environment/SysEnvLocationFinderTest.java
@@ -48,6 +48,11 @@
  */
 package org.mevenide.environment;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.apache.tools.ant.taskdefs.condition.Os;
+
 
 /**  
  * 
@@ -56,26 +61,93 @@ package org.mevenide.environment;
  * 
  */
 public class SysEnvLocationFinderTest extends AbstractLocationFinderTest {
-    //only java_home and maven_home are tested since those are supposed to be required by maven
-    //no additional test setup required. 
     
     private SysEnvLocationFinder finder;
+    private String mavenHome, javaHome;
+    
+    private String[] commandLine;
     
     protected void setUp() throws Exception {
         finder = SysEnvLocationFinder.getInstance();
+        setUpCommandLine();
+        setUpExceptedEnvironmentVariables();
     }
     
+    
+    private void setUpCommandLine() {
+    	commandLine = getProcEnvCommand();
+    }
+    
+    private void setUpExceptedEnvironmentVariables() throws Exception {
+    	Process p = Runtime.getRuntime().exec(commandLine);
+    	BufferedReader br = new BufferedReader(new InputStreamReader( p.getInputStream() ));
+    	String line = null;
+    	while( (line = br.readLine()) != null ) {
+    		int idx = line.indexOf( '=' );
+    		String key = line.substring( 0, idx );
+    		String value = line.substring( idx+1 );
+    		if  ( key.equals("JAVA_HOME") ) {
+    			javaHome = value;
+    		}
+    		if  ( key.equals("MAVEN_HOME") ) {
+    			mavenHome = value;
+    		}
+    	}
+    }
     
     protected void tearDown() throws Exception {
         finder = null;
+        javaHome = null;
+        mavenHome = null;
     }
     
     public void testGetJavaHome() {
-        assertNotNull(finder.getJavaHome());
+        assertEquals(javaHome, finder.getJavaHome());
     }
 
     public void testGetMavenHome() {
-		assertNotNull(finder.getMavenHome());
+		assertEquals(mavenHome, finder.getMavenHome());
     }
 
+    //from org.apache.tools.ant.taskdefs.Execute
+    private static String[] getProcEnvCommand() {
+    	if (Os.isFamily("os/2")) {
+    		// OS/2 - use same mechanism as Windows 2000
+    		String[] cmd = {"cmd", "/c", "set" };
+    		return cmd;
+    	} 
+    	else if (Os.isFamily("windows")) {
+    		// Determine if we're running under XP/2000/NT or 98/95
+    		if (!Os.isFamily("win9x")) {
+    			// Windows XP/2000/NT
+    			String[] cmd = {"cmd", "/c", "set" };
+    			return cmd;
+    		} 
+    		else {
+    			// Windows 98/95
+    			String[] cmd = {"command.com", "/c", "set" };
+    			return cmd;
+    		}
+    	} 
+    	else if (Os.isFamily("z/os")) {
+    		String[] cmd = {"/bin/env"};
+    		return cmd;
+    	} 
+    	else if (Os.isFamily("unix")) {
+    		// Generic UNIX
+    		// Alternatively one could use: /bin/sh -c env
+    		String[] cmd = {"/usr/bin/env"};
+    		return cmd;
+    	} 
+    	else if (Os.isFamily("netware")) {
+    		String[] cmd = {"env"};
+    		return cmd;
+    	} 
+    	else {
+    		// MAC OS 9 and previous
+    		// TODO: I have no idea how to get it, someone must fix it
+    		String[] cmd = null;
+    		return cmd;
+    	}
+    }
 }
