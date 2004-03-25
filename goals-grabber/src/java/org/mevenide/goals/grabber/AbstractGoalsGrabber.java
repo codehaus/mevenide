1ed8f657dfed2af7c0ef12b9fa7de1bf4326ec5b
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2004-03-25 23:54:35 +0000

    o remove unused imports o remove dependecy on commons-collection

--- goals-grabber/src/java/org/mevenide/goals/grabber/AbstractGoalsGrabber.java
+++ goals-grabber/src/java/org/mevenide/goals/grabber/AbstractGoalsGrabber.java
@@ -16,10 +16,8 @@
  */
 package org.mevenide.goals.grabber;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
