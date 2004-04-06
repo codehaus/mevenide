e04a7847333833f29012496dc85d1910b2306b82
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2004-04-06 22:13:12 +0000

    polish dialog (tho still not linked back)

--- mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/goals/filter/GoalFilterDialog.java
+++ mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/goals/filter/GoalFilterDialog.java
@@ -17,12 +17,19 @@
 package org.mevenide.ui.eclipse.goals.filter;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.eclipse.jface.dialogs.Dialog;
-import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.jface.viewers.CheckStateChangedEvent;
 import org.eclipse.jface.viewers.CheckboxTreeViewer;
+import org.eclipse.jface.viewers.ICheckStateListener;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -32,11 +39,14 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.PlatformUI;
 import org.mevenide.ui.eclipse.goals.model.Element;
+import org.mevenide.ui.eclipse.goals.model.Goal;
 import org.mevenide.ui.eclipse.goals.model.GoalsProvider;
+import org.mevenide.ui.eclipse.goals.model.Plugin;
 import org.mevenide.ui.eclipse.goals.viewer.GoalsLabelProvider;
 import org.mevenide.ui.eclipse.preferences.PreferencesManager;
 
 /**  
+ * @todo factorize updateCheckedItems so that it can be shared between GoalsPickerDIalog and GoalsFilterDialog
  * 
  * @author <a href="mailto:rhill2@free.fr">Gilles Dodinet</a>
  * @version $Id: GoalFilterDialog.java,v 1.1 4 avr. 2004 Exp gdodinet 
@@ -51,13 +61,15 @@ public class GoalFilterDialog extends Dialog {
 	
 	private PreferencesManager preferencesManager;
 	
-	private StringFieldEditor regexFilterEditor;
+	private Text patternText;
 	private String regex;
 	
 	private CheckboxTreeViewer goalsViewer; 
+	private List checkedItems = new ArrayList();
 	
 	//if all goals selected : needed to support main view shortcut
-	private boolean filterOrigin; 
+	private boolean filterOrigin;
+	private GoalsProvider goalsProvider; 
 	
 	public GoalFilterDialog() {
 		super(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
@@ -78,7 +90,6 @@ public class GoalFilterDialog extends Dialog {
 		Composite composite = new Composite(parent, SWT.NULL);
 		GridLayout layout = new GridLayout();
 		layout.numColumns = 2;
-		layout.makeColumnsEqualWidth = true;
 		composite.setLayout(layout);
 		
 		GridData gridData = new GridData(GridData.FILL_BOTH);
@@ -97,14 +108,26 @@ public class GoalFilterDialog extends Dialog {
 		return composite;
 	}
 	
-	private void createSelectionButtonPanel(Composite composite) {
+	private void createSelectionButtonPanel(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 2;
+		composite.setLayout(layout);
+		
+		GridData gridData = new GridData();
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.horizontalSpan = 2;
+		composite.setLayoutData(gridData);
+		
 		Button selectAllButton = new Button(composite, SWT.NULL);
 		selectAllButton.setText("Select All");
-		selectAllButton.setLayoutData(new GridData());
+		GridData sData = new GridData();
+		sData.grabExcessHorizontalSpace = false;
+		selectAllButton.setLayoutData(sData);
 		selectAllButton.addMouseListener(
 			new MouseAdapter() {
 				public void mouseDown(MouseEvent e) {
-					//goalsViewer.setCheckedElements(goalsViewer.);
+					//goalsViewer.setSubtreeChecked(goalsViewer.getInput(), true);
 				}
 			}
 		);
@@ -112,23 +135,48 @@ public class GoalFilterDialog extends Dialog {
 		Button deSelectAllButton = new Button(composite, SWT.NULL);
 		deSelectAllButton.setText("Deselect All");
 		GridData dsData = new GridData();
-		dsData.verticalAlignment = GridData.BEGINNING;
 		deSelectAllButton.setLayoutData(dsData);
 		deSelectAllButton.addMouseListener(
 			new MouseAdapter() {
 				public void mouseDown(MouseEvent e) {
-					//goalsViewer.setCheckedElements(goalsViewer.);
+					//goalsViewer.setSubtreeChecked(goalsViewer.getInput(), false);
 				}
 			}
 		);
 	}
 	
-	private void createRegexFilterEditor(Composite composite) {
-		regexFilterEditor = new StringFieldEditor(CUSTOM_FILTERS_KEY, "Custom filter regular expression (matching names will be hidden) :", composite);
-		regexFilterEditor.fillIntoGrid(composite, 3);
-		regexFilterEditor.setPreferenceStore(preferencesManager.getPreferenceStore());
-		regexFilterEditor.load();
-		regexFilterEditor.setStringValue(preferencesManager.getValue(CUSTOM_FILTERS_KEY));
+	private void createRegexFilterEditor(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NULL);
+		composite.setLayout(new GridLayout());
+		GridData gridData = new GridData();
+		gridData.grabExcessHorizontalSpace = true;
+		composite.setLayoutData(gridData);
+		
+		final Button checkbox = new Button(composite, SWT.CHECK);
+		GridData checkboxData = new GridData();
+		checkboxData.grabExcessHorizontalSpace = false;
+		checkbox.setLayoutData(checkboxData);
+		checkbox.setText("Custom filter regular expressions (matching names will be hidden) :");
+		
+		patternText = new Text(composite, SWT.BORDER );
+		GridData textData = new GridData(GridData.FILL_HORIZONTAL);
+		textData.grabExcessHorizontalSpace = true;
+		patternText.setLayoutData(textData);
+		patternText.setText(preferencesManager.getValue(CUSTOM_FILTERS_KEY));
+		patternText.setEnabled(checkbox.getSelection());
+		
+		final Label label = new Label(composite, SWT.NULL);
+		label.setText("Patterns are separated by comma");
+		label.setEnabled(checkbox.getSelection());
+		
+		checkbox.addSelectionListener(
+			new SelectionAdapter() {
+				public void widgetSelected(SelectionEvent e) {
+					label.setEnabled(checkbox.getSelection());
+					patternText.setEnabled(checkbox.getSelection());
+				}
+			}
+		);
 	}
 
 	private void createSeparator(Composite composite) {
@@ -147,8 +195,8 @@ public class GoalFilterDialog extends Dialog {
 		
 		goalsViewer = new CheckboxTreeViewer(parent, SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER);
     	
-    	GoalsProvider goalsProvider = new GoalsProvider();
-        GoalsLabelProvider goalsLabelProvider = new GoalsLabelProvider();
+    	goalsProvider = new GoalsProvider();
+		GoalsLabelProvider goalsLabelProvider = new GoalsLabelProvider();
     	
     	goalsViewer.setContentProvider(goalsProvider);
     	goalsViewer.setLabelProvider(goalsLabelProvider);
@@ -160,12 +208,73 @@ public class GoalFilterDialog extends Dialog {
     	gridData.heightHint = 300;
     
     	goalsViewer.getTree().setLayoutData(gridData);
+    	
+    	goalsViewer.addCheckStateListener(
+        	new ICheckStateListener() {
+				public void checkStateChanged(CheckStateChangedEvent event) {
+                	updateCheckedItems(event);
+        		}
+        	}
+        );
+	}
+	
+	private void updateCheckedItems(CheckStateChangedEvent e) {
+		
+		boolean isSelectionChecked = ((CheckboxTreeViewer) e.getSource()).getChecked(e.getElement());
+		
+		if ( e.getElement() instanceof Goal ) {
+			Goal goal = (Goal) e.getElement();
+			updateCheckedGoal(isSelectionChecked, goal);
+		}
+		else {
+			Plugin plugin = (Plugin) e.getElement();
+			//it is way too confusing when plugins are checkable. indeed when 
+			//theres a default goal, both the default and the plugin should be 
+			//checkable, thus we got the goal multiple times. I think its best
+			//to just disable plugins.
+			
+			//updateCheckedPlugin(isSelectionChecked, plugin);
+			
+			//prevent user to check a plugin	
+			goalsViewer.setChecked(plugin, false);
+		}
+	}
+	
+	private void updateCheckedGoal(boolean isSelectionChecked, Goal goal) {
+		String fullyQualifiedGoalName = goal.getPlugin().getName();
+		if ( !goal.getName().equals(Goal.DEFAULT_GOAL) ) {
+			fullyQualifiedGoalName += ":" + goal.getName();
+		}
+		if ( isSelectionChecked ) {
+			checkedItems.add(fullyQualifiedGoalName);
+		}
+		else {
+			checkedItems.remove(fullyQualifiedGoalName);
+		}
+	}
+	
+	private void updateCheckedPlugin(boolean isSelectionChecked, Plugin plugin) {
+		String pluginName = plugin.getName();
+		String[] goals = goalsProvider.getGoalsGrabber().getGoals(pluginName);
+		if ( goals != null && goals.length > 0 ) {
+			if ( !Arrays.asList(goals).contains(Goal.DEFAULT_GOAL) ) {
+				goalsViewer.setChecked(pluginName, false);
+			}
+			else {
+				if ( isSelectionChecked ) {
+					checkedItems.add(pluginName);
+				}
+				else {
+					checkedItems.remove(pluginName);
+				}
+			}
+		}
 	}
 	
 	protected void okPressed() {
 	    super.okPressed();
 	    
-	    regex = regexFilterEditor.getStringValue();
+	    regex = patternText.getText();
 	    preferencesManager.setValue(CUSTOM_FILTERS_KEY, regex);
 	    
 	    preferencesManager.store();
