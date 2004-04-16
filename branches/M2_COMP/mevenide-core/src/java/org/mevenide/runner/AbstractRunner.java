a7d15f6e4dfdb5f0ed43eabc6ec3e432126cbd46
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2004-04-16 22:53:19 +0000

    use o.a.m.m.Scm in place of o.a.m.m.Repository

--- branches/M2_COMP/mevenide-core/src/java/org/mevenide/runner/AbstractRunner.java
+++ branches/M2_COMP/mevenide-core/src/java/org/mevenide/runner/AbstractRunner.java
@@ -110,6 +110,9 @@ public abstract class AbstractRunner {
      */
     protected abstract void launchVM(String[] options, String[] goals) throws Exception ;
 
+    protected void testMavenNG() {
+    	
+    }
 
 
 }
