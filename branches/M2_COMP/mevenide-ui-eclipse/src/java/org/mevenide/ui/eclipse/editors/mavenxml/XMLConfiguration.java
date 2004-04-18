aa80d673897d8dd2dd9ae50b737f6df245ed7e55
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2004-04-18 13:50:40 +0000

    begin to add contentassist support. not fully functionnal yet, committing it for backup purposes

--- branches/M2_COMP/mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/editors/mavenxml/XMLConfiguration.java
+++ branches/M2_COMP/mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/editors/mavenxml/XMLConfiguration.java
@@ -1,8 +1,13 @@
 package org.mevenide.ui.eclipse.editors.mavenxml;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextDoubleClickStrategy;
 import org.eclipse.jface.text.TextAttribute;
+import org.eclipse.jface.text.contentassist.ContentAssistant;
+import org.eclipse.jface.text.contentassist.IContentAssistProcessor;
+import org.eclipse.jface.text.contentassist.IContentAssistant;
 import org.eclipse.jface.text.presentation.IPresentationReconciler;
 import org.eclipse.jface.text.presentation.PresentationReconciler;
 import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
@@ -11,20 +16,27 @@ import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.text.source.SourceViewerConfiguration;
 
 public class XMLConfiguration extends SourceViewerConfiguration {
+	private static final Log log = LogFactory.getLog(XMLConfiguration.class);
+	
 	private XMLDoubleClickStrategy doubleClickStrategy;
 	private XMLTagScanner tagScanner;
 	private XMLScanner scanner;
 	private ColorManager colorManager;
 
-	public XMLConfiguration(ColorManager colorManager) {
+	private MavenXmlEditor editor;
+	
+	public XMLConfiguration(ColorManager colorManager, MavenXmlEditor editor) {
 		this.colorManager = colorManager;
+		this.editor = editor;
 	}
+	
 	public String[] getConfiguredContentTypes(ISourceViewer sourceViewer) {
 		return new String[] {
 			IDocument.DEFAULT_CONTENT_TYPE,
 			XMLPartitionScanner.XML_COMMENT,
 			XMLPartitionScanner.XML_TAG };
 	}
+	
 	public ITextDoubleClickStrategy getDoubleClickStrategy(
 		ISourceViewer sourceViewer,
 		String contentType) {
@@ -43,6 +55,7 @@ public class XMLConfiguration extends SourceViewerConfiguration {
 		}
 		return scanner;
 	}
+	
 	protected XMLTagScanner getXMLTagScanner() {
 		if (tagScanner == null) {
 			tagScanner = new XMLTagScanner(colorManager);
@@ -76,4 +89,26 @@ public class XMLConfiguration extends SourceViewerConfiguration {
 		return reconciler;
 	}
 
+	public IContentAssistant getContentAssistant(ISourceViewer viewer) {
+		ContentAssistant assistant = new ContentAssistant();
+		try {
+			// Create content assistant processor
+			IContentAssistProcessor processor = new MavenXmlContentAssistProcessor(editor);
+			   
+			// Set this processor for each supported content type
+			assistant.setContentAssistProcessor(processor, XMLPartitionScanner.XML_TAG);
+			assistant.setContentAssistProcessor(processor, XMLPartitionScanner.XML_DEFAULT);
+			assistant.setContentAssistProcessor(processor, IDocument.DEFAULT_CONTENT_TYPE);
+			  
+			assistant.enableAutoActivation(true);
+			assistant.enableAutoInsert(true);
+			   
+			assistant.install(viewer);
+			   
+		} 
+	    catch (Exception e) {
+			log.error("Unable to create ContentAssistant. Unasigned assistant will be returned.", e);
+		}
+		return assistant;
+	}
 }
