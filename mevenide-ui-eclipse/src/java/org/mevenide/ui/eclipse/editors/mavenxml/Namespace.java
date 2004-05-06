1dbabd46be684917a59ede4f0038b7943f0a50ff
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2004-05-06 19:09:21 +0000

    place subtags at top of the list. remaining issue for attainGoal.

--- mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/editors/mavenxml/Namespace.java
+++ mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/editors/mavenxml/Namespace.java
@@ -20,13 +20,22 @@ public class Namespace {
     private static final Log log = LogFactory.getLog(Namespace.class);
 
     public static final String TOPLEVEL = "-toplevel-";
-    public static final String DEFAULTNAMESPACE = "defaultns";
+    public static final String DEFAULTNAMESPACE = "default:maven";
 	
-	private static final List WERKZ_LIST = new ArrayList();    
+	private static final List WERKZ_LIST = new ArrayList(); 
+	    
+    private static final List PROJECT_LIST = new ArrayList();
+    {
+    	PROJECT_LIST.add("project");
+    }
+    
     
     private String prefix;
     private String uri;
-
+    
+    /** indicates if the NameSpace should targeted to generic jelly editor */
+    private boolean generic;
+    
     private TagLib taglib;
 
 	private Map attributes = new TreeMap();
@@ -34,13 +43,13 @@ public class Namespace {
 	private List rootTags = new ArrayList();
 	private Map subTags = new TreeMap();
 	
-    private static final String[] WERKZ_TAGS = new String[] {"goal", "preGoal", "attainGoal", "postGoal",};
+    private static final String[] WERKZ_TAGS = new String[] {"goal", "preGoal", "postGoal",};
 
 
     public Namespace(String prefix, String uri) {
     	this.prefix = prefix;
         this.uri = uri;
-        initializeWerkzList();
+        initializeWerkzLists();
         loadTagLib();
         if ( taglib == null ) {
             taglib = new EmptyTagLibImpl(prefix + ":" + uri);
@@ -52,9 +61,9 @@ public class Namespace {
         Collections.sort(rootTags);
     }
 
-    private void initializeWerkzList() {
+    private void initializeWerkzLists() {
     	for (int i = 0; i < WERKZ_TAGS.length; i++) {
-        	WERKZ_LIST.add(WERKZ_TAGS[i]);    
+        	WERKZ_LIST.add(WERKZ_TAGS[i]);
         }
     }
 
@@ -118,18 +127,21 @@ public class Namespace {
 
 
     public Collection getSubTags(String outerTag) {
-        if ( "project".equals(outerTag) ) {
-        	return WERKZ_LIST; 
+        if ( !generic && TOPLEVEL.equals(outerTag) ) {
+        	return PROJECT_LIST;
+        }
+        else if ( !generic && "project".equals(outerTag) ) {
+            return WERKZ_LIST;
         } 
-        else if ( WERKZ_LIST.contains(outerTag) ) {
-        	return rootTags;
+        else if ( !generic && WERKZ_LIST.contains(outerTag) ) {
+            return rootTags;
         }
         else {
 	        if ( outerTag.indexOf(":") != -1 ) {
 	            outerTag = StringUtils.split(outerTag, ":")[1];
 	        }
             return getCandidateTags(outerTag);
-        }
+      }
     }
 
     private Collection getCandidateTags(String outerTag) {
@@ -145,15 +157,30 @@ public class Namespace {
                 collectedTags.add(prefix + ":" + tag);
             }
         	
-        	Collections.sort((List) collectedTags);
+        	//Collections.sort((List) collectedTags);
         	candidates = collectedTags;
         	subTags.put(outerTag, collectedTags);
         }
+        
+        if ( !generic && !rootTags.contains("attainGoal") ) {
+	        rootTags.add("attainGoal");
+	    }
         
         return candidates;
     }
     
     public List getRootTags() {
         return rootTags;
+    }
+    
+    public boolean isGeneric() {
+        return generic;
+    }
+    
+    public void setGeneric(boolean generic) {
+        this.generic = generic;
+    }
+    public List getWerkzList() {
+        return WERKZ_LIST;
     }
 }
