be299049be31a74d33afaeccd2461724db87d52a
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2004-05-13 20:01:31 +0000

    added ${pom.<>} resolving into  the IPropertyResolver implementation. Current usages apply, the new one needs some more work.

---
+++ mevenide-config/src/java/org/mevenide/properties/resolver/ProjectWalker2.java
@@ -0,0 +1,93 @@
+/* ==========================================================================
+ * Copyright 2003-2004 Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * =========================================================================
+ */
+package org.mevenide.properties.resolver;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.StringTokenizer;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.maven.project.Project;
+import org.mevenide.context.IQueryContext;
+import org.mevenide.properties.IPropertyFinder;
+
+
+/**
+ * IQueryContext based replacement for ProjectWalker. fits into the resolver pattern.
+ * Is used in PropertyFilesAggregator to resolve ${pom. values)
+ * I guess we should rename the propertyFilesAgreggator.
+ * @author  <a href="mailto:ca206216@tiscali.cz">Milos Kleint</a>
+ *
+ */
+public class ProjectWalker2 implements IPropertyFinder {
+    private static final Log logger = LogFactory.getLog(ProjectWalker2.class);
+    
+    private IQueryContext context;
+    public ProjectWalker2(IQueryContext qcontext) {
+        context = qcontext;
+    }
+    
+    
+    public String getValue(String key) {
+        if (!key.startsWith("pom.")) {
+            return null;
+        }
+        Project proj = context.getFinalProject();
+        if (proj == null) {
+            return null;
+        }
+        StringTokenizer tok = new StringTokenizer(key, ".", false);
+        // skip "pom."
+        tok.nextToken();
+        Object currentReflectionObject = proj;
+        while (tok.hasMoreTokens()) {
+            String next = tok.nextToken();
+            next = "get" + Character.toUpperCase(next.charAt(0)) + next.substring(1);
+            try {
+                Method method = currentReflectionObject.getClass().getMethod(next, null); 
+                currentReflectionObject = method.invoke(currentReflectionObject, null);
+            } catch (NoSuchMethodException exc) {
+                currentReflectionObject = null;
+                logger.error("wrong pom definition=" + key, exc);
+            } catch (InvocationTargetException exc2) {
+                currentReflectionObject = null;
+                logger.error("wrong pom definition=" + key, exc2);
+            } catch (Exception exc3) {
+                // illegataccess + illegalargument
+                currentReflectionObject = null;
+                logger.error("wrong pom definition=" + key, exc3);
+            }
+            if (currentReflectionObject == null) {
+                break;
+            }
+        }
+        if (currentReflectionObject != null && currentReflectionObject instanceof String) {
+            return currentReflectionObject.toString();
+        }
+        if (currentReflectionObject != null && (! (currentReflectionObject instanceof String))) {
+            logger.warn("not a string pom value=" + key + " class=" + currentReflectionObject.getClass());
+        }
+        return null;
+    }
+    
+    public void reload() {
+    }
+    
+}
