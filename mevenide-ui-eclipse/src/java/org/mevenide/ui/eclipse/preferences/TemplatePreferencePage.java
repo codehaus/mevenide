c2c5713e5f0a439a6b6baef80bf0a6126e2791e8
jens <jens@c6c22482-0310-0410-b13f-f1d71ac210c7>
2004-05-31 22:54:49 +0000

    added a model to the TemplatePreferencePage and implemented the add and remove actions

--- mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/preferences/TemplatePreferencePage.java
+++ mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/preferences/TemplatePreferencePage.java
@@ -17,9 +17,8 @@
  */
 package org.mevenide.ui.eclipse.preferences;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.nio.channels.FileChannel;
+import java.util.Observable;
+import java.util.Observer;
 
 import org.apache.maven.project.Project;
 import org.eclipse.core.runtime.IPath;
@@ -28,21 +27,30 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.mevenide.project.io.ProjectReader;
 import org.mevenide.ui.eclipse.Mevenide;
+import org.mevenide.ui.eclipse.template.model.Template;
+import org.mevenide.ui.eclipse.template.model.Templates;
+import org.mevenide.ui.eclipse.util.FileUtils;
 /**
  * This preference page contains information about the POM templates which
  * the used has added to his or her .meta folder. From this page it's possible 
@@ -53,9 +61,106 @@ import org.mevenide.ui.eclipse.Mevenide;
  * @author	<a href="mailto:jens@iostream.net">Jens Andersen</a>, Last updated by $Author$
  * @version $Id$
  */
-public class TemplatePreferencePage extends PreferencePage
-		implements
-			IWorkbenchPreferencePage {
+public class TemplatePreferencePage extends PreferencePage implements IWorkbenchPreferencePage 
+{
+	private Templates fTemplates;
+	private TreeViewer templateViewer;
+	private Template fCurrentSelction;
+	private ISelectionChangedListener fSelectionProvicer;
+	
+	class TemplateContentProvider implements ITreeContentProvider, Observer
+	{
+		private Object[] EMPTY_ARRAY = new Object[0];
+		private Viewer fViewer;
+
+		/* (non-Javadoc)
+		 * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(java.lang.Object)
+		 */
+		public boolean hasChildren(Object arg0) {
+			return false;
+		}	
+		/* (non-Javadoc)
+		 * @see org.eclipse.jface.viewers.ITreeContentProvider#getChildren(java.lang.Object)
+		 */
+		public Object[] getChildren(Object obj) {
+			if(obj instanceof Templates)
+			{
+				return ((Templates)obj).getTemplates(); 
+			}
+			return EMPTY_ARRAY;
+		}
+		/* (non-Javadoc)
+		 * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java.lang.Object)
+		 */
+		public Object[] getElements(Object obj) {
+			return getChildren(obj);
+		}
+		/* (non-Javadoc)
+		 * @see org.eclipse.jface.viewers.ITreeContentProvider#getParent(java.lang.Object)
+		 */
+		public Object getParent(Object arg0) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		/* (non-Javadoc)
+		 * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
+		 */
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+			fViewer = viewer;
+			
+			if(oldInput != null && oldInput instanceof Templates)
+				((Templates)oldInput).deleteObserver(this);
+			if(newInput != null && newInput instanceof Templates)
+				((Templates)newInput).addObserver(this);
+
+		}
+
+		/* (non-Javadoc)
+		 * @see org.eclipse.jface.viewers.IContentProvider#dispose()
+		 */
+		public void dispose() {
+		}
+		/* (non-Javadoc)
+		 * @see java.util.Observer#update(java.util.Observable, java.lang.Object)
+		 */
+		public void update(Observable o, Object arg) {
+			Display.getDefault().syncExec(new Runnable(){
+				/* (non-Javadoc)
+				 * @see java.lang.Runnable#run()
+				 */
+				public void run() {
+					fViewer.refresh();
+				}	
+			});
+		}
+	}
+	
+	class TemplateLabelProvider extends LabelProvider {
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org.eclipse.jface.viewers.LabelProvider#getText(java.lang.Object)
+		 */
+		public String getText(Object obj) {
+			if (obj instanceof Template) {
+				return ((Template)obj).getTemplateName();
+			}
+			return super.getText(obj);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org.eclipse.jface.viewers.LabelProvider#getImage(java.lang.Object)
+		 */
+		public Image getImage(Object obj) {
+			if (obj instanceof Template) {
+				return Mevenide.getImageDescriptor("pom_file.gif").createImage();
+			}
+			return super.getImage(obj);
+		}
+
+	}
 	/**
 	 * @param arg0
 	 * @param arg1
@@ -70,7 +175,35 @@ public class TemplatePreferencePage extends PreferencePage
 	 * @see org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
 	 */
 	public void init(IWorkbench arg0) {
-		// TODO Auto-generated method stub
+		fTemplates = new Templates();
+
+		File tmplFolder = Mevenide.getInstance().getStateLocation().append("templates").toFile();//$NON-NLS-1$
+		
+		if(tmplFolder.exists())
+		{
+			Template tmp;
+			File f;
+			File[] tmpls = tmplFolder.listFiles();
+			ProjectReader reader; 
+			
+			for(int i=0; i < tmpls.length; i++)
+			{
+				try
+				{
+					f = tmpls[i];
+					if(f.getName().endsWith("tmpl"))//$NON-NLS-1$
+					{
+						reader = ProjectReader.getReader();
+						Project pom = reader.read(f);
+						fTemplates.addTemplate(new Template(pom));
+					}
+				}catch(Exception e)
+				{
+					
+				}
+			}
+		}
+		
 	}
 	/*
 	 * (non-Javadoc)
@@ -85,27 +218,31 @@ public class TemplatePreferencePage extends PreferencePage
 		layout.numColumns = 2;
 		composite.setLayout(layout);
 		GridData gd = new GridData(GridData.FILL_BOTH);
-		TreeViewer templateViewer = new TreeViewer(composite);
+		
+		templateViewer = new TreeViewer(composite);
 		templateViewer.getControl().setLayoutData(gd);
 		templateViewer.setUseHashlookup(true);
-		//templateViewer.setContentProvider(new TreeViewContentProvider());
-		//templateViewer.setLabelProvider(new TreeViewLabelProvider());
-		//templateViewer.setInput();
-		//templateViewer.setSorter(new NameSorter());
-		ISelectionChangedListener selectionProvicer = new ISelectionChangedListener() {
+		TemplateContentProvider contentProvider = new TemplateContentProvider();
+		fTemplates.addObserver(contentProvider);
+		templateViewer.setContentProvider(contentProvider);
+		templateViewer.setLabelProvider(new TemplateLabelProvider());
+		templateViewer.setInput(fTemplates);
+		templateViewer.setSorter(new ViewerSorter());
+		fSelectionProvicer = new ISelectionChangedListener() {
 			public void selectionChanged(SelectionChangedEvent event) {
-				IStructuredSelection selection = (IStructuredSelection) event
-						.getSelection();
+				IStructuredSelection selection = (IStructuredSelection) event.getSelection();
+				fCurrentSelction = (Template)selection.getFirstElement();
 			}
 		};
-		templateViewer.addSelectionChangedListener(selectionProvicer);
-		
+		templateViewer.addSelectionChangedListener(fSelectionProvicer);
+
 		Composite buttonComp = new Composite(composite, SWT.NONE);
 		GridLayout envButtonLayout = new GridLayout();
 		envButtonLayout.marginHeight = 0;
 		envButtonLayout.marginWidth = 0;
 		buttonComp.setLayout(envButtonLayout);
-		gd = new GridData(GridData.VERTICAL_ALIGN_BEGINNING | GridData.HORIZONTAL_ALIGN_FILL);
+		gd = new GridData(GridData.VERTICAL_ALIGN_BEGINNING
+				| GridData.HORIZONTAL_ALIGN_FILL);
 		buttonComp.setLayoutData(gd);
 		Button addtemplateButton = new Button(buttonComp, SWT.PUSH);
 		addtemplateButton.setText(Mevenide.getResourceString("TemplatePreferencePage.template.add"));//$NON-NLS-1$
@@ -119,18 +256,17 @@ public class TemplatePreferencePage extends PreferencePage
 			/**
 			 * @see org.eclipse.swt.events.SelectionListener#widgetSelected(SelectionEvent)
 			 */
-			public void widgetSelected(SelectionEvent e)
-			{
+			public void widgetSelected(SelectionEvent e) {
 				FileDialog dialog = new FileDialog(getShell(), SWT.SINGLE);
 				dialog.open();
 				IPath p = new Path(dialog.getFilterPath() + File.separator + dialog.getFileName());
-				
-				if(!copyTemplateToPreferences(p))
-					MessageDialog.openError(getShell(), Mevenide.getResourceString("TemplatePreferencePage.op_error_title"), Mevenide.getResourceString("TemplatePreferencePage.op_error_create.message"));
+
+				if (!copyTemplateToPreferences(p))
+					MessageDialog.openError(getShell(),Mevenide.getResourceString("TemplatePreferencePage.op_error_title"),Mevenide.getResourceString("TemplatePreferencePage.op_error_create.message"));//$NON-NLS-1$
 			}
 		});
 		Button removeTemplateButton = new Button(buttonComp, SWT.PUSH);
-		removeTemplateButton.setText(Mevenide.getResourceString("TemplatePreferencePage.template.remove"));
+		removeTemplateButton.setText(Mevenide.getResourceString("TemplatePreferencePage.template.remove"));//$NON-NLS-1$
 		removeTemplateButton.addSelectionListener(new SelectionListener() {
 			/**
 			 * @see org.eclipse.swt.events.SelectionListener#widgetDefaultSelected(SelectionEvent)
@@ -141,42 +277,41 @@ public class TemplatePreferencePage extends PreferencePage
 			 * @see org.eclipse.swt.events.SelectionListener#widgetSelected(SelectionEvent)
 			 */
 			public void widgetSelected(SelectionEvent e) {
+				if(fCurrentSelction != null)
+				{
+					if(MessageDialog.openConfirm(getShell(),Mevenide.getResourceString("TemplatePreferencePage.del.confirm.title"),Mevenide.getResourceString("TemplatePreferencePage.del.confirm.message") + " " + fCurrentSelction.getTemplateName())) //$NON-NLS-1$
+					{
+						fCurrentSelction.getProject().getFile().delete();
+						fTemplates.removeTemplate(fCurrentSelction);
+						fCurrentSelction = null;
+					}
+				}
 			}
 		});
 		return composite;
 	}
 	/**
-	 * 
+	 *  
 	 */
-	private boolean copyTemplateToPreferences(IPath source)
-	{
-		try{
+	private boolean copyTemplateToPreferences(IPath source) {
+		try {
 			ProjectReader reader = ProjectReader.getReader();
 			Project pom = reader.read(source.toFile());
-			
-			IPath dest = Mevenide.getInstance().getStateLocation().append("templates");
+
+			IPath dest = Mevenide.getInstance().getStateLocation().append("templates");//$NON-NLS-1$
 
 			//If it's the first template we are going to add then create the
 			//template folder
-			if(!dest.toFile().exists())
+			if (!dest.toFile().exists())
 				dest.toFile().mkdir();
-			dest = dest.append(pom.toString() + pom.hashCode() +".tmpl");
+			dest = dest.append(pom.toString() + pom.hashCode() + ".tmpl");//$NON-NLS-1$
 
-			copyFile(source.toFile(), dest.toFile());
+			FileUtils.copyFile(source.toFile(), dest.toFile());
+			pom = reader.read(dest.toFile());
+			fTemplates.addTemplate(new Template(pom));
 			return true;
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			return false;
 		}
 	}
-
-	  public void copyFile(File in, File out) throws Exception {
-	     FileChannel sourceChannel = new FileInputStream(in).getChannel();
-	     FileChannel destinationChannel = new FileOutputStream(out).getChannel();
-	     sourceChannel.transferTo(0, sourceChannel.size(), destinationChannel);
-	     sourceChannel.close();
-	     destinationChannel.close();
-	     }
-
 }
