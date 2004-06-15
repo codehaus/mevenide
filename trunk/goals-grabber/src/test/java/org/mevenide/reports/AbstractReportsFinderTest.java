b0fa998108e62d7572e5ddceb7fce0e4848e30c4
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2004-06-15 19:28:23 +0000

    fix MEVENIDE-26 :   drop the DefaultRportsFinder b/c it doesnot pass new tests added (that are more complex with some prereqs handling needed)   replace it by JDomReportsFinder which first find candidates goal in the plugins.cache file

--- trunk/goals-grabber/src/test/java/org/mevenide/reports/AbstractReportsFinderTest.java
+++ trunk/goals-grabber/src/test/java/org/mevenide/reports/AbstractReportsFinderTest.java
@@ -18,30 +18,34 @@ package org.mevenide.reports;
 
 import java.io.File;
 import java.util.Arrays;
-
 import org.mevenide.goals.AbstractTestCase;
 import org.mevenide.goals.test.util.TestUtils;
 
+
 /**  
  * 
- * @author Gilles Dodinet (gdodinet@wanadoo.fr)
- * @version $Id: DefaultReportsFinderTest.java,v 1.1 21 sept. 2003 Exp gdodinet 
+ * @author <a href="mailto:rhill2@free.fr">Gilles Dodinet</a>
+ * @version $Id$
  * 
  */
-public class DefaultReportsFinderTest extends AbstractTestCase {
+public abstract class AbstractReportsFinderTest extends AbstractTestCase {
 	
+    private IReportsFinder finder;
+    
 	private String[] testPluginDirs = 
 		{
 			"maven-faq-plugin-1.1-SNAPSHOT", //nota - faq-plugin has been modified in src/test/conf so that doc:registerReport gets called twice
 			"maven-findbugs-plugin-1.0-SNAPSHOT",
 			"maven-tasklist-plugin-2.2-SNAPSHOT",
 			"maven-test-plugin-1.4-SNAPSHOT",
+			"maven-toto-plugin-1.1"
 		};
 		
     protected void setUp() throws Exception {
     	super.setUp();
+    	finder = getReportsFinder();
     	for (int i = 0; i < testPluginDirs.length; i++) {
-    		File src = new File(DefaultReportsFinderTest.class.getResource("/plugins/" + testPluginDirs[i] + "/plugin.jelly").getFile());
+    		File src = new File(AbstractReportsFinderTest.class.getResource("/plugins/" + testPluginDirs[i] + "/plugin.jelly").getFile());
 			File pluginDir = new File(pluginsLocal, testPluginDirs[i]);
 			if ( !pluginDir.exists() ) {
 				pluginDir.mkdirs();
@@ -52,16 +56,18 @@ public class DefaultReportsFinderTest extends AbstractTestCase {
     	
     }
 
+    protected abstract IReportsFinder getReportsFinder();
+
     protected void tearDown() throws Exception {
     	super.tearDown();
     }
 
     public void testFindReports() throws Exception {
-    	String[] reports = new DefaultReportsFinder().findReports();
-    	assertEquals(3, reports.length);
+    	String[] reports = finder.findReports();
+    	assertEquals(4, reports.length);
     	assertTrue(Arrays.asList(reports).contains("maven-faq-plugin"));
+    	assertTrue(Arrays.asList(reports).contains("maven-tutu-plugin"));
 		assertTrue(Arrays.asList(reports).contains("maven-findbugs-plugin"));
 		assertTrue(Arrays.asList(reports).contains("maven-tasklist-plugin"));
     }
-
 }
