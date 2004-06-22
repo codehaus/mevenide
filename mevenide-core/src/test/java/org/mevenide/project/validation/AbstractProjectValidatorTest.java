9f98532de788b6b2c6ddb82a1449a5bab2a32a91
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2004-06-22 22:48:14 +0000

    first pieces of validation

---
+++ mevenide-core/src/test/java/org/mevenide/project/validation/AbstractProjectValidatorTest.java
@@ -0,0 +1,35 @@
+/* ==========================================================================
+ * Copyright 2003-2004 Apache Software Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * =========================================================================
+ */
+package org.mevenide.project.validation;
+
+import org.mevenide.AbstractMevenideTestCase;
+import org.mevenide.project.io.ProjectReader;
+
+
+/**  
+ * 
+ * @author <a href="mailto:rhill2@free.fr">Gilles Dodinet</a>
+ * @version $Id$
+ * 
+ */
+public abstract class AbstractProjectValidatorTest extends AbstractMevenideTestCase {
+    public void testValidate() throws Exception  {
+        getValidator().validate(ProjectReader.getReader().read(projectFile));
+    }
+    
+    protected abstract IProjectValidator getValidator();
+}
