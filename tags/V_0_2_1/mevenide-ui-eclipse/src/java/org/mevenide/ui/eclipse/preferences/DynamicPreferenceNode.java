6aa23aa16f1166f9c4b743b234479f60564c61d4
nobody <nobody@c6c22482-0310-0410-b13f-f1d71ac210c7>
2004-07-13 21:11:12 +0000

    This commit was manufactured by cvs2svn to create tag 'V_0_2_1'.

---
+++ tags/V_0_2_1/mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/preferences/DynamicPreferenceNode.java
@@ -0,0 +1,152 @@
+/* ==========================================================================
+ * Copyright 2003-2004 Apache Software Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * =========================================================================
+ */
+package org.mevenide.ui.eclipse.preferences;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.eclipse.jface.preference.IPreferenceNode;
+import org.eclipse.jface.preference.IPreferencePage;
+import org.eclipse.jface.preference.PreferenceNode;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.util.Assert;
+import org.eclipse.swt.graphics.Image;
+
+
+/**  
+ * 
+ * needed to obtain a consistent behaviour (f.i. disposed page cause troubles if not correctly handled) 
+ * much has been taken from superclass 
+ * 
+ * @author <a href="mailto:rhill2@free.fr">Gilles Dodinet</a>
+ * @version $Id$
+ * 
+ */
+public class DynamicPreferenceNode extends PreferenceNode {
+
+    private String pageId;
+    private String pageTitle;
+    private String pluginDescription;
+    
+    private List pluginCategories;
+    
+    private IPreferencePage page;
+	private List subNodes;
+	private ImageDescriptor imageDescriptor;
+	private Image image;
+	
+    public DynamicPreferenceNode(String pageId, String pageTitle, String pluginDescription, List categories) {
+        super(pageId, pageTitle, null, DynamicPreferencePage.class.getName());
+        this.pageId = pageId;
+        this.pageTitle = pageTitle; 
+        this.pluginCategories = categories;
+        this.pluginDescription = pluginDescription;
+    }
+    
+    public void createPage() {
+        if ( page == null ) {
+            page = new DynamicPreferencePage();
+            page.setTitle(pageTitle);
+            ((DynamicPreferencePage) page).setCategories(pluginCategories);
+            ((DynamicPreferencePage) page).setPluginDescription(pluginDescription);
+            ((DynamicPreferencePage) page).setPluginName(pageTitle);
+            if (getLabelImage() != null) {
+    			page.setImageDescriptor(imageDescriptor);
+            }
+        }
+    }
+    
+    public String getLabelText() {
+        return pageTitle;
+    }
+    
+    public IPreferencePage getPage() {
+        if ( page == null ) {
+            createPage();
+        }
+        return page;
+    }
+    
+	public void add(IPreferenceNode node) {
+		if (subNodes == null)
+			subNodes = new ArrayList();
+		subNodes.add(node);
+	}
+
+	public void disposeResources() {
+		if (image != null) {
+			image.dispose();
+			image = null;
+		}
+		if (page != null) {
+			page.dispose();
+			page = null;
+		}
+	}
+	
+	public IPreferenceNode findSubNode(String id) {
+		Assert.isNotNull(id);
+		Assert.isTrue(id.length() > 0);
+		if (subNodes == null)
+			return null;
+		int size = subNodes.size();
+		for (int i = 0; i < size; i++) {
+			IPreferenceNode node = (IPreferenceNode) subNodes.get(i);
+			if (id.equals(node.getId()))
+				return node;
+		}
+		return null;
+	}
+	
+	public String getId() {
+		return this.pageId;
+	}
+	
+	protected ImageDescriptor getImageDescriptor() {
+		return imageDescriptor;
+	}
+	
+	public Image getLabelImage() {
+		if (image == null && imageDescriptor != null) {
+			image = imageDescriptor.createImage();
+		}
+		return image;
+	}
+	
+	
+	public IPreferenceNode[] getSubNodes() {
+		if (subNodes == null)
+			return new IPreferenceNode[0];
+		return (IPreferenceNode[]) subNodes.toArray(new IPreferenceNode[subNodes.size()]);
+	}
+	
+	public IPreferenceNode remove(String id) {
+		IPreferenceNode node = findSubNode(id);
+		if (node != null)
+			remove(node);
+		return node;
+	}
+	
+	public boolean remove(IPreferenceNode node) {
+		if (subNodes == null)
+			return false;
+		return subNodes.remove(node);
+	}
+	
+	public void setPage(IPreferencePage newPage) {
+		page = newPage;
+	}
+}
---
+++ tags/V_0_2_1/mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/preferences/DynamicPreferencePage.java
@@ -0,0 +1,269 @@
+/* ==========================================================================
+ * Copyright 2003-2004 Apache Software Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * =========================================================================
+ */
+package org.mevenide.ui.eclipse.preferences;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.mevenide.ui.eclipse.Mevenide;
+import org.mevenide.util.StringUtils;
+
+
+/**  
+ * 
+ * @author <a href="mailto:rhill2@free.fr">Gilles Dodinet</a>
+ * @version $Id$
+ * 
+ */
+public class DynamicPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
+    
+    private List categories;
+    private String pluginDescription;
+    private String pluginName;
+    
+    private Map editors = new HashMap();
+    
+    private PreferencesManager preferencesManager;
+    
+    private List dirtyProperties = new ArrayList();
+    
+    public DynamicPreferencePage() {
+        super();
+        preferencesManager = DynamicPreferencesManager.getDynamicManager();
+		preferencesManager.loadPreferences();
+    }
+    
+    protected Control createContents(Composite parent) {
+        Composite composite = new Composite(parent, SWT.NULL);
+        composite.setLayout(new GridLayout());
+        
+        TabFolder tabFolder = new TabFolder(composite, SWT.TOP);
+        tabFolder.setLayoutData(new GridData(GridData.FILL_BOTH));
+        
+        createDescriptionComposite(tabFolder);
+        
+        for (int i = 0; i < categories.size(); i++) {
+	        createCategoryComposite(tabFolder, (PluginCategory) categories.get(i));
+        }
+        
+        return composite; 
+	}
+  
+	private void createDescriptionComposite(TabFolder tabFolder) {
+	    TabItem tabItem = new TabItem(tabFolder, SWT.NULL); 
+	    tabItem.setText(Mevenide.getResourceString("DynamicPreferencePage.Description")); //$NON-NLS-1$
+	    
+	    Composite area = new Composite(tabFolder, SWT.NULL);
+        GridLayout layout = new GridLayout();
+        layout.marginHeight = 10;
+        layout.marginWidth = 10;
+        area.setLayout(layout);
+        
+        Group descriptionGroup = new Group(area, SWT.NULL);
+        descriptionGroup.setText(pluginName + " " + Mevenide.getResourceString("DynamicPreferencePage.Description"));
+        descriptionGroup.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+        descriptionGroup.setLayout(new GridLayout());
+
+        Text textDescription = new Text(descriptionGroup, SWT.READ_ONLY | SWT.MULTI);
+        textDescription.setText(pluginDescription != null ? pluginDescription : Mevenide.getResourceString("DynamicPreferencePage.nodescription")); //$NON-NLS-1$
+        GridData data = new GridData(GridData.FILL_HORIZONTAL);
+        textDescription.setLayoutData(data);
+        
+        tabItem.setControl(area);
+    }
+
+    private void createCategoryComposite(TabFolder tabFolder, PluginCategory category) {
+        TabItem tabItem = new TabItem(tabFolder, SWT.NULL);
+        tabItem.setText(category.getName());
+        
+
+        Composite area = new Composite(tabFolder, SWT.NULL);
+        GridLayout layout = new GridLayout();
+        layout.marginHeight = 10;
+        layout.marginWidth = 10;
+        area.setLayout(layout);
+        
+        Group propertiesGroup = createPropertyGroup(area, category.getName());
+        Composite groupIndirectionComposite = new Composite(propertiesGroup, SWT.NULL);
+        GridLayout groupIndirectionLayout = new GridLayout();
+        GridData groupIndirectionData = new GridData(GridData.FILL_BOTH);
+        groupIndirectionComposite.setLayoutData(groupIndirectionData);
+        
+        List properties = category.getProperties();
+        
+        if( properties != null && properties.size() > 0 ) {
+            for ( Iterator it = properties.iterator(); it.hasNext(); ) {
+		        PluginProperty pluginProperty = (PluginProperty) it.next();
+                StringFieldEditor editor = createPluginPropertyEditor(groupIndirectionComposite, pluginProperty);
+                editors.put(pluginProperty, editor);
+            }
+        }
+        else {
+            Text noPropertyWarningText = new Text(propertiesGroup, SWT.READ_ONLY);
+            noPropertyWarningText.setText(Mevenide.getResourceString("DynamicPreferencePage.noproperty")); //$NON-NLS-1$
+            GridData warningData = new GridData(GridData.FILL_BOTH);
+            noPropertyWarningText.setLayoutData(warningData);
+        }
+        
+        tabItem.setControl(area);
+    }
+
+    private Group createPropertyGroup(Composite composite, String categoryName) {
+        Group propertiesGroup = new Group(composite, SWT.NULL);
+        GridLayout layout = new GridLayout();
+        layout.numColumns = 1;
+        layout.marginHeight = 10;
+        layout.marginWidth = 10;
+        propertiesGroup.setLayout(layout);
+        GridData groupData = new GridData(GridData.FILL_HORIZONTAL);
+        propertiesGroup.setLayoutData(groupData);
+        propertiesGroup.setText(Mevenide.getResourceString("DynamicPreferencePage.Configuration", categoryName)); //$NON-NLS-1$
+        return propertiesGroup;
+    }
+
+    private StringFieldEditor createPluginPropertyEditor(Composite composite, final PluginProperty pluginProperty) {
+        
+        String propertyName = pluginProperty.getName(); 
+        String propertyDefault = pluginProperty.getDefault();
+        String propertyLabel = pluginProperty.getLabel();
+        String propertyDescription = pluginProperty.getDescription();
+        String propertyType = pluginProperty.getType();
+        String pageId = pluginProperty.getPageId();
+        
+        StringFieldEditor editor = new StringFieldEditor(pageId + DynamicPreferencesManager.SEPARATOR + propertyName, propertyLabel, composite);
+        editor.fillIntoGrid(composite, 2);
+        
+        editor.setPreferenceStore(preferencesManager.getPreferenceStore());
+        editor.load();
+
+        editor.setEmptyStringAllowed(!pluginProperty.isRequired());
+        editor.setErrorMessage(Mevenide.getResourceString("DynamicPreferencePage.Empty.NotAllowed", pluginProperty.getLabel())); //$NON-NLS-1$
+        
+        ModifyListener editorListener = new ModifyListener() {
+            public void modifyText(ModifyEvent arg0) {
+                dirtyProperties.add(pluginProperty);
+                updateApplyButton();
+                getContainer().updateButtons();
+            }
+        };
+
+        String toolTip = propertyName + " : " +  //$NON-NLS-1$
+        				(!StringUtils.isNull(propertyDescription) ? 
+        				        					propertyDescription : 
+        				        					Mevenide.getResourceString("DynamicPreferencePage.property.nodescription")); //$NON-NLS-1$
+        editor.getLabelControl(composite).setToolTipText(toolTip);
+        
+        if ( StringUtils.isNull(editor.getStringValue()) && 
+                !StringUtils.isNull(propertyDefault) ) {
+            editor.setStringValue(propertyDefault);
+        }
+        
+        editor.getTextControl(composite).addModifyListener(editorListener);
+        
+        return editor;
+    }
+
+    public void init(IWorkbench workbench) {
+    }
+	
+    public List getCategories() {
+        return categories;
+    }
+    
+    public void setCategories(List properties) {
+        this.categories = properties;
+    }
+    
+    protected void performApply() {
+        performOk();
+    }
+    
+    protected void performDefaults() {
+        for (Iterator it = editors.keySet().iterator(); it.hasNext(); ) {
+            PluginProperty pluginProperty = (PluginProperty) it.next();
+            StringFieldEditor editor = (StringFieldEditor) editors.get(pluginProperty);
+            editor.setStringValue(pluginProperty.getDefault());
+        }
+        super.performDefaults();
+    }
+    
+    public boolean isValid() {
+        boolean valid = true;
+        setErrorMessage(null);
+        for (Iterator it = editors.keySet().iterator(); it.hasNext(); ) {
+            PluginProperty pluginProperty = (PluginProperty) it.next();
+            StringFieldEditor editor = (StringFieldEditor) editors.get(pluginProperty);
+            if ( StringUtils.isNull(editor.getStringValue()) && pluginProperty.isRequired() && !StringUtils.isNull(pluginProperty.getDefault())) {
+                setErrorMessage(editor.getErrorMessage());
+                valid = false;
+                break;
+            }
+        }
+        return valid;
+    }
+    
+    public boolean performOk() {
+        for (Iterator it = editors.keySet().iterator(); it.hasNext(); ) {
+            PluginProperty pluginProperty = (PluginProperty) it.next();
+            if ( dirtyProperties.contains(pluginProperty) ) {
+                StringFieldEditor editor = (StringFieldEditor) editors.get(pluginProperty);
+	            String pageId = pluginProperty.getPageId();
+	            String propertyName = pluginProperty.getName();
+	            if ( !StringUtils.relaxEqual(pluginProperty.getDefault(), editor.getStringValue()) ) {
+	                preferencesManager.setValue(pageId + DynamicPreferencesManager.SEPARATOR + propertyName, editor.getStringValue());
+	            }
+	            else {
+	                preferencesManager.remove(pageId + DynamicPreferencesManager.SEPARATOR + propertyName);
+	            }
+            }
+        }
+        return preferencesManager.store();
+    }
+    
+    public String getPluginDescription() {
+        return pluginDescription;
+    }
+    
+    public void setPluginDescription(String pluginDescription) {
+        this.pluginDescription = pluginDescription;
+    }
+    
+    public String getPluginName() {
+        return pluginName;
+    }
+    
+    public void setPluginName(String pageName) {
+        this.pluginName = pageName;
+    }
+}
