e123743a36bbc0d7fd95ad2957fd08be4ea8ca65
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2004-07-14 19:56:20 +0000

    format MavenJob (remove tabs) and externalize Job name

--- trunk/mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/launch/configuration/MavenJob.java
+++ trunk/mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/launch/configuration/MavenJob.java
@@ -1,17 +1,18 @@
-/* ==========================================================================
+/*
+ * ==========================================================================
  * Copyright 2003-2004 Apache Software Foundation
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  * =========================================================================
  */
 package org.mevenide.ui.eclipse.launch.configuration;
@@ -33,74 +34,68 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.externaltools.internal.model.IExternalToolConstants;
 import org.mevenide.ui.eclipse.Mevenide;
 
-
-/**  
+/**
  * 
- * @author <a href="mailto:rhill2@free.fr">Gilles Dodinet</a>
+ * @author <a href="mailto:rhill2@free.fr">Gilles Dodinet </a>
  * @version $Id$
- * 
+ *  
  */
 public class MavenJob extends Job {
-    
+
     private static final Log log = LogFactory.getLog(MavenJob.class);
-    
     private ILaunchConfiguration configuration;
     private IPath basedir;
     private boolean showDialog;
     private Shell shell;
-    
+
     public MavenJob(Shell shell, ILaunchConfiguration configuration, IPath basedir, boolean showDialog) {
-        super("Maven Runner");
+        super(Mevenide.getResourceString("MavenJob.Name")); //$NON-NLS-1$
         this.configuration = configuration;
         this.basedir = basedir;
         this.showDialog = showDialog;
         this.shell = shell;
         setPriority(Job.BUILD);
     }
-    
-    transient IStatus status = null;
-    
+
     protected IStatus run(IProgressMonitor monitor) {
         Runnable mavenRunner = new Runnable() {
             public void run() {
                 runMaven();
             }
-        }; 
+        };
         PlatformUI.getWorkbench().getDisplay().asyncExec(mavenRunner);
-        return new Status(Status.OK, "org.mevenide.ui", 0, Mevenide.getResourceString("MavenRunner.Done"), null); //$NON-NLS-1$ //$NON-NLS-2$
+        return new Status(Status.OK, "org.mevenide.ui", 0, Mevenide.getResourceString("MavenJob.Done"), null); //$NON-NLS-1$ //$NON-NLS-2$
     }
 
     private void runMaven() {
-        if (configuration != null) {
-			if ( showDialog ) {
-				//IStatus status = new Status(IStatus.INFO, Mevenide.PLUGIN_ID, 0, "", null); //$NON-NLS-1$
-				int val = DebugUITools.openLaunchConfigurationDialog(shell, configuration, IExternalToolConstants.ID_EXTERNAL_TOOLS_LAUNCH_GROUP, null);
-				if ( val == Window.CANCEL ) {
-					try {
-						configuration.delete();
-					}
-					catch ( Exception e ) {
-						log.debug("Exception while cancelling launch : ", e ); //$NON-NLS-1$
-					}
-				}
-			}
-			
-			String newName= DebugPlugin.getDefault().getLaunchManager().generateUniqueLaunchConfigurationNameFrom(configuration.getName());
-			try {
-			    if ( configuration.exists() ) {
-			        configuration = configuration.copy(newName);
-			    }
-				if (showDialog) {
-					configuration = configuration.getWorkingCopy().doSave();
-				}
-				else {
-				    //caused config to be launched twice (see MavenLaunchDelegate)
-				    DebugUITools.launch(configuration, ILaunchManager.RUN_MODE);
-				}
-			}
-			catch (Exception e) {
-				log.error("Unable to copy configuration due to : ", e); //$NON-NLS-1$
-			}
-		}
+        if ( configuration != null ) {
+            if ( showDialog ) {
+                int val = DebugUITools.openLaunchConfigurationDialog(shell, configuration, IExternalToolConstants.ID_EXTERNAL_TOOLS_LAUNCH_GROUP, null);
+                if ( val == Window.CANCEL ) {
+                    try {
+                        configuration.delete();
+                    }
+                    catch (Exception e) {
+                        log.debug("Exception while cancelling launch : ", e); //$NON-NLS-1$
+                    }
+                }
+            }
+            String newName = DebugPlugin.getDefault().getLaunchManager().generateUniqueLaunchConfigurationNameFrom(
+                    configuration.getName());
+            try {
+                if ( configuration.exists() ) {
+                    configuration = configuration.copy(newName);
+                }
+                if ( showDialog ) {
+                    configuration = configuration.getWorkingCopy().doSave();
+                }
+                else {
+                    DebugUITools.launch(configuration, ILaunchManager.RUN_MODE);
+                }
+            }
+            catch (Exception e) {
+                log.error("Unable to copy configuration due to : ", e); //$NON-NLS-1$
+            }
+        }
     }
 }
