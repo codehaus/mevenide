31826bc679ec0e97f4768a672afac2a7927f5e92
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2004-07-15 20:17:41 +0000

    first try for the provider plugin

---
+++ maven-plugins/maven-provider-plugin/src/main/java/org/codehaus/mevenide/provider/DefaultElementHandler.java
@@ -0,0 +1,107 @@
+/* ==========================================================================
+ * Copyright 2003-2004 Apache Software Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * =========================================================================
+ */
+package org.codehaus.mevenide.provider;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Stack;
+import org.mevenide.properties.Comment;
+import org.mevenide.properties.Element;
+import org.mevenide.properties.KeyValuePair;
+
+
+/**  
+ * 
+ * @author <a href="mailto:rhill2@free.fr">Gilles Dodinet</a>
+ * @version $Id$
+ * 
+ */
+public class DefaultElementHandler implements IElementHandler {
+
+    //stack of the comments found so far
+    private Stack contextStack = new Stack(); 
+
+    //commentStack is copied to backupStack before being cleared
+    //thus giving us a chance to categorize properties
+    private Stack backupStack;
+    
+    //map hashed by the KeyValuePair
+    private Map propertyMap = new HashMap();
+    
+    //map hashed by the KeyValuePair - should surely be possible to compute categories from # ---- comments
+    //private Map categoryMap = new HashMap();
+    
+    
+    public void handle(Element element) {
+        if ( element instanceof Comment ) {
+            handleComment((Comment) element);
+        }
+        if ( element instanceof KeyValuePair ) {
+            handleKeyValuePair((KeyValuePair) element);
+        }
+    }
+
+    private void handleKeyValuePair(KeyValuePair pair) {
+        String associatedComment = ""; //$NON-NLS-1$
+        while ( !contextStack.isEmpty() ) {
+            Comment comment = (Comment) contextStack.pop();
+            if ( comment.getValue().length() >= 2 ) {
+                associatedComment = comment.getValue().substring(1) + associatedComment;
+            }
+        }
+        propertyMap.put(pair, associatedComment);
+    }
+
+
+    private void handleComment(Comment comment) {
+        if ( comment.getValue().startsWith("# ---") ) { //$NON-NLS-1$
+            if ( contextStack.isEmpty() ) {
+                contextStack.push(comment);
+            }
+            else {
+                contextStack.clear();
+            }
+        }
+        else {
+            contextStack.push(comment);
+        }
+    }
+
+//    <!ELEMENT plugin (property*)>
+//    <!ATTLIST plugin name>
+//    <!ATTLIST property 
+//              name CDATA #REQUIRED
+//              label CDATA #IMPLIED
+//              default CDATA #IMPLIED 
+//              required (true|false) "false"
+//              description CDATA #IMPLIED 
+//              validator CDATA #IMPLIED
+//              validate (true|false) "true" 
+//              scope (project|global) "project"
+//    			category CDATA #IMPLIED> 
+    public String getXmlDescription() {
+        
+        for (Iterator it = propertyMap.keySet().iterator(); it.hasNext();) {
+            KeyValuePair pair = (KeyValuePair) it.next();
+            String description = (String) propertyMap.get(pair);
+            //System.err.println(pair + "=>" + description);
+        }
+        
+        return null;
+    }
+}
