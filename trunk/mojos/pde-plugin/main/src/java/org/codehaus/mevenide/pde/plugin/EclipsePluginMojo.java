8fea6118eeb66be4592fe2baff68bc5984438676
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2004-10-01 21:57:35 +0000

    grab parameters. todo :   o see how to factorize parameters   o look at validation strategies   o check that Long parameters behave correctly   o look at AbstractPluginTestCase (or something like that)

--- trunk/mojos/pde-plugin/main/src/java/org/codehaus/mevenide/pde/plugin/EclipsePluginMojo.java
+++ trunk/mojos/pde-plugin/main/src/java/org/codehaus/mevenide/pde/plugin/EclipsePluginMojo.java
@@ -16,19 +16,66 @@
  */
 package org.codehaus.mevenide.pde.plugin;
 
+import java.io.File;
 import org.apache.maven.plugin.PluginExecutionRequest;
 import org.apache.maven.plugin.PluginExecutionResponse;
 import org.codehaus.mevenide.pde.EclipseArtifactMojo;
 
 
 /**  
+ * @open can tags be inherited ?
  * 
+ * @goal plugin
+ * @description builds an Eclipse plugin
+ * @parameter name="eclipseHome" 
+ *            type="String" 
+ *            required="true" 
+ *            validator="" 
+ *            expression="#maven.pde.eclipse.home" 
+ *            description="Location of Eclipse home directory"
+ * @parameter name="eclipseConfigurationFolder" 
+ *            type="String" 
+ *            required="false" 
+ *            validator="" 
+ *            expression="#maven.pde.eclipse.configuration" 
+ *            description="Eclipse configuration folder"
+ * @parameter name="maxBuildId" 
+ *            type="Long" 
+ *            required="false" 
+ *            validator="" 
+ *            expression="#maven.pde.maxBuildId" 
+ *            description="Max compatible platform buildId"
+ * @parameter name="minBuildId" 
+ *            type="Long" 
+ *            required="false" 
+ *            validator="" 
+ *            expression="#maven.pde.minBuildId" 
+ *            description="Min compatible platform buildId"           
+ * @parameter name="outputDirectory" 
+ *            type="String" 
+ *            required="true" 
+ *            validator="" 
+ *            expression="#maven.build.dir/eclipse" 
+ *            description="Directory where the generated artifact will be outputted"
+ *            
  * @author <a href="mailto:rhill2@free.fr">Gilles Dodinet</a>
  * @version $Id$
  * 
  */
 public class EclipsePluginMojo extends EclipseArtifactMojo {
     
-    public void execute(PluginExecutionRequest arg0, PluginExecutionResponse arg1) throws Exception {
+    public void execute(PluginExecutionRequest request, PluginExecutionResponse response) throws Exception {
+        String eclipseHomeLocation = (String) request.getParameter("eclipseHome");
+        setEclipseHome(new File(eclipseHomeLocation));
+        
+        String eclipseConfigurationFolder = (String) request.getParameter("eclipseConfigurationFolder");
+        setConfigurationFolder(new File(eclipseConfigurationFolder));
+        
+        long maxBuildId = ((Long) request.getParameter("maxBuildId")).longValue();
+        long minBuildId = ((Long) request.getParameter("minBuildId")).longValue();
+        
+        checkBuildId(minBuildId, maxBuildId);
+        
+        String outputDirectory = (String) request.getParameter("outputDirectory");
     }
 }
