e156d37705217258672ac0934dbe705d54725286
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2004-11-07 16:34:59 +0000

    using the error handling hack from mevenide-config, resolve the MEVENIDE-128 issue.

--- branches/CONFIG_REWRITE/mevenide-netbeans-project/src/java/org/mevenide/netbeans/project/nodes/MavenProjectNode.java
+++ branches/CONFIG_REWRITE/mevenide-netbeans-project/src/java/org/mevenide/netbeans/project/nodes/MavenProjectNode.java
@@ -20,6 +20,7 @@ import java.awt.Image;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import javax.swing.Action;
+import javax.swing.ImageIcon;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -50,6 +51,7 @@ public class MavenProjectNode extends AbstractNode {
         project.addPropertyChangeListener(new PropertyChangeListener() {
             public void propertyChange(PropertyChangeEvent event) {
                 fireDisplayNameChange(null, getDisplayName());
+                fireIconChange();
             }
         });
 //        setIconBase("org/mevenide/netbeans/projects/resources/MavenIcon");
@@ -61,20 +63,14 @@ public class MavenProjectNode extends AbstractNode {
         return project.getDisplayName();
     }
     
-    public Image getIcon(int param)
-    {
-        if (icon == null) {
-            icon = Utilities.loadImage("org/mevenide/netbeans/project/resources/MavenIcon.gif"); //NOI18N
-        }
-        return icon;
+    public Image getIcon(int param) {
+        //HACK
+        return ((ImageIcon)project.getProjectInfo().getIcon()).getImage();
     }
     
-    public Image getOpenedIcon(int param)
-    {
-        if (icon == null) {
-            icon = Utilities.loadImage("org/mevenide/netbeans/project/resources/MavenIcon.gif"); //NOI18N
-        }
-        return icon;
+    public Image getOpenedIcon(int param) {
+        //HACK
+        return ((ImageIcon)project.getProjectInfo().getIcon()).getImage();
     }
     
     public javax.swing.Action[] getActions(boolean param) {
@@ -109,65 +105,9 @@ public class MavenProjectNode extends AbstractNode {
         return toReturn;
     }
 
-//    protected Sheet createSheet() {
-//        Sheet sheet = super.createSheet();
-//        Sheet.Set set = sheet.get(SHEET_MAVEN_PROPS);
-//        if (set == null) {
-//            set = createMavenPropsSet();
-//            sheet.put(set);
-//        }
-//        MavenProjectCookie cook = (MavenProjectCookie)this.getCookie(MavenProjectCookie.class);
-//        sheetCreated = true;
-//        set.put(cook.getProperties());
-//        return sheet;
-//    }
-//    
-//    private static class DependenciesNode extends AbstractNode
-//    {
-//        DataObject obj;
-//        public DependenciesNode(MavenProjectDataObject obj, ArtifactCookie cook)
-//        {
-//            super(new DependencyChildren(cook));
-//            setName("Dependencies");
-//            setDisplayName("Dependencies");
-//            setIconBase("org/mevenide/ui/netbeans/loader/jars");//NOI18N
-//            this.obj = obj;
-//        }
-//        
-//        public Cookie getCookie(Class clazz)
-//        {
-//            if (ArtifactCookie.class.isAssignableFrom(clazz))
-//            {
-//                return obj.getCookie(clazz);
-//            }
-//            return  super.getCookie(clazz);
-//        }
-//        
-//        public SystemAction[] getActions()
-//        {
-//            SystemAction[] actions = new SystemAction[]
-//            {
-//                SystemAction.get(MountDependenciesAction.class),
-//                null,
-//                SystemAction.get(PropertiesAction.class)
-//            };
-//            return actions;
-//        }        
-//        
-//    }
-    
-//    /** 
-//    * Convenience method to create new sheet set named MavenProjectNode.SHEET_MAVEN_PROPS.
-//    * @return a new properties sheet set
-//    */
-//    public static final Sheet.Set createMavenPropsSet () {
-//        Sheet.Set ps = new Sheet.Set ();
-//        ps.setName(SHEET_MAVEN_PROPS);
-//        ps.setDisplayName(NbBundle.getMessage(MavenProjectNode.class, "MavenProjectNode.sheetName"));
-//        ps.setShortDescription(NbBundle.getMessage(MavenProjectNode.class, "MavenProjectNode.sheetDesc"));
-//        return ps;
-//    }
-//    
-//    // Don't use getDefaultAction(); just make that first in the data loader's getActions list
-    
+    public String getShortDescription() {
+        return project.getShortDescription();
+    }
+
+
 }
