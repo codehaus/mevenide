bd84c7b578a8aa8fbebff2f61ae89ff2c3833996
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2005-04-24 19:42:27 +0000

    initial j2ee support for netbeans, is based on netbeans 4.1 and cargo 0.5

---
+++ mevenide-netbeans-j2ee/src/java/org/mevenide/netbeans/j2ee/J2eeActions.java
@@ -0,0 +1,43 @@
+/* ==========================================================================
+ * Copyright 2003-2004 Mevenide Team
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * =========================================================================
+ */
+
+package org.mevenide.netbeans.j2ee;
+
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import org.mevenide.netbeans.api.project.AdditionalActionsProvider;
+import org.mevenide.netbeans.j2ee.deploy.DeployAction;
+import org.mevenide.netbeans.project.MavenProject;
+
+/**
+ *
+ * @author  Milos Kleint (mkleint@codehaus.org)
+ */
+
+public class J2eeActions implements AdditionalActionsProvider {
+    
+    /** Creates a new instance of J2EEActions */
+    public J2eeActions() {
+    }
+
+    public Action[] createPopupActions(MavenProject project) {
+        return new Action[] {new DeployAction(project)};
+    }
+    
+
+}
