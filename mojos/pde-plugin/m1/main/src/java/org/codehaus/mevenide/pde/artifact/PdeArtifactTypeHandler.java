2e478d751826ca1a41a52162f98b5bae4a1711ac
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2005-04-29 21:06:34 +0000

    extract most of pde:install into java classes

---
+++ mojos/pde-plugin/m1/main/src/java/org/codehaus/mevenide/pde/artifact/PdeArtifactTypeHandler.java
@@ -0,0 +1,36 @@
+package org.codehaus.mevenide.pde.artifact;
+
+import org.apache.commons.jelly.JellyContext;
+import org.apache.commons.jelly.JellyTagException;
+import org.apache.maven.MavenException;
+import org.apache.maven.project.Project;
+import org.apache.maven.repository.ArtifactTypeHandler;
+
+public class PdeArtifactTypeHandler implements ArtifactTypeHandler {
+
+	private JellyContext context;
+	
+	public String constructRepositoryFullPath(String type, Project project, String version) throws MavenException {
+		PdeArtifactNameTag tag = new PdeArtifactNameTag();
+		tag.setProject(project);
+		try {
+			tag.setContext(context);
+		}
+		catch (JellyTagException e) {
+			throw new MavenException("Unable to set context", e);
+		}
+		String fileName = tag.getArtifactName();
+		
+		return (constructRepositoryDirectoryPath(type, project) + fileName + ".jar").toString();
+	}
+
+	public String constructRepositoryDirectoryPath(String type, Project project) throws MavenException {
+		return (project.getArtifactDirectory() + "/pdes/").toString();
+	}
+
+	public void setContext(JellyContext context) {
+		this.context = context;
+	}
+	
+
+}
