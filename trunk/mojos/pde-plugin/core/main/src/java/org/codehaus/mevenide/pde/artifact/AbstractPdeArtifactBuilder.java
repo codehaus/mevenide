27898e0de0888cef7b2aa83115f229ad02149674
gdodinet <gdodinet@c6c22482-0310-0410-b13f-f1d71ac210c7>
2005-05-01 13:37:02 +0000

    o fix includes of <nondefaulttarget> libs o start feature work o disable sourcespresent check as it conflicts with feature processing

--- trunk/mojos/pde-plugin/core/main/src/java/org/codehaus/mevenide/pde/artifact/AbstractPdeArtifactBuilder.java
+++ trunk/mojos/pde-plugin/core/main/src/java/org/codehaus/mevenide/pde/artifact/AbstractPdeArtifactBuilder.java
@@ -1,13 +1,44 @@
 package org.codehaus.mevenide.pde.artifact;
 
+import java.io.File;
+
+import org.apache.maven.project.MavenProject;
 import org.codehaus.mevenide.pde.PdePluginException;
 
 public abstract class AbstractPdeArtifactBuilder implements IPdeArtifactBuilder {
-
+	
+	/** base directory */    
+    protected File basedir;
+    
+    /** project under construction */
+	protected MavenProject project;
+	
+	/** artifactName referencing the primary artifact */
+	protected String artifactName;
+	
+	/** name of generated zip file */
+	protected String artifact;
+	
+	/** helper */
+	protected PdeBuilderHelper helper;
+	
 	public void build() throws PdePluginException {
-	    updateDescriptor();
 	    collectDependencies();
+	    updateDescriptor();
 		createArchive();
     }
+	
+	public File getBasedir() { return basedir; }
+    public void setBasedir(File basedir) { this.basedir = basedir; }
+	
+	public MavenProject getProject() { return project; }
+    public void setProject(MavenProject project) { 
+		this.project = project; 
+		helper = new PdeBuilderHelper(project);
+	}
+	
+	public void setArtifactName(String name) { this.artifactName = name; }
 
+	public String getArtifact() { return artifact; }
+    public void setArtifact(String artifact) { this.artifact = artifact; }
 }
