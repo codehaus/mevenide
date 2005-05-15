91f6fa08760222ea4151861650f24cab1884cc78
arik <arik@c6c22482-0310-0410-b13f-f1d71ac210c7>
2005-05-15 19:36:47 +0000

    solved the focus issues, and moved the focus handling into the superclass (AbstractPomLayerPanel) so that future POM panels will utilize it as well. added some documentation

--- trunk/mevenide-idea/src/main/java/org/mevenide/idea/editor/pom/ui/layer/AbstractPomLayerPanel.java
+++ trunk/mevenide-idea/src/main/java/org/mevenide/idea/editor/pom/ui/layer/AbstractPomLayerPanel.java
@@ -18,23 +18,69 @@ package org.mevenide.idea.editor.pom.ui.layer;
 
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
-import org.jdom.Element;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.mevenide.idea.Res;
 
 import javax.swing.*;
+import java.awt.event.FocusListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.HierarchyListener;
+import java.awt.event.HierarchyEvent;
+import java.awt.*;
 
 /**
+ * A base class for all POM editing panels.
+ *
+ * <p>Automatically keeps track of the current focusable component. This is a bug
+ * fix for IDEA - when a FileEditor has a panel with multiple fields, switching
+ * to another editor and returning returns the focus back to the first component.
+ * This class fixes that.</p>
+ *
+ * <p>Also, this class provides a {@link Log} instance, the correct resource bundle
+ * and the project and document this panel edits.</p>
+ *
  * @author Arik
  */
 public abstract class AbstractPomLayerPanel extends JPanel {
+    /**
+     * Logging.
+     */
     protected final Log LOG;
+
+    /**
+     * Resources.
+     */
     protected final Res RES;
+
+    /**
+     * The IDEA project the POM file, that this POM panel edits, belongs to.
+     */
     protected final Project project;
+
+    /**
+     * The POM file's IDEA document. Changes are propagated from the user interface
+     * into this object.
+     */
     protected final Document editorDocument;
 
+    /**
+     * The current focused component.
+     */
+    protected Component focusedComponent = null;
+
+    /**
+     * The focus tracker. This listener is attached to every focusable component
+     * added to this panel.
+     */
+    private final FocusTracker focusTracker = new FocusTracker();
+
+    /**
+     * Creates an instance for the given project and POM document.
+     *
+     * @param pProject the project that the POM file belongs to
+     * @param pPomDocument the POM file's IDEA document
+     */
     protected AbstractPomLayerPanel(final Project pProject,
                                     final Document pPomDocument) {
 
@@ -43,5 +89,48 @@ public abstract class AbstractPomLayerPanel extends JPanel {
 
         project = pProject;
         editorDocument = pPomDocument;
+
+        //
+        //this will make sure that when the editor is reselected, the correct
+        //component will receive the focus
+        //
+        this.addHierarchyListener(new HierarchyListener() {
+            public void hierarchyChanged(HierarchyEvent e) {
+                if(focusedComponent != null) {
+                    SwingUtilities.invokeLater(new Runnable() {
+                        public void run() {
+                            focusedComponent.requestFocusInWindow();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    /**
+     * Overriden to attach our focus tracker to the new component, if it's
+     * focusable.
+     *
+     * @param comp the component
+     * @param constraints ignored (simply passed to the super implementation of this method)
+     * @param index ignored (simply passed to the super implementation of this method)
+     */
+    @Override protected void addImpl(Component comp, Object constraints, int index) {
+        super.addImpl(comp, constraints, index);
+        if(comp.isFocusable())
+            comp.addFocusListener(focusTracker);
+    }
+
+    /**
+     * A focus listener which keeps track of the currently focused component.
+     */
+    private class FocusTracker implements FocusListener {
+
+        public void focusGained(FocusEvent e) {
+            focusedComponent = e.getComponent();
+        }
+
+        public void focusLost(FocusEvent e) {
+        }
     }
 }
