e27fd764c54b2177e66889cc59294c3d98df4fc7
arik <arik@c6c22482-0310-0410-b13f-f1d71ac210c7>
2005-06-03 14:01:17 +0000

    fix renderer and ui settings after model refresh

--- mevenide-idea/src/main/java/org/mevenide/idea/repository/RepositoryTree.java
+++ mevenide-idea/src/main/java/org/mevenide/idea/repository/RepositoryTree.java
@@ -10,11 +10,17 @@ import com.intellij.util.ui.Tree;
 public class RepositoryTree extends Tree {
 
     public RepositoryTree() {
-
+        setShowsRootHandles(true);
+        setRootVisible(false);
+        setCellRenderer(new RepositoryTreeCellRenderer());
     }
 
     public RepositoryTree(final TreeModel pModel) {
         super(pModel);
+    }
+
+    @Override public final void setModel(TreeModel newModel) {
+        super.setModel(newModel);
         setShowsRootHandles(true);
         setRootVisible(false);
         setCellRenderer(new RepositoryTreeCellRenderer());
