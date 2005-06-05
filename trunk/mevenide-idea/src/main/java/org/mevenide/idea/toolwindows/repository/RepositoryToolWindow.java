1989165a8e6748a7c4d46c5608aac550990a49d6
arik <arik@c6c22482-0310-0410-b13f-f1d71ac210c7>
2005-06-05 03:44:46 +0000

    added download support to repository browser

--- trunk/mevenide-idea/src/main/java/org/mevenide/idea/toolwindows/repository/RepositoryToolWindow.java
+++ trunk/mevenide-idea/src/main/java/org/mevenide/idea/toolwindows/repository/RepositoryToolWindow.java
@@ -3,11 +3,14 @@ package org.mevenide.idea.toolwindows.repository;
 import java.awt.BorderLayout;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import javax.swing.JPanel;
 import javax.swing.JTree;
+import javax.swing.ListSelectionModel;
 import javax.swing.tree.TreePath;
 
 import com.intellij.ide.CommonActionsManager;
@@ -15,16 +18,23 @@ import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.ActionToolbar;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.ModuleListener;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.SelectFromListDialog;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowAnchor;
 import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.ui.ScrollPaneFactory;
+import org.mevenide.context.IQueryContext;
+import org.mevenide.environment.ILocationFinder;
 import org.mevenide.idea.Res;
+import org.mevenide.idea.module.ModuleLocationFinder;
 import org.mevenide.idea.module.ModuleSettings;
+import org.mevenide.idea.repository.ArtifactDownloadManager;
 import org.mevenide.idea.repository.RepositoryTree;
 import org.mevenide.idea.repository.RepositoryTreeExpander;
 import org.mevenide.idea.repository.RepositoryUtils;
@@ -32,7 +42,10 @@ import org.mevenide.idea.repository.actions.RefreshRepoTreeAction;
 import org.mevenide.idea.repository.model.RepoTreeNode;
 import org.mevenide.idea.repository.model.RepositoryTreeModel;
 import org.mevenide.idea.util.actions.AbstractAnAction;
+import org.mevenide.idea.util.ui.UIUtils;
 import org.mevenide.idea.util.ui.images.Icons;
+import org.mevenide.properties.IPropertyResolver;
+import org.mevenide.repository.RepoPathElement;
 
 /**
  * @author Arik
@@ -167,10 +180,100 @@ public class RepositoryToolWindow extends JPanel implements PropertyChangeListen
         }
 
         public void actionPerformed(final AnActionEvent pEvent) {
+            final Module selectedModule = getModuleForDownload();
+            if(selectedModule == null)
+                return;
+
+            //
+            //locate callbacks
+            //
+            final ILocationFinder finder = new ModuleLocationFinder(selectedModule);
+            final ModuleSettings settings = ModuleSettings.getInstance(selectedModule);
+            final IQueryContext queryContext = settings.getQueryContext();
+            final IPropertyResolver resolver = queryContext.getResolver();
+
+            //
+            //prepare list of path elements to download
+            //
+            final RepoTreeNode[] selectedElements = getSelectedElements();
+            final List<RepoPathElement> pathElements = new ArrayList<RepoPathElement>(selectedElements.length);
+            for (RepoTreeNode node : selectedElements) {
+                final RepoPathElement[] nodeElements = node.getPathElements();
+                for (RepoPathElement pathElement : nodeElements)
+                    pathElements.add(pathElement);
+            }
+
+            final Runnable downloader = new Runnable() {
+                public void run() {
+                    final RepoPathElement[] buffer = new RepoPathElement[pathElements.size()];
+                    final RepoPathElement[] pathElementsArray = pathElements.toArray(buffer);
+                    try {
+                        final ArtifactDownloadManager downloadMgr = ArtifactDownloadManager.getInstance();
+                        downloadMgr.downloadArtifact(finder, resolver, pathElementsArray);
+                    }
+                    catch (IOException e) {
+                        UIUtils.showError(selectedModule, e);
+                    }
+                }
+            };
+
+            final Application app = ApplicationManager.getApplication();
+            app.runProcessWithProgressSynchronously(downloader,
+                                                    "Downloading...",
+                                                    true, 
+                                                    project);
         }
 
         @Override public void update(final AnActionEvent pEvent) {
             pEvent.getPresentation().setEnabled(getSelectedElements().length > 0);
+        }
+
+        private Module getModuleForDownload() {
+            final Module selectedModule;
+            final Module[] modules = ModuleManager.getInstance(project).getModules();
+            if (modules.length == 1)
+                selectedModule = modules[0];
+            else {
+                final Set<Module> localRepos = new HashSet<Module>(modules.length);
+                for (Module module : modules) {
+                    final ModuleSettings settings = ModuleSettings.getInstance(module);
+                    final IQueryContext context = settings.getQueryContext();
+                    if (context == null)
+                        continue;
+
+                    localRepos.add(module);
+                }
+
+                if (localRepos.size() > 1) {
+                    final SelectFromListDialog dlg = new SelectFromListDialog(
+                        project,
+                        localRepos.toArray(),
+                        new SelectFromListDialog.ToStringAspect() {
+                            public String getToStirng(Object obj) {
+                                final Module module = (Module) obj;
+                                final ILocationFinder finder = new ModuleLocationFinder(module);
+                                final String repo = finder.getMavenLocalRepository();
+                                return module.getName() + " - Local repository at " + repo;
+                            }
+                        },
+                        "Please select a local repository",
+                        ListSelectionModel.SINGLE_SELECTION);
+
+                    dlg.show();
+                    if (!dlg.isOK())
+                        return null;
+
+                    final Object[] selection = dlg.getSelection();
+                    if (selection.length == 0)
+                        return null;
+
+                    selectedModule = (Module) selection[0];
+                }
+                else
+                    selectedModule = modules[0];
+            }
+
+            return selectedModule;
         }
     }
 }
