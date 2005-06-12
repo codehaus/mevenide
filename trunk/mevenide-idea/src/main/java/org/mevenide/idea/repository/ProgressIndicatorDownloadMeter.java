b31f7f3638bb46132a848e77615e3cbcfa18d724
arik <arik@c6c22482-0310-0410-b13f-f1d71ac210c7>
2005-06-12 12:52:21 +0000

    fixed progress meter always stalling at zero progress due to calculating fraction on an integer base - casting to 'double' (...silly eh?)

--- trunk/mevenide-idea/src/main/java/org/mevenide/idea/repository/ProgressIndicatorDownloadMeter.java
+++ trunk/mevenide-idea/src/main/java/org/mevenide/idea/repository/ProgressIndicatorDownloadMeter.java
@@ -3,11 +3,14 @@ package org.mevenide.idea.repository;
 import com.intellij.openapi.progress.ProgressIndicator;
 import org.apache.maven.util.DownloadMeter;
 import org.mevenide.idea.util.IDEUtils;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author Arik
  */
 public class ProgressIndicatorDownloadMeter implements DownloadMeter {
+
+    private AtomicBoolean started = new AtomicBoolean(false);
 
     public void finish(final int pTotal) {
         final ProgressIndicator indicator = IDEUtils.getProgressIndicator();
@@ -17,8 +20,13 @@ public class ProgressIndicatorDownloadMeter implements DownloadMeter {
 
     public void update(final int pComplete, final int pTotal) {
         final ProgressIndicator indicator = IDEUtils.getProgressIndicator();
-        if (indicator != null)
-            indicator.setFraction(pComplete / pTotal);
+        if (indicator != null) {
+            if(started.compareAndSet(false, true)) 
+                indicator.setIndeterminate(false);
+
+            final double fraction = (double)pComplete / (double)pTotal;
+            indicator.setFraction(fraction);
+        }
     }
 
 }
