cdc0f3bd6fae0774193731e51d11577fa99bad17
fdutton <fdutton@c6c22482-0310-0410-b13f-f1d71ac210c7>
2005-06-29 13:18:34 +0000

    Added utility methods.

--- mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/nature/MavenBuilder.java
+++ mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/nature/MavenBuilder.java
@@ -1,5 +1,5 @@
 /* ==========================================================================
- * Copyright 2003-2004 Apache Software Foundation
+ * Copyright 2003-2005 MevenIDE Project
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,18 +14,20 @@
  *  limitations under the License.
  * =========================================================================
  */
+
 package org.mevenide.ui.eclipse.nature;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+
 import org.apache.maven.project.Dependency;
 import org.apache.maven.project.Project;
+import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -35,7 +37,6 @@ import org.mevenide.project.dependency.DependencyUtil;
 import org.mevenide.project.io.ProjectReader;
 import org.mevenide.ui.eclipse.Mevenide;
 
-
 /**  
  * 
  * @author <a href="mailto:rhill2@free.fr">Gilles Dodinet</a>
@@ -43,49 +44,44 @@ import org.mevenide.ui.eclipse.Mevenide;
  * 
  */
 public class MavenBuilder extends IncrementalProjectBuilder {
-    private static final Log log = LogFactory.getLog(MavenBuilder.class);
-    
     public static final String BUILDER_ID = Mevenide.PLUGIN_ID + ".mavenbuilder"; //$NON-NLS-1$
-    
-    
+
     protected IProject[] build(int kind, Map args, IProgressMonitor monitor) throws CoreException {
-        if ( getProject() == null ) {
+        if (getProject() == null) {
             return new IProject[0];
         }
         IResourceDelta d = getDelta(getProject());
-        if ( d != null ) {
-	        List actionDefinitions = Mevenide.getInstance().getActionDefinitionsManager().getDefinitions();
-	        ActionActivator activator = new ActionActivator(actionDefinitions, getProject());
-	        d.accept(activator);
+        if (d != null) {
+            List actionDefinitions = Mevenide.getInstance().getActionDefinitionsManager().getDefinitions();
+            ActionActivator activator = new ActionActivator(actionDefinitions, getProject());
+            d.accept(activator);
         }
         return getMavenRequiredProjects();
     }
-    
+
     private IProject[] getMavenRequiredProjects() {
-        List projects = new ArrayList(); 
+        List projects = new ArrayList();
         try {
-	        IFile file = getProject().getFile("project.xml"); //$NON-NLS-1$
-	        if ( file.exists() ) {
-	            File currentPomFile = file.getLocation().toFile();
-	            Project currentPom = ProjectReader.getReader().read(currentPomFile);
-	            IProject[] wsProjects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
-			    for (int i = 0; i < wsProjects.length; i++) {
-			        IProject wsProject = wsProjects[i];
-			        if ( isDependingUpon(currentPom, wsProject) ) {
-			            projects.add(wsProject);
-			        }
-	            } 
-	        }
-        }
-        catch (Exception e) {
+            IFile file = getProject().getFile("project.xml"); //$NON-NLS-1$
+            if (file.exists()) {
+                File currentPomFile = file.getLocation().toFile();
+                Project currentPom = ProjectReader.getReader().read(currentPomFile);
+                IProject[] wsProjects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+                for (int i = 0; i < wsProjects.length; i++) {
+                    IProject wsProject = wsProjects[i];
+                    if (isDependingUpon(currentPom, wsProject)) {
+                        projects.add(wsProject);
+                    }
+                }
+            }
+        } catch (Exception e) {
             //try/catch in loop in order to be able to process other projects   
             String message = "Unable to check for compute dependent projects"; //$NON-NLS-1$
-            log.error(message, e);
+            Mevenide.displayError("Get Dependent Projects", message, e);
         }
         return (IProject[]) projects.toArray(new IProject[projects.size()]);
     }
 
-    
     private boolean isDependingUpon(Project currentPom, IProject wsProject) {
         boolean explicitDependency = isExplicitelyDependingUpon(currentPom, wsProject);
         boolean implicitDependency = isImplicitelyDependingUpon(currentPom, wsProject);
@@ -95,29 +91,74 @@ public class MavenBuilder extends IncrementalProjectBuilder {
     private boolean isImplicitelyDependingUpon(Project currentPom, IProject wsProject) {
         return false;
     }
-    
+
     private boolean isExplicitelyDependingUpon(Project currentPom, IProject wsProject) {
         try {
             IFile file = wsProject.getFile("project.xml"); //$NON-NLS-1$
-            if ( file.exists() ) {
+            if (file.exists()) {
                 Dependency dependency = ProjectReader.getReader().extractDependency(file.getLocation().toFile());
                 List dependencies = currentPom.getDependencies();
-                if ( dependencies != null ) {
-	                for (int i = 0; i < dependencies.size(); i++) {
-	                    if ( DependencyUtil.areEquals((Dependency) dependencies.get(i), dependency) ) {
-	                        return true;
-	                    }
-	                }
+                if (dependencies != null) {
+                    for (int i = 0; i < dependencies.size(); i++) {
+                        if (DependencyUtil.areEquals((Dependency) dependencies.get(i), dependency)) {
+                            return true;
+                        }
+                    }
                 }
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             String message = "Unable to check project dependencies"; //$NON-NLS-1$
-            log.error(message, e);
+            Mevenide.displayError("Check Dependent Project", message, e);
         }
         return false;
     }
 
+////////////////////////////////////////////////////////////////////////////////
+
+    static final void addToProject(IProject project) throws CoreException {
+        IProjectDescription projectDescription = project.getDescription();
+        ICommand[] commands = project.getDescription().getBuildSpec();
+        boolean addBuilder = true;
+        for (int i = 0; i < commands.length; i++) {
+            if (commands[i].getBuilderName().equals(BUILDER_ID)) {
+                addBuilder = false;
+            }
+        }
+        if (addBuilder) {
+            ICommand[] newCommands = null;
+            ICommand command = projectDescription.newCommand();
+            command.setBuilderName(BUILDER_ID);
+            newCommands = new ICommand[commands.length + 1];
+            System.arraycopy(commands, 0, newCommands, 0, commands.length);
+            newCommands[commands.length] = command;
+            projectDescription.setBuildSpec(newCommands);
+            project.setDescription(projectDescription, null);
+        }
+    }
+
+    static final void removeFromProject(IProject project) throws CoreException {
+        if (project != null) {
+            IProjectDescription projectDescription = project.getDescription();
+            ICommand[] commands = projectDescription.getBuildSpec();
+            ICommand[] newCommands = null;
+            int builderIndex = -1;
+            for (int i = 0; i < commands.length; i++) {
+                if (commands[i].getBuilderName().equals(BUILDER_ID)) {
+                    builderIndex = i;
+                }
+            }
+            if (builderIndex != -1) {
+                newCommands = new ICommand[commands.length - 1];
+                for (int i = 0; i < builderIndex; i++) {
+                    newCommands[i] = commands[i];
+                }
+                for (int i = builderIndex + 1; i < commands.length; i++) {
+                    newCommands[i - 1] = commands[i];
+                }
+                projectDescription.setBuildSpec(newCommands);
+                project.setDescription(projectDescription, null);
+            }
+        }
+    }
+
 }
-
-
