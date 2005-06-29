0e89b10b37e2cf75264c05c0b4dfd416983d3c57
fdutton <fdutton@c6c22482-0310-0410-b13f-f1d71ac210c7>
2005-06-29 13:02:51 +0000

    Added a preference for controlling automatic POM to classpath synchronization.

--- trunk/mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/preferences/MevenidePreferenceKeys.java
+++ trunk/mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/preferences/MevenidePreferenceKeys.java
@@ -1,5 +1,5 @@
 /* ==========================================================================
- * Copyright 2003-2004 Apache Software Foundation
+ * Copyright 2003-2005 MevenIDE Project
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,26 +14,33 @@
  *  limitations under the License.
  * =========================================================================
  */
+
 package org.mevenide.ui.eclipse.preferences;
 
 /**  
+ * Constant definitions for plug-in preferences
  * 
  * @author Gilles Dodinet (gdodinet@wanadoo.fr)
  * @version $Id: MevenidePreferenceKeys.java,v 1.1 15 sept. 2003 Exp gdodinet 
- * 
  */
-public class MevenidePreferenceKeys {
+public interface MevenidePreferenceKeys {
 
-    public static final String JAVA_HOME_PREFERENCE_KEY = "java.home"; //$NON-NLS-1$
-    public static final String MEVENIDE_CHECKTIMESTAMP_PREFERENCE_KEY = "mevenide.checktimestamp"; //$NON-NLS-1$
-    public static final String JAVA_HEAP_SIZE_PREFERENCE_KEY = "maven.heap.size"; //$NON-NLS-1$
-    public static final String MAVEN_HOME_PREFERENCE_KEY = "maven.home"; //$NON-NLS-1$
-    public static final String MAVEN_LAUNCH_DEFAULTGOALS_PREFERENCE_KEY = "maven.launch.defaultgoals"; //$NON-NLS-1$
-    public static final String MAVEN_LOCAL_HOME_PREFERENCE_KEY = "maven.local.home"; //$NON-NLS-1$
-    public static final String MAVEN_REPO_PREFERENCE_KEY = "maven.repo"; //$NON-NLS-1$
-    public static final String POM_TEMPLATE_LOCATION_PREFERENCE_KEY = "pom.template.location"; //$NON-NLS-1$
-    public static final String DEFAULT_GOALS_PREFERENCE_KEY = "maven.launch.defaultgoals"; //$NON-NLS-1$
-    public static final String REGISTERED_DEPENPENCY_TYPES = "mevenide.pom.dependency.type"; //$NON-NLS-1$
-    public static final String TOOLS_JAR_PREFERENCE_KEY = "tools.jar"; //$NON-NLS-1$
+    String JAVA_HOME_PREFERENCE_KEY                 = "java.home";                    //$NON-NLS-1$
+    String MEVENIDE_CHECKTIMESTAMP_PREFERENCE_KEY   = "mevenide.checktimestamp";      //$NON-NLS-1$
+    String JAVA_HEAP_SIZE_PREFERENCE_KEY            = "maven.heap.size";              //$NON-NLS-1$
+    String MAVEN_HOME_PREFERENCE_KEY                = "maven.home";                   //$NON-NLS-1$
+    String MAVEN_LAUNCH_DEFAULTGOALS_PREFERENCE_KEY = "maven.launch.defaultgoals";    //$NON-NLS-1$
+    String MAVEN_LOCAL_HOME_PREFERENCE_KEY          = "maven.local.home";             //$NON-NLS-1$
+    String MAVEN_REPO_PREFERENCE_KEY                = "maven.repo";                   //$NON-NLS-1$
+    String POM_TEMPLATE_LOCATION_PREFERENCE_KEY     = "pom.template.location";        //$NON-NLS-1$
+    String DEFAULT_GOALS_PREFERENCE_KEY             = "maven.launch.defaultgoals";    //$NON-NLS-1$
+    String REGISTERED_DEPENPENCY_TYPES              = "mevenide.pom.dependency.type"; //$NON-NLS-1$
+    String TOOLS_JAR_PREFERENCE_KEY                 = "tools.jar";                    //$NON-NLS-1$
+
+    /**
+     * Controls automatic POM synchronization behavior.
+     * <p>value: {@value}</p>
+     */
+    String AUTOSYNC_ENABLED = "mevenide.autosync.enabled"; //$NON-NLS-1$
 
 }
