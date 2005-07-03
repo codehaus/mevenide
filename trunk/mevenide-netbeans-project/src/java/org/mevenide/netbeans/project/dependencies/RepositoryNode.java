5c60493e77c714ff60a60973c99cc0d22fd49b58
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2005-07-03 16:54:12 +0000

    fix adding of dependencies. also allow to add dependency to maven projects from repository browser.

--- trunk/mevenide-netbeans-project/src/java/org/mevenide/netbeans/project/dependencies/RepositoryNode.java
+++ trunk/mevenide-netbeans-project/src/java/org/mevenide/netbeans/project/dependencies/RepositoryNode.java
@@ -17,15 +17,39 @@
 
 package org.mevenide.netbeans.project.dependencies;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 import java.util.TreeSet;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import org.apache.maven.project.Dependency;
+import org.apache.maven.project.Project;
+import org.mevenide.netbeans.project.MavenProject;
+import org.mevenide.netbeans.project.customizer.DependencyPOMChange;
+import org.mevenide.netbeans.project.customizer.ui.LocationComboFactory;
+import org.mevenide.netbeans.project.customizer.ui.OriginChange;
+import org.mevenide.netbeans.project.queries.MavenFileOwnerQueryImpl;
+import org.mevenide.netbeans.project.writer.NbProjectWriter;
 import org.mevenide.repository.RepoPathElement;
+import org.openide.DialogDisplayer;
+import org.openide.ErrorManager;
+import org.openide.NotifyDescriptor;
+import org.openide.actions.PropertiesAction;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
 import org.openide.nodes.Node;
+import org.openide.nodes.PropertySupport;
+import org.openide.nodes.Sheet;
 import org.openide.util.RequestProcessor;
+import org.openide.util.actions.Presenter;
 import org.openide.util.lookup.Lookups;
 
 /**
@@ -56,7 +80,7 @@ public class RepositoryNode extends AbstractNode implements LocalRepoRefresher {
     
     /**
      * constructor for use when creating the root node.
-     */ 
+     */
     public RepositoryNode(RepoPathElement el, URI rootUri) {
         this(el);
         root = rootUri;
@@ -78,18 +102,28 @@ public class RepositoryNode extends AbstractNode implements LocalRepoRefresher {
         return super.getHtmlDisplayName();
     }
     
+    public Action[] getActions(boolean context) {
+        if (element.isLeaf()) {
+            Action[] retValue = new Action[3];
+            retValue[0] = new AddAsDependencyAction();
+            retValue[1] = null;
+            retValue[2] = ((PropertiesAction)PropertiesAction.get(PropertiesAction.class)).createContextAwareInstance(Lookups.singleton(this));
+            return retValue;
+        }
+        return new Action[0];
+    }
     
     private String createName() {
         switch (element.getLevel()) {
-            case RepoPathElement.LEVEL_ROOT : 
+            case RepoPathElement.LEVEL_ROOT :
                 String str = root == null ? "" : root.toString();
                 if (str.startsWith("http://")) {
                     return "Remote at " + str;
                 }
                 return "Local at " + str;
-            case RepoPathElement.LEVEL_GROUP : 
+            case RepoPathElement.LEVEL_GROUP :
                 return element.getGroupId();
-            case RepoPathElement.LEVEL_TYPE : 
+            case RepoPathElement.LEVEL_TYPE :
                 return element.getType();
             case RepoPathElement.LEVEL_ARTIFACT :
                 return element.getArtifactId();
@@ -98,9 +132,9 @@ public class RepositoryNode extends AbstractNode implements LocalRepoRefresher {
                 if (parent != null && parent instanceof RepositoryNode) {
                     int level = ((RepositoryNode)parent).getRepoLevel();
                     if (level == RepoPathElement.LEVEL_ARTIFACT) {
-                       return element.getVersion(); 
+                        return element.getVersion();
                     }
-                } 
+                }
                 return element.getArtifactId() + "  (" + element.getVersion() + ")";
         }
         throw new IllegalStateException();
@@ -130,7 +164,7 @@ public class RepositoryNode extends AbstractNode implements LocalRepoRefresher {
     
     public void markAsDownloaded() {
         //what do to?
-//        
+//
 //        if (element.isRemote() || isLeaf()) {
 //            return;
 //        }
@@ -138,8 +172,8 @@ public class RepositoryNode extends AbstractNode implements LocalRepoRefresher {
         
     }
     
-    public static RepoPathElement generateSameLevelElement(RepoPathElement element, 
-                                                           RepoPathElement el) {
+    public static RepoPathElement generateSameLevelElement(RepoPathElement element,
+            RepoPathElement el) {
         String group = null;
         String type = null;
         String artifact = null;
@@ -162,6 +196,37 @@ public class RepositoryNode extends AbstractNode implements LocalRepoRefresher {
             group = el.getGroupId();
         }
         return new RepoPathElement(element.getReader(), null, group, type, artifact, version, ext);
+    }
+    
+    protected Sheet createSheet() {
+        Sheet sheet = Sheet.createDefault();
+        Sheet.Set basicProps = sheet.get(Sheet.PROPERTIES);
+        try {
+            PropertySupport.Reflection artifactId = new PropertySupport.Reflection(element, String.class, "getArtifactId", null);
+            artifactId.setName("artifactId");
+            artifactId.setDisplayName("Artifact Id");
+            artifactId.setShortDescription("");
+            PropertySupport.Reflection groupId = new PropertySupport.Reflection(element, String.class, "getGroupId", null);
+            groupId.setName("groupId");
+            groupId.setDisplayName("Group Id");
+            groupId.setShortDescription("");
+            PropertySupport.Reflection version = new PropertySupport.Reflection(element, String.class, "getVersion", null);
+            version.setName("version");
+            version.setDisplayName("Version");
+            version.setShortDescription("");
+            PropertySupport.Reflection type = new PropertySupport.Reflection(element, String.class, "getType", null);
+            type.setName("type");
+            type.setDisplayName("Type");
+            type.setShortDescription("");
+            
+            
+            basicProps.put(new Node.Property[] {
+                artifactId, groupId, version, type
+            });
+        } catch (NoSuchMethodException exc) {
+            exc.printStackTrace();
+        }
+        return sheet;
     }
     
     private static class RepositoryChildren extends Children.Keys {
@@ -190,12 +255,12 @@ public class RepositoryNode extends AbstractNode implements LocalRepoRefresher {
             System.out.println("wrong object..");
             return new Node[0];
         }
-
+        
         protected void removeNotify() {
             setKeys(Collections.EMPTY_LIST);
             super.removeNotify();
         }
-
+        
         protected void addNotify() {
             setKeys(Collections.singleton(LOADING));
             runLoad();
@@ -248,6 +313,90 @@ public class RepositoryNode extends AbstractNode implements LocalRepoRefresher {
 //            }
 //        }
     }
-
+    
+    private class AddAsDependencyAction extends AbstractAction implements Presenter.Popup {
+        private MavenProject project;
+        
+        public AddAsDependencyAction() {
+        }
+        
+        public AddAsDependencyAction(MavenProject proj) {
+            project = proj;
+        }
+        
+        public void actionPerformed(java.awt.event.ActionEvent actionEvent) {
+            Project[] projs = project.getContext().getPOMContext().getProjectLayers();
+            ArrayList deps = new ArrayList();
+            DependencyPOMChange change = null;
+            for (int i = 0; i < projs.length; i++) {
+                List ones = projs[i].getDependencies();
+                if (ones != null) {
+                    Iterator it = ones.iterator();
+                    while (it.hasNext()) {
+                        Dependency dep = (Dependency)it.next();
+                        DependencyPOMChange chng = DependencyPOMChange.createChangeInstance(dep, i, new HashMap(),
+                                LocationComboFactory.createPOMChange(project, false), false);
+                            deps.add(chng);
+                        if (((element.getArtifactId().equals(dep.getArtifactId()) && 
+                             element.getGroupId().equals(dep.getGroupId()))
+                             || (element.getArtifactId().equals(element.getGroupId()) && 
+                                 element.getArtifactId().equals(dep.getId()))
+                             ) && (element.getType().equals(dep.getType()) || (element.getType().equals("jar") && dep.getType() == null))) {
+                            change = chng;
+                        }
+                    }
+                }
+            }
+            if (change == null) {
+                change = DependencyPOMChange.createChangeInstance(null, OriginChange.LOCATION_POM, new HashMap(), LocationComboFactory.createPOMChange(project, false), false);
+                deps.add(change);
+            } else {
+                NotifyDescriptor dd = new NotifyDescriptor.Confirmation(
+                        "The project already has a dependency with '" + element.getGroupId() + ":" + element.getArtifactId() + "' id. Replace it?",
+                        "Dependency conflict", 
+                        NotifyDescriptor.YES_NO_OPTION,
+                        NotifyDescriptor.QUESTION_MESSAGE);
+                Object ret = DialogDisplayer.getDefault().notify(dd);
+                if (ret != NotifyDescriptor.YES_OPTION) {
+                    return;
+                }
+            }
+            HashMap newValues = new HashMap();
+            newValues.put("artifactId", element.getArtifactId());
+            newValues.put("groupId", element.getGroupId());
+            newValues.put("version", element.getVersion());
+            newValues.put("type", element.getType());
+            change.setNewValues(newValues, new HashMap());
+            try {
+                NbProjectWriter writer = new NbProjectWriter(project);
+                writer.applyChanges(deps);
+            } catch (Exception exc) {
+                ErrorManager.getDefault().notify(ErrorManager.USER, exc);
+            }
+            
+        }
+        
+        public JMenuItem getPopupPresenter() {
+            JMenu menu = new JMenu();
+            if (project == null) {
+                menu.setText("Add as dependency to");
+                Set projs = MavenFileOwnerQueryImpl.getInstance().getOpenedProjects();
+                if (projs.size() == 0) {
+                    menu.setEnabled(false);
+                } else {
+                    Iterator it = projs.iterator();
+                    while (it.hasNext()) {
+                        MavenProject prj = (MavenProject)it.next();
+                        JMenuItem item = new JMenuItem(new AddAsDependencyAction(prj));
+                        item.setText(prj.getProjectInfo().getDisplayName());
+                        item.setIcon(prj.getProjectInfo().getIcon());
+                        menu.add(item);
+                    }
+                }
+            }
+            return menu;
+        }
+    }
+    
 }
 
