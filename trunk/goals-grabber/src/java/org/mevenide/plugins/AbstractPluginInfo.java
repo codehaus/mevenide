d1777caedd2014e5a01ea2a994f4ddd53002f1a3
fdutton <fdutton@c6c22482-0310-0410-b13f-f1d71ac210c7>
2005-09-09 12:14:03 +0000

    Removed unused imports.

--- trunk/goals-grabber/src/java/org/mevenide/plugins/AbstractPluginInfo.java
+++ trunk/goals-grabber/src/java/org/mevenide/plugins/AbstractPluginInfo.java
@@ -19,8 +19,6 @@ package org.mevenide.plugins;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
