b967b8e2039e34fd246f59058c3f9a41c9798e2f
fdutton <fdutton@c6c22482-0310-0410-b13f-f1d71ac210c7>
2005-12-24 12:01:13 +0000

    Added javadoc comments.

--- trunk/mevenide-repository/src/java/org/mevenide/repository/RepoPathElement.java
+++ trunk/mevenide-repository/src/java/org/mevenide/repository/RepoPathElement.java
@@ -1,5 +1,5 @@
 /* ==========================================================================
- * Copyright 2003-2004 Mevenide Team
+ * Copyright (c) 2003-2005 Mevenide Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
