c8dcb2ee9aedc0656a9915bcc71e2c195334e542
fdutton <fdutton@c6c22482-0310-0410-b13f-f1d71ac210c7>
2005-12-31 18:55:40 +0000

    Added warning explaining that a POM cannot be created in a folder that already has one.

--- mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/actions/CreatePomAction.java
+++ mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/actions/CreatePomAction.java
@@ -22,9 +22,12 @@ import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Display;
+import org.mevenide.ui.eclipse.Mevenide;
 import org.mevenide.ui.eclipse.template.view.ChooseTemplateDialog;
 import org.mevenide.ui.eclipse.util.FileUtils;
 import org.mevenide.util.StringUtils;
@@ -56,6 +59,15 @@ public class CreatePomAction extends AbstractMevenideAction {
 			else {
 			    //should we allow pom file name customization through this action ?
 			    //should we warn the user that project.xml already exists in this folder ?
+		        Display display = getStandardDisplay();
+		        display.asyncExec(new Runnable() {
+		            public void run() {
+		                MessageDialog.openWarning(
+		                        null,
+		                        Mevenide.getResourceString("ChoosePomTemplateDialog.Title"), //$NON-NLS-1$
+		                        "A POM already exists in this folder.");
+		            }
+		        });
 			}
 		} 
 		catch (Exception e) {
@@ -63,7 +75,17 @@ public class CreatePomAction extends AbstractMevenideAction {
 			log.debug("Unable to create POM due to", e); //$NON-NLS-1$
 		}
 	}
-	
+
+    /**
+     * Returns the standard display to be used. The method first checks, if
+     * the thread calling this method has an associated disaply. If so, this
+     * display is returned. Otherwise the method returns the default display.
+     */
+    private static final Display getStandardDisplay() {
+        final Display display = Display.getCurrent();
+        return (display != null) ? display : Display.getDefault();
+    }
+
 	private String chooseTemplate() {
 	    ChooseTemplateDialog dialog = new ChooseTemplateDialog();
 	    int userChoice = dialog.open();
