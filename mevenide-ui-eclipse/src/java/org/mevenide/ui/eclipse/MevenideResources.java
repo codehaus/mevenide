91b09304cc77fe1aae705a0f43c3c319b6e873b2
fdutton <fdutton@c6c22482-0310-0410-b13f-f1d71ac210c7>
2006-01-05 11:56:24 +0000

    Updated copyright statement.

--- mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/MevenideResources.java
+++ mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/MevenideResources.java
@@ -1,5 +1,5 @@
 /* ==========================================================================
- * Copyright 2003-2005 MevenIDE Project
+ * Copyright 2003-2006 Mevenide Team
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +14,7 @@
  *  limitations under the License.
  * =========================================================================
  */
+
 package org.mevenide.ui.eclipse;
 
 /**
