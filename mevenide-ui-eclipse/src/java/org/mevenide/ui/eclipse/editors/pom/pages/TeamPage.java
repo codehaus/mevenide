692cddc3594074999f7243d1101633e99ab49d77
fdutton <fdutton@c6c22482-0310-0410-b13f-f1d71ac210c7>
2006-01-05 12:27:19 +0000

    Updated copyright statement.

--- mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/editors/pom/pages/TeamPage.java
+++ mevenide-ui-eclipse/src/java/org/mevenide/ui/eclipse/editors/pom/pages/TeamPage.java
@@ -1,7 +1,5 @@
 /* ==========================================================================
- * Copyright 2003-2004 Apache Software Foundation
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
+* CCopyright 2003-2006 Mevenide Teamnsed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
