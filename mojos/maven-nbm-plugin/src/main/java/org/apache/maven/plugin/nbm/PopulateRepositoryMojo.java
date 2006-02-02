4da6566d94e0766c0759ff9415f9b83c448f6b7b
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2006-02-02 16:19:07 +0000

    allow deploying artifacts in the populate-repository goal, have some basic dependencies generated for netbeans modules in the same goal.

--- mojos/maven-nbm-plugin/src/main/java/org/apache/maven/plugin/nbm/PopulateRepositoryMojo.java
+++ mojos/maven-nbm-plugin/src/main/java/org/apache/maven/plugin/nbm/PopulateRepositoryMojo.java
@@ -1,5 +1,5 @@
 /* ==========================================================================
- * Copyright 2003-2004 Mevenide Team
+ * Copyright 2003-2006 Mevenide Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,19 +14,29 @@
  *  limitations under the License.
  * =========================================================================
  */
-
-
 package org.apache.maven.plugin.nbm;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.deployer.ArtifactDeployer;
+import org.apache.maven.artifact.deployer.ArtifactDeploymentException;
 import org.apache.maven.artifact.factory.ArtifactFactory;
 import org.apache.maven.artifact.installer.ArtifactInstallationException;
 import org.apache.maven.artifact.installer.ArtifactInstaller;
 import org.apache.maven.artifact.metadata.ArtifactMetadata;
 import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
+import org.apache.maven.artifact.repository.DefaultArtifactRepository;
+import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
+import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
+import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -40,7 +50,7 @@ import org.apache.tools.ant.types.FileSet;
 
 /**
  * a goal for identifying netbeans modules from the installation and populationg the local
- * repository with them.
+ * repository with them. Optionally you can also deploy to a remote repository.
  *
  * @author <a href="mailto:mkleint@codehaus.org">Milos Kleint</a>
  * @goal populate-repository
@@ -57,6 +67,13 @@ public class PopulateRepositoryMojo
      * @readonly
      */
     protected ArtifactRepository localRepository;
+
+    /**
+     * an url where to deploy the netbeans artifacts.. optional, if not specified, the artifacts will be only installed
+     * in local repository
+     * @parameter expression="${deployUrl}"
+     */
+    private String deployUrl;
     
     /**
      * Location of netbeans installation
@@ -65,6 +82,15 @@ public class PopulateRepositoryMojo
      */
     
     protected String netbeansInstallDirectory;
+    
+    
+    /**
+     * If you want to install/deploy also netbeans api javadocs, download the javadoc zip file from netbeans.org
+     * expand it to a directory, it should contain multiple zip files. Define this parameter as absolute path to the zip files folder.
+     *
+     * @parameter expression="${netbeansJavadocDirectory}"
+     */
+    protected String netbeansJavadocDirectory;
     
     /**
      * Optional parameter, when specified, will force all modules to have the designated version.
@@ -90,12 +116,37 @@ public class PopulateRepositoryMojo
      * @required
      * @readonly
      */
-    private ArtifactInstaller artifactInstaller;    
+    private ArtifactInstaller artifactInstaller;
+    
+    
+    /**
+     * Maven ArtifactDeployer.
+     *
+     * @parameter expression="${component.org.apache.maven.artifact.deployer.ArtifactDeployer}"
+     * @required
+     * @readonly
+     */
+    private ArtifactDeployer artifactDeployer;
+    
+    /**
+     * Maven ArtifactRepositoryFactory.
+     *
+     * @parameter expression="${component.org.apache.maven.artifact.repository.ArtifactRepositoryFactory}"
+     * @required
+     * @readonly
+     */
+    private ArtifactRepositoryFactory repositoryFactory;
     
     
     public void execute() throws MojoExecutionException {
         getLog().info("Populate repository with netbeans modules");
         Project antProject = registerNbmAntTasks();
+        ArtifactRepository deploymentRepository = null;
+        if (deployUrl != null) {
+            ArtifactRepositoryLayout layout = new DefaultRepositoryLayout();
+            deploymentRepository = repositoryFactory.createDeploymentArtifactRepository("netbeans", deployUrl, layout, true);
+        }
+        
         
         if (netbeansInstallDirectory == null) {
             Input input = (Input)antProject.createTask("input");
@@ -139,11 +190,12 @@ public class PopulateRepositoryMojo
         
         String prop = antProject.getProperty("netbeansincludes");
         StringTokenizer tok = new StringTokenizer(prop, ",");
-        ExamineManifest examinator = new ExamineManifest();
+        HashMap moduleDefinitions = new HashMap();
         while (tok.hasMoreTokens()) {
             String token = tok.nextToken();
             File module = new File(token);
-            examinator.resetExamination();
+            ExamineManifest examinator = new ExamineManifest();
+            examinator.setPopulateDependencies(true);
             examinator.setJarFile(module);
             examinator.checkFile();
             if (examinator.isNetbeansModule()) {
@@ -151,41 +203,121 @@ public class PopulateRepositoryMojo
                 String artifact = module.getName().substring(0, module.getName().indexOf(".jar"));
                 String version = forcedVersion == null ? examinator.getSpecVersion() : forcedVersion;
                 String group = "org.netbeans." + (examinator.hasPublicPackages() ? "api" : "modules");
-                
-                Artifact art = createArtifact(artifact, version, group);
-                File pom = createMavenProject(artifact, version, group);
-                ArtifactMetadata metadata = new ProjectArtifactMetadata(art, pom);
-                art.addMetadata( metadata );
-                
-                try {
-                    artifactInstaller.install(module, art, localRepository );
-                } catch ( ArtifactInstallationException e ) {
-                    // TODO: install exception that does not give a trace
-                    throw new BuildException( "Error installing artifact", e );
+                String m = examinator.getModule();
+                int slash = m.indexOf("/");
+                if (slash > 0) {
+                    m = m.substring(0, slash - 1);
                 }
-                
+                m = m.trim();
+                examinator.setModule(m);
+                Artifact art = createArtifact(artifact, version, group);
+                moduleDefinitions.put(new ModuleWrapper(artifact, version, group, examinator, module), art);
             }
+        }
+        List wrapperList = new ArrayList(moduleDefinitions.keySet());
+        Iterator it = moduleDefinitions.entrySet().iterator();
+        File javadocRoot = null;
+        if (netbeansJavadocDirectory != null) {
+            javadocRoot = new File(netbeansJavadocDirectory);
+            if (!javadocRoot.exists()) {
+                javadocRoot = null;
+                throw new MojoExecutionException("The netbeansJavadocFolder parameter doesn't point to an existing folder");
+            }
+        }
+        while (it.hasNext()) {
+            Map.Entry elem = (Map.Entry) it.next();
+            ModuleWrapper man = (ModuleWrapper)elem.getKey();
+            Artifact art = (Artifact)elem.getValue();
+            
+            File pom = createMavenProject(man, wrapperList);
+            ArtifactMetadata metadata = new ProjectArtifactMetadata(art, pom);
+            art.addMetadata( metadata );
+            File javadoc = null;
+            Artifact javadocArt = null;
+            if (javadocRoot != null) {
+                File zip = new File(javadocRoot, art.getArtifactId() + ".zip");
+                if (zip.exists()) {
+                    javadoc = zip;
+                    javadocArt = artifactFactory.createArtifactWithClassifier(art.getGroupId(), 
+                                                                              art.getArtifactId(),
+                                                                              art.getVersion(),
+                                                                              "jar", "javadoc");
+                }
+            }
+            
+            try {
+                if (javadoc != null) {
+                    artifactInstaller.install(javadoc,javadocArt, localRepository);
+                }
+                artifactInstaller.install(man.getFile(), art, localRepository );
+            } catch ( ArtifactInstallationException e ) {
+                // TODO: install exception that does not give a trace
+                throw new MojoExecutionException( "Error installing artifact", e );
+            }
+            try {
+                if (deploymentRepository != null) {
+                    if (javadoc != null) {
+                        artifactDeployer.deploy(javadoc, javadocArt, deploymentRepository, localRepository);
+                    }
+                    artifactDeployer.deploy(man.getFile(), art, deploymentRepository, localRepository);
+                }
+            } catch (ArtifactDeploymentException ex) {
+                throw new MojoExecutionException( "Error Deploying artifact", ex );
+            }
+            
         }
     }
     
-    private File createMavenProject(String artifact, String version, String group) {
+    
+    
+    
+    
+    private File createMavenProject(ModuleWrapper wrapper, List wrapperList) {
         Model mavenModel = new Model();
-
-        mavenModel.setGroupId(group);
-        mavenModel.setArtifactId(artifact);
-        mavenModel.setVersion(version);
+        
+        mavenModel.setGroupId(wrapper.getGroup());
+        mavenModel.setArtifactId(wrapper.getArtifact());
+        mavenModel.setVersion(wrapper.getArtifact());
         mavenModel.setPackaging("jar");
         mavenModel.setModelVersion("4.0.0");
+        ExamineManifest man = wrapper.getModuleManifest();
+        if (!man.getDependencyTokens().isEmpty()) {
+            List deps = new ArrayList();
+            Iterator it = man.getDependencyTokens().iterator();
+            while (it.hasNext()) {
+                String elem = (String) it.next();
+                // create pseudo wrapper
+                ModuleWrapper wr = new ModuleWrapper(elem);
+                int index = wrapperList.indexOf(wr);
+                if (index > -1) {
+                    wr = (ModuleWrapper)wrapperList.get(index);
+                    //we don't want the API modules to depend on non-api ones..
+                    // otherwise the transitive dependency mechanism pollutes your classpath..
+                    if ((wr.getModuleManifest().hasPublicPackages() && wrapper.getModuleManifest().hasPublicPackages())
+                                 || !wrapper.getModuleManifest().hasPublicPackages()) {
+                        Dependency dep = new Dependency();
+                        dep.setArtifactId(wr.getArtifact());
+                        dep.setGroupId(wr.getGroup());
+                        dep.setVersion(wr.getVersion());
+                        dep.setType("jar");
+                        deps.add(dep);
+                    }
+                } else {
+                    getLog().warn("No module found for dependency '" + elem + "'");
+                }
+            }
+            mavenModel.setDependencies(deps);
+        }
         FileWriter writer = null;
         File fil = null;
-            try {
-                MavenXpp3Writer xpp = new MavenXpp3Writer();
-                fil = File.createTempFile("maven", "pom");
-                writer = new FileWriter(fil);
-                xpp.write(writer, mavenModel);
-                return fil;
-            } catch (IOException ex) {
-                ex.printStackTrace();
+        try {
+            MavenXpp3Writer xpp = new MavenXpp3Writer();
+            fil = File.createTempFile("maven", "pom");
+            writer = new FileWriter(fil);
+            xpp.write(writer, mavenModel);
+            return fil;
+        } catch (IOException ex) {
+            ex.printStackTrace();
             
         } finally {
             if (writer != null) {
@@ -202,5 +334,59 @@ public class PopulateRepositoryMojo
     
     private Artifact createArtifact(String artifact, String version, String group) {
         return artifactFactory.createBuildArtifact(group, artifact, version, "jar");
+    }
+    
+    private class ModuleWrapper  {
+        ExamineManifest man;
+        private String artifact;
+        private String version;
+        private String group;
+        private File file;
+
+        String module;
+        
+        public ModuleWrapper(String module) {
+            this.module = module;
+        }
+        
+        public ModuleWrapper(String art, String ver, String grp, ExamineManifest manifest, File fil) {
+            man = manifest;
+            artifact = art;
+            version = ver;
+            group = grp;
+            file = fil;
+        }
+        
+        public int hashCode() {
+            return getModule().hashCode();
+        }
+        
+        public boolean equals(Object obj) {
+            return getModule().equals(((ModuleWrapper)obj).getModule());
+        }
+        
+        public String getModule() {
+            return module != null ? module : getModuleManifest().getModule();
+        }
+        
+        public ExamineManifest getModuleManifest() {
+            return man;
+        }
+        
+        private String getArtifact() {
+            return artifact;
+        }
+        
+        private String getVersion() {
+            return version;
+        }
+        
+        private String getGroup() {
+            return group;
+        }
+
+        private File getFile() {
+            return file;
+        }
     }
 }
