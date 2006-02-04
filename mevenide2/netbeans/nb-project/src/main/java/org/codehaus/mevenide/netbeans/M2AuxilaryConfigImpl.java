083318ae31cfeab4008be0a01730ee2e3dcf899b
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2006-02-04 13:18:28 +0000

    add auxilary configuration impl, to have bookmarks, automatic project files opening and other features working..

---
+++ mevenide2/netbeans/nb-project/src/main/java/org/codehaus/mevenide/netbeans/M2AuxilaryConfigImpl.java
@@ -0,0 +1,158 @@
+/*
+ * M2AuxilaryConfigImpl.java
+ *
+ * Created on February 3, 2006, 9:23 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.codehaus.mevenide.netbeans;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.codehaus.plexus.util.StringOutputStream;
+import org.netbeans.api.project.ProjectManager;
+import org.netbeans.spi.project.AuxiliaryConfiguration;
+import org.netbeans.spi.project.CacheDirectoryProvider;
+import org.openide.ErrorManager;
+import org.openide.filesystems.FileObject;
+import org.openide.util.Mutex;
+import org.openide.xml.XMLUtil;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ * implementation of AuxiliaryConfiguration that relies on FileObject's attributes
+ * @author mkleint
+ */
+public class M2AuxilaryConfigImpl implements AuxiliaryConfiguration {
+    
+    private NbMavenProject project;
+    
+    /** Creates a new instance of M2AuxilaryConfigImpl */
+    public M2AuxilaryConfigImpl(NbMavenProject proj) {
+        this.project = proj;
+    }
+    
+    public Element getConfigurationFragment(final String elementName, final String namespace, boolean shared) {
+        if (shared) {
+            ErrorManager.getDefault().log("Maven2 support doesn't support shared custom configurations. Element was:" + elementName + " , namespace:" + namespace);
+            return null;
+        }
+        return (Element)ProjectManager.mutex().readAccess(new Mutex.Action() {
+            public Object run() {
+                String str = (String)project.getProjectDirectory().getAttribute("AuxilaryConfiguration");
+                if (str != null) {
+                    Document doc;
+                    try {
+                        doc = XMLUtil.parse(new InputSource(new StringReader(str)), false, true, null, null);
+                        return findElement(doc.getDocumentElement(), elementName, namespace);
+                    } catch (SAXException ex) {
+                        ex.printStackTrace();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                return null;
+            }});
+    }
+    
+    public void putConfigurationFragment(final Element fragment, boolean shared) throws IllegalArgumentException {
+        if (shared) {
+            ErrorManager.getDefault().log("Maven2 support doesn't support shared custom configurations. Element was:" + fragment.getNodeName());
+        }
+        ProjectManager.mutex().writeAccess(new Mutex.Action() {
+            public Object run() {
+                String str = (String)project.getProjectDirectory().getAttribute("AuxilaryConfiguration");
+                Document doc = null;
+                if (str != null) {
+                    try {
+                        doc = XMLUtil.parse(new InputSource(new StringReader(str)), false, true, null, null);
+                    } catch (SAXException ex) {
+                        ex.printStackTrace();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                } else {
+                    String element = "project-private"; // NOI18N
+                    doc = XMLUtil.createDocument(element, null, null, null);
+                }
+                if (doc != null) {
+                    Element el = findElement(doc.getDocumentElement(), fragment.getNodeName(), fragment.getNamespaceURI());
+                    if (el != null) {
+                        doc.getDocumentElement().removeChild(el);
+                    }
+                    doc.getDocumentElement().appendChild(doc.importNode(fragment, true));
+                }
+                
+                try {
+                    StringOutputStream wr = new StringOutputStream();
+                    XMLUtil.write(doc, wr, "UTF-8");
+                    project.getProjectDirectory().setAttribute("AuxilaryConfiguration", wr.toString());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                return null;
+            }
+        });
+        
+    }
+    
+    public boolean removeConfigurationFragment(String elementName, String namespace, boolean shared) throws IllegalArgumentException {
+        System.out.println("Element was:" + elementName + " , namespace:" + namespace);
+        if (shared) {
+            ErrorManager.getDefault().log("Maven2 support doesn't support shared custom configurations. Element was:" + elementName + " , namespace:" + namespace);
+            return false;
+        }
+        
+        return true;
+    }
+    
+    
+    private static Element findElement(Element parent, String name, String namespace) {
+        Element result = null;
+        NodeList l = parent.getChildNodes();
+        int len = l.getLength();
+        for (int i = 0; i < len; i++) {
+            if (l.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                Element el = (Element)l.item(i);
+                if (name.equals(el.getLocalName()) && namespace.equals(el.getNamespaceURI())) {
+                    if (result == null) {
+                        result = el;
+                    } else {
+                        return null;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+    
+    private static final DocumentBuilder db;
+    static {
+        try {
+            db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            throw new AssertionError(e);
+        }
+    }
+    private static Element cloneSafely(Element el) {
+        // Using XMLUtil.createDocument is much too slow.
+        synchronized (db) {
+            Document dummy = db.newDocument();
+            return (Element) dummy.importNode(el, true);
+        }
+        
+    }
+
+    
+}
