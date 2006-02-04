6670ef15c9f2806c385b56360082bcda258cc2d5
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2006-02-04 17:50:11 +0000

    remove configuration code added. license header

--- trunk/mevenide2/netbeans/nb-project/src/main/java/org/codehaus/mevenide/netbeans/M2AuxilaryConfigImpl.java
+++ trunk/mevenide2/netbeans/nb-project/src/main/java/org/codehaus/mevenide/netbeans/M2AuxilaryConfigImpl.java
@@ -1,10 +1,18 @@
-/*
- * M2AuxilaryConfigImpl.java
+/* ==========================================================================
+ * Copyright 2006 Mevenide Team
  *
- * Created on February 3, 2006, 9:23 PM
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * =========================================================================
  */
 
 package org.codehaus.mevenide.netbeans;
@@ -107,14 +115,42 @@ public class M2AuxilaryConfigImpl implements AuxiliaryConfiguration {
         
     }
     
-    public boolean removeConfigurationFragment(String elementName, String namespace, boolean shared) throws IllegalArgumentException {
-        System.out.println("Element was:" + elementName + " , namespace:" + namespace);
+    public boolean removeConfigurationFragment(final String elementName, final String namespace, boolean shared) throws IllegalArgumentException {
         if (shared) {
             ErrorManager.getDefault().log("Maven2 support doesn't support shared custom configurations. Element was:" + elementName + " , namespace:" + namespace);
             return false;
         }
-        
-        return true;
+        return ((Boolean)ProjectManager.mutex().writeAccess(new Mutex.Action() {
+            public Object run() {
+                String str = (String)project.getProjectDirectory().getAttribute("AuxilaryConfiguration");
+                Document doc = null;
+                if (str != null) {
+                    try {
+                        doc = XMLUtil.parse(new InputSource(new StringReader(str)), false, true, null, null);
+                    } catch (SAXException ex) {
+                        ex.printStackTrace();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                } else {
+                    return Boolean.FALSE;
+                }
+                if (doc != null) {
+                    Element el = findElement(doc.getDocumentElement(), elementName, namespace);
+                    if (el != null) {
+                        doc.getDocumentElement().removeChild(el);
+                    }
+                }
+                try {
+                    StringOutputStream wr = new StringOutputStream();
+                    XMLUtil.write(doc, wr, "UTF-8");
+                    project.getProjectDirectory().setAttribute("AuxilaryConfiguration", wr.toString());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                return Boolean.TRUE;
+            }
+        })).booleanValue();
     }
     
     
