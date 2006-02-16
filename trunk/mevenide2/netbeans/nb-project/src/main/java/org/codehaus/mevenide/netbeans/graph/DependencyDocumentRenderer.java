8c24f75d579da7d46b47d455c5af6df18a072573
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2006-02-16 18:57:12 +0000

    improved layouting color nodes according to scope..

--- trunk/mevenide2/netbeans/nb-project/src/main/java/org/codehaus/mevenide/netbeans/graph/DependencyDocumentRenderer.java
+++ trunk/mevenide2/netbeans/nb-project/src/main/java/org/codehaus/mevenide/netbeans/graph/DependencyDocumentRenderer.java
@@ -9,11 +9,15 @@
 
 package org.codehaus.mevenide.netbeans.graph;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.Stroke;
+import java.awt.geom.Rectangle2D;
+import org.apache.maven.artifact.Artifact;
 import org.netbeans.graph.api.control.GraphHelper;
 import org.netbeans.graph.api.control.IGraphLinkRenderer;
 import org.netbeans.graph.api.control.IGraphLinkRouter;
@@ -25,6 +29,7 @@ import org.netbeans.graph.api.model.IGraphNode;
 import org.netbeans.graph.api.model.IGraphPort;
 import org.netbeans.graph.vmd.VMDDocumentRenderer;
 import org.netbeans.graph.vmd.VMDEmptyLinkRouter;
+import org.netbeans.graph.vmd.VMDNodeRenderer;
 
 /**
  *
@@ -32,13 +37,31 @@ import org.netbeans.graph.vmd.VMDEmptyLinkRouter;
  */
 public class DependencyDocumentRenderer extends VMDDocumentRenderer {
     
+    public interface IArtifactGetter {
+        Artifact getArtifact();
+    }
+    
+    //cyan
+    private static Color RUNTIME = new Color(203, 255, 252);
+    // green
+    private static Color TEST = new Color(203, 255, 209);
+    // blue
+    private static Color PROVIDED = new Color(221, 203, 255);
+    // yellow
+    private static Color COMPILE = new Color(252, 255, 205);
+    //red
+    private static Color SYSTEM = new Color(255, 203, 214);
+        
     private final VMDEmptyLinkRouter emptyLinkRouter = new VMDEmptyLinkRouter();
     private final SimpleLinkRenderer linkRenderer;
     private final DependencyGraphNodeLayouter layout = new DependencyGraphNodeLayouter();
+
+    private String title;
     /** Creates a new instance of DependencyDocumentRenderer */
-    public DependencyDocumentRenderer(GraphHelper helper) {
+    public DependencyDocumentRenderer(String title, GraphHelper helper) {
         super();
         linkRenderer = new SimpleLinkRenderer(helper);
+        this.title = title;
     }
     
     public IGraphLinkRouter getLinkRouter(IGraphLink link) {
@@ -50,7 +73,7 @@ public class DependencyDocumentRenderer extends VMDDocumentRenderer {
         Color color = gr.getColor();
         gr.setFont(fnt.deriveFont(30f));
         gr.setColor(new Color(109, 226, 255));
-        gr.drawString("Maven2 transitive dependencies", 30, 40);
+        gr.drawString(title, 30, 40);
         gr.setColor(color);
         gr.setFont(fnt);
     }
@@ -60,10 +83,7 @@ public class DependencyDocumentRenderer extends VMDDocumentRenderer {
     }
     
     public IGraphNodeRenderer getNodeRenderer(IGraphNode node) {
-        IGraphNodeRenderer retValue;
-        
-        retValue = super.getNodeRenderer(node);
-        return retValue;
+        return new DelegateNodeRenderer(getHelper(), node);
     }
     
     public IGraphLinkRenderer getLinkRenderer(IGraphLink link) {
@@ -72,6 +92,88 @@ public class DependencyDocumentRenderer extends VMDDocumentRenderer {
 
     public IGraphNodesLayouter getNodesLayouter() {
         return layout;
+    }
+    
+    private class DelegateNodeRenderer implements IGraphNodeRenderer {
+        private IGraphNodeRenderer vmd;
+
+        private GraphHelper helper;
+        
+        public DelegateNodeRenderer(GraphHelper helper, IGraphNode node) {
+            vmd = new VMDNodeRenderer(helper, node);
+            this.helper = helper;
+        }
+
+        public void layoutNode(IGraphNode node, Graphics2D gr) {
+            vmd.layoutNode(node, gr);
+        }
+
+        public int[] getLayers(IGraphNode node) {
+            return vmd.getLayers(node);
+        }
+
+                
+        public void renderNode(IGraphNode node, Graphics2D gr, int layer) {
+            vmd.renderNode(node, gr, layer);
+                    //LAYER_NODE_BACKGROUND = 100;
+            if (layer == 100) {
+                if (! helper.isComponentSelected(node)) {
+                    Rectangle rect = helper.getBounds(node);
+                    IArtifactGetter get = (IArtifactGetter)node.getLookup().lookup(IArtifactGetter.class);
+                    Artifact art = get.getArtifact();
+                    Color col = null;
+                    if ("runtime".equals(art.getScope())) {
+                        col = RUNTIME; // cyan
+                    }
+                    if ("test".equals(art.getScope())) {
+                        col = TEST; // green
+                    }
+                    if ("provided".equals(art.getScope())) {
+                        col = PROVIDED; // blue
+                    }
+                    // no scope == compile?
+                    if ("compile".equals(art.getScope()) || art.getScope() == null) {
+                        col = COMPILE; // yellow
+                    }
+                    if ("system".equals(art.getScope())) {
+                        col = SYSTEM;  //red
+                    }
+                    if (col != null) {
+                        Color old = gr.getColor();
+                        gr.setColor (col);
+                        gr.fillRect (rect.x, rect.y, rect.width, rect.height);
+                        gr.setColor(old);
+                    }
+                    DependencyGraphNodeLayouter.IRootDistance dist = (DependencyGraphNodeLayouter.IRootDistance)node.getLookup().lookup(DependencyGraphNodeLayouter.IRootDistance.class);
+                    if (dist.getDistanceFromRoot() == 0) {
+                        Color old = gr.getColor();
+                        gr.setColor(new Color(0xCDCDCD));
+                        Stroke oldStroke = gr.getStroke();
+                        gr.setStroke(new BasicStroke(4, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+                        gr.draw (new Rectangle2D.Float (rect.x + 0.5f, rect.y + 0.5f, rect.width - 1, rect.height - 1));
+                        gr.setColor(old);
+                        gr.setStroke(oldStroke);
+                    }
+                }
+            }
+        }
+
+        public IGraphEditor getEditor(IGraphNode node, Point position) {
+            return vmd.getEditor(node, position);
+        }
+
+        public String getToolTipText(IGraphNode node, Point position) {
+            return vmd.getToolTipText(node, position);
+        }
+
+        public Point locationSuggested(IGraphNode node, Point suggestedLocation) {
+            return vmd.locationSuggested(node, suggestedLocation);
+        }
+
+        public void portLocationSuggested(IGraphPort port, Point suggestedLocation) {
+            vmd.portLocationSuggested(port, suggestedLocation);
+        }
+        
     }
 
 }
--- trunk/mevenide2/netbeans/nb-project/src/main/java/org/codehaus/mevenide/netbeans/graph/DependencyGraphNodeLayouter.java
+++ trunk/mevenide2/netbeans/nb-project/src/main/java/org/codehaus/mevenide/netbeans/graph/DependencyGraphNodeLayouter.java
@@ -9,16 +9,22 @@
 
 package org.codehaus.mevenide.netbeans.graph;
 
+import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 import org.netbeans.graph.api.control.GraphHelper;
 import org.netbeans.graph.api.control.IGraphNodesLayouter;
@@ -49,33 +55,24 @@ public class DependencyGraphNodeLayouter implements IGraphNodesLayouter {
     }
     
     public void layoutNodesLocations(Graphics2D graphics, GraphHelper helper, IGraphNode[] nodes) {
-        Set currents = new TreeSet(new DistanceComparator());
+        Set currents = new TreeSet(new ComplexComparator());
         currents.addAll(Arrays.asList(nodes));
         IGraphNode root = findRootNode(helper);
         Point rootPoint;
         if (currents.contains(root)) {
-            rootPoint =  new Point(700, 700);
+            rootPoint =  new Point(1000, 1000);
             helper.setNodeLocation(root, rootPoint);
-        } else {
-            rootPoint = helper.getNodeLocation(root);
-        }
+        } 
+        TreeMap map = groupOneParented(currents);
         currents.remove(root);
-        double r = 150;
-        double theta = 0;
-        double thetaStep = Math.PI / 5;
-        while (currents.size() != 0) {
-            AffineTransform tr = AffineTransform.getRotateInstance(theta);
-            Point2D d2point = tr.transform(new Point2D.Double((double)(r), (double)(r)), null);
-            Point point = new Point((int)d2point.getX() + rootPoint.x, (int)d2point.getY() + rootPoint.y);
-            IGraphNode nd = findBestFitNode(currents, root);
-            currents.remove(nd);
-            helper.setNodeLocation(nd, point);
-            theta = theta + thetaStep;
-            if (theta > (Math.PI * 2 - Math.PI / 10)) {
-                r = r + 90;
-                theta = theta - Math.PI * 2;
-                thetaStep = thetaStep * 3 / 4; 
-            }
+        Iterator it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry elem = (Map.Entry) it.next();
+            IGraphNode master = (IGraphNode) elem.getKey();
+        
+            Collection childs = (Collection) elem.getValue();
+            layoutCirculary(childs, master, root, helper);
+            currents.removeAll(childs);
         }
         
         // at the end align the nodes to the edge..
@@ -94,6 +91,42 @@ public class DependencyGraphNodeLayouter implements IGraphNodesLayouter {
         }
     }
     
+    private void layoutCirculary(Collection nodes, IGraphNode master, IGraphNode root, GraphHelper helper) {
+        Point masterPoint = helper.getNodeLocation(master);
+            double r;
+            double theta;
+            double thetaStep = Math.PI / 5;
+        if (root == master) {
+            r = 150;
+            theta = 0;
+        } else {
+            r = 150;
+            theta = 0;
+        }
+        Iterator it = nodes.iterator();
+        IGraphNode nd = (IGraphNode)it.next();
+        while (true) {
+            AffineTransform tr = AffineTransform.getRotateInstance(theta);
+            Point2D d2point = tr.transform(new Point2D.Double((double)(0), (double)(r)), null);
+            Point point = new Point((int)d2point.getX() + masterPoint.x, (int)d2point.getY() + masterPoint.y);
+            if (isThereFreeSpace(point, nd, helper)) {
+                helper.setNodeLocation(nd, point);
+                if (it.hasNext()) {
+                    nd = (IGraphNode)it.next();
+                } else {
+                    return;
+                }
+            }
+            theta = theta + thetaStep;
+            if (theta > (Math.PI * 2 - Math.PI / 10)) {
+                r = r + 90;
+                theta = theta - Math.PI * 2;
+                thetaStep = thetaStep * 3 / 4; 
+            }
+        }
+        
+    }
+    
     private IGraphNode findRootNode(GraphHelper helper) {
         IGraphNode[] nodes = helper.getNodes();
         for (int i = 0; i < nodes.length; i++) {
@@ -110,66 +143,86 @@ public class DependencyGraphNodeLayouter implements IGraphNodesLayouter {
         return nd;
     }
 
-    private int getHopsToRoot(IGraphNode nd, IGraphNode root) {
-        if (nd == root) {
-            return 0;
-        }
+    private static int getDistance(IGraphNode nd) {
         IRootDistance dist = (IRootDistance) nd.getLookup().lookup(IRootDistance.class);
         return dist.getDistanceFromRoot();
-//        IGraphLink[] links = getChildPort(nd);
-//        int min = Integer.MAX_VALUE;
-//        for (int i = 0; i < links.length; i++) {
-//            int curr = getHopsToRoot(links[i].getSourcePort().getNode(), root);
-//            min = Math.min(min, curr);
-//        }
-//        return min == Integer.MAX_VALUE ? 0 : min + 1;
+    }
+    private static IGraphLink[] getOutgoingLinks(IGraphNode nd) {
+        IOutLinks out = (IOutLinks) nd.getLookup().lookup(IOutLinks.class);
+        return out.getOutgoingLinks();
     }
     
-    private IGraphLink[] getChildPort(IGraphNode node) {
-        IGraphPort[] ports = node.getPorts();
-        if (ports != null) {
-            for (int i = 0; i < ports.length; i++) {
-                if ("Child".equals(ports[i].getID())) {
-                    return ports[i].getLinks() != null ? ports[i].getLinks() : new IGraphLink[0];
+    private static IGraphLink[] getIncomingLinks(IGraphNode nd) {
+        IInLinks in = (IInLinks) nd.getLookup().lookup(IInLinks.class);
+        return in.getIncomingLinks();
+    }
+    
+    private static boolean isThereFreeSpace(Point pnt, IGraphNode node, GraphHelper helper) {
+        Rectangle bnds = new Rectangle(0,0, 100, 150);
+        bnds = new Rectangle(pnt.x, pnt.y, bnds.width, bnds.height);
+        IGraphNode[] nds = helper.getNodes();
+        for (int i = 0; i < nds.length; i++) {
+            Rectangle bounds = helper.getBounds(nds[i]);
+            if (bounds == null) {
+                bounds = new Rectangle(helper.getNodeLocation(nds[i]), new Dimension(150, 200));
+            }
+            if (bnds.intersects((bounds))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private TreeMap groupOneParented(Set currents) {
+        TreeMap map = new TreeMap(new DistanceComparator());
+        Iterator it = currents.iterator();
+        while (it.hasNext()) {
+            IGraphNode master = (IGraphNode) it.next();
+            IGraphLink[] outs = getOutgoingLinks(master);
+            if (outs.length > 0) {
+                Collection col = new ArrayList();
+                for (int i = 0; i < outs.length; i++) {
+                   IGraphNode child = outs[i].getTargetPort().getNode();
+//                   if (getIncomingLinks(child).length == 1) {
+                       col.add(child);
+//                   }
+                }
+                if (col.size() > 0) {
+                    map.put(master, col);
                 }
             }
         }
-        return new IGraphLink[0];
+        return map;
     }
     
-    private IGraphLink[] getParentPort(IGraphNode node) {
-        IGraphPort[] ports = node.getPorts();
-        if (ports != null) {
-            for (int i = 0; i < ports.length; i++) {
-                if ("Parent".equals(ports[i].getID())) {
-                    return ports[i].getLinks() != null ? ports[i].getLinks() : new IGraphLink[0];
-                }
+    public class ComplexComparator implements Comparator {
+        public int compare(Object o1, Object o2) {
+            IGraphNode node1 = (IGraphNode)o1;
+            IGraphNode node2 = (IGraphNode)o2;
+            int in =  getIncomingLinks(node1).length - getIncomingLinks(node2).length;
+            if (in != 0) {
+                return in;
             }
+            int dist =  getDistance(node1) - getDistance(node2);
+            if (dist != 0) {
+                return dist;
+            }
+            int out =  getOutgoingLinks(node1).length - getOutgoingLinks(node2).length;
+            if (out != 0) {
+                return out;
+            }
+            return node1.getID().compareTo(node2.getID());
         }
-        return new IGraphLink[0];
+        
     }
     
     public class DistanceComparator implements Comparator {
         public int compare(Object o1, Object o2) {
             IGraphNode node1 = (IGraphNode)o1;
             IGraphNode node2 = (IGraphNode)o2;
-            IInLinks in1 = (IInLinks) node1.getLookup().lookup(IInLinks.class);
-            IInLinks in2 = (IInLinks) node2.getLookup().lookup(IInLinks.class);
-            int in =  in1.getIncomingLinks().length - in2.getIncomingLinks().length;
-            if (in != 0) {
-                return in;
-            }
-            IRootDistance dist1 = (IRootDistance) node1.getLookup().lookup(IRootDistance.class);
-            IRootDistance dist2 = (IRootDistance) node2.getLookup().lookup(IRootDistance.class);
-            int dist =  dist1.getDistanceFromRoot() - dist2.getDistanceFromRoot();
+            int dist =  getDistance(node1) - getDistance(node2);
             if (dist != 0) {
                 return dist;
-            }
-            IOutLinks out1 = (IOutLinks) node1.getLookup().lookup(IOutLinks.class);
-            IOutLinks out2 = (IOutLinks) node2.getLookup().lookup(IOutLinks.class);
-            int out =  out1.getOutgoingLinks().length - out2.getOutgoingLinks().length;
-            if (out != 0) {
-                return out;
             }
             return node1.getID().compareTo(node2.getID());
         }
