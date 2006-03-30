23596a16ab21eadfd7cc2d95e19c49094a9945c6
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2006-03-30 18:01:52 +0000

    have the deploy plugin directly use the debugger plugin when debugging..

--- trunk/mevenide2/netbeans/ide-mojos/netbeans-debugger-plugin/src/main/java/org/codehaus/mevenide/plugin/debugger/JPDAConnectMojo.java
+++ trunk/mevenide2/netbeans/ide-mojos/netbeans-debugger-plugin/src/main/java/org/codehaus/mevenide/plugin/debugger/JPDAConnectMojo.java
@@ -38,24 +38,24 @@ public class JPDAConnectMojo extends AbstractMojo {
      * @readonly
      */
     private MavenProject project;
-
+    
     /**
      * @parameter expression="${jpda.host}"
      */
     private String host = "localhost";
-
+    
     /**
      * @parameter expression="${jpda.address}"
      * @required
      */
     private String address;
     
-    /** 
+    /**
      * Name which will represent this debugging session in debugger UI.
      * @parameter expression="${project.artifactId}"
      */
     private String name;
-
+    
     /**
      * @parameter expression="${jpda.transport}"
      */
@@ -67,49 +67,48 @@ public class JPDAConnectMojo extends AbstractMojo {
     
     public void execute() throws MojoExecutionException, MojoFailureException {
         getLog().info("Attaching JPDA Debugger...");
-        getLog().info("    Transport=" + transport);
-        getLog().info("    Address=" + address);
-        getLog().info("    Host=" + host);
+        getLog().info("    Transport=" + getTransport());
+        getLog().info("    Address=" + getAddress());
+        getLog().info("    Host=" + getHost());
 //        JPDAStart.verifyPaths(getProject(), classpath);
 //        JPDAStart.verifyPaths(getProject(), sourcepath);
         
         final Object[] lock = new Object [1];
         try {
-
-            Project nbproject = ProjectManager.getDefault().findProject(FileUtil.toFileObject(project.getBasedir()));
-            ClassPath sourcePath = Utils.createSourcePath(nbproject, project);
+            
+            Project nbproject = ProjectManager.getDefault().findProject(FileUtil.toFileObject(getProject().getBasedir()));
+            ClassPath sourcePath = Utils.createSourcePath(nbproject, getProject());
             ClassPath jdkSourcePath = Utils.createJDKSourcePath(nbproject);
             
-            final Map properties = new HashMap ();
-            properties.put ("sourcepath", sourcePath);
-            properties.put ("name", name);
-            properties.put ("jdksources", jdkSourcePath);
+            final Map properties = new HashMap();
+            properties.put("sourcepath", sourcePath);
+            properties.put("name", name);
+            properties.put("jdksources", jdkSourcePath);
             
-
+            
             synchronized(lock) {
-                RequestProcessor.getDefault ().post (new Runnable () {
+                RequestProcessor.getDefault().post(new Runnable() {
                     public void run() {
                         synchronized(lock) {
                             try {
-                                // VirtualMachineManagerImpl can be initialized 
+                                // VirtualMachineManagerImpl can be initialized
                                 // here, so needs to be inside RP thread.
-                                if (transport.equals ("dt_socket"))
+                                if (getTransport().equals("dt_socket"))
                                     try {
-                                        JPDADebugger.attach (
-                                            host, 
-                                            Integer.parseInt (address), 
-                                            new Object[] {properties}
+                                        JPDADebugger.attach(
+                                                getHost(),                                             
+                                                Integer.parseInt(getAddress()),
+                                                new Object[] {properties}
                                         );
                                     } catch (NumberFormatException e) {
                                         throw new MojoFailureException(
-                                            "address attribute must specify port " +
-                                            "number for dt_socket connection");
-                                    }
-                                else
-                                    JPDADebugger.attach (
-                                        address, 
-                                        new Object[] {properties}
-                                    );
+                                                "address attribute must specify port " +
+                                                "number for dt_socket connection");
+                                    } else
+                                        JPDADebugger.attach(
+                                                getAddress(),               
+                                                new Object[] {properties}
+                                        );
                             } catch (Throwable e) {
                                 lock[0] = e;
                             } finally {
@@ -126,7 +125,7 @@ public class JPDAConnectMojo extends AbstractMojo {
                 if (lock[0] != null)  {
                     throw new MojoExecutionException("", (Throwable) lock[0]);
                 }
-
+                
             }
         } catch (IOException ex) {
             throw new MojoExecutionException("", ex);
@@ -141,6 +140,38 @@ public class JPDAConnectMojo extends AbstractMojo {
 //                "end: success "
 //            );
         
+    }
+    
+    public String getHost() {
+        return host;
+    }
+    
+    public void setHost(String host) {
+        this.host = host;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public String getTransport() {
+        return transport;
+    }
+    
+    public void setTransport(String transport) {
+        this.transport = transport;
+    }
+
+    public MavenProject getProject() {
+        return project;
+    }
+
+    public void setProject(MavenProject project) {
+        this.project = project;
     }
     
 }
