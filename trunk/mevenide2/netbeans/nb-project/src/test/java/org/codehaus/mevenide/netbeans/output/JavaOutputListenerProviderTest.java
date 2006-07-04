157ab08374db056e6952b10f1b71f226c8d0e7d0
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2006-07-04 20:38:27 +0000

    additional testcase

--- trunk/mevenide2/netbeans/nb-project/src/test/java/org/codehaus/mevenide/netbeans/output/JavaOutputListenerProviderTest.java
+++ trunk/mevenide2/netbeans/nb-project/src/test/java/org/codehaus/mevenide/netbeans/output/JavaOutputListenerProviderTest.java
@@ -53,6 +53,9 @@ public class JavaOutputListenerProviderTest extends TestCase {
         provider.processLine("/home/mkleint/src/mevenide/mevenide2/netbeans/nb-project/src/test/java/org/codehaus/mevenide/netbeans/output/JavaOutputListenerProviderTest.java:[31,1] illegal start of type", visitor);
         assertNotNull(visitor.getOutputListener());
         visitor.resetVisitor();
+        provider.processLine("K:\\jsr144-private\\common-1_4\\workspace\\ri\\oss_common_j2eesdk-1_4-src-ri\\oss_cbe_party_ri\\..\\src\\main\\java\\ossj\\common\\cbe\\party\\PartyValueIteratorImpl.java:[22,7] ossj.common.cbe.party.PartyValueIteratorImpl is not abstract and does not override abstract method getNextPartys(int) in javax.oss.cbe.party.PartyValueIterator", visitor);
+        assertNotNull(visitor.getOutputListener());
+        visitor.resetVisitor();
         provider.processLine("C:\\lfo\\pers\\projects\\mojos\\maven-hello-plugin\\src\\main\\java\\org\\laurentforet\\mojos\\hello\\GreetingMojo.java:[14,8] cannot find symbol", visitor);
         assertNotNull(visitor.getOutputListener());
         visitor.resetVisitor();
