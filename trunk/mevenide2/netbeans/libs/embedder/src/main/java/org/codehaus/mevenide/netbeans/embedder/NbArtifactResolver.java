bce3c6c9601673ad57084da0f0cef371f158c0c9
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2006-07-08 04:38:05 +0000

    MEVENIDE-422, try download the parent pom when not available locally.. try hard not to download dependency poms/jars

--- trunk/mevenide2/netbeans/libs/embedder/src/main/java/org/codehaus/mevenide/netbeans/embedder/NbArtifactResolver.java
+++ trunk/mevenide2/netbeans/libs/embedder/src/main/java/org/codehaus/mevenide/netbeans/embedder/NbArtifactResolver.java
@@ -50,39 +50,66 @@ import org.codehaus.plexus.personality.plexus.lifecycle.phase.Contextualizable;
  */
 public class NbArtifactResolver extends DefaultArtifactResolver {    
     
-    
     private ResolutionListener listener;
+    protected Field wagonMan;
     
     /** Creates a new instance of NbWagonManager */
     public NbArtifactResolver() {
         super();
+        try {
+            wagonMan = DefaultArtifactResolver.class.getDeclaredField("wagonManager");
+            wagonMan.setAccessible(true);
+        } catch (SecurityException ex) {
+            ex.printStackTrace();
+        } catch (NoSuchFieldException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public void resolve(Artifact artifact, List list, ArtifactRepository artifactRepository) throws ArtifactResolutionException, ArtifactNotFoundException {
 //        artifact.setResolved(true);
-        try {
-            super.resolve(artifact, list, artifactRepository);
-        } catch (ArtifactResolutionException exc) {
-            if (exc.getCause() instanceof IOException) {
-                // DefaultArtifactResolver:193 when having snapshots something gets copied and fails
-                // when the wagon manager just pretends to download something..
-                System.out.println("exc=" + exc.getCause().getMessage());
-                return;
+        //MEVENIDE-422 
+        if (artifact.getScope() == null && "pom".equals(artifact.getType())) {
+            //the condition is meant to mean.. "if we look for parent pom", not sure it's close enough..
+            try {
+                letArtifactGo(artifact);
+                super.resolve(artifact, list, artifactRepository);
+            } catch (ArtifactResolutionException exc) {
+                if (exc.getCause() instanceof IOException) {
+                    // DefaultArtifactResolver:193 when having snapshots something gets copied and fails
+                    // when the wagon manager just pretends to download something..
+                    System.out.println("exc=" + exc.getCause().getMessage());
+                    return;
+                }
+                throw exc;
             }
-            throw exc;
+            finally {
+                cleanLetGone(artifact);
+            }
+        } else {
+            super.resolve(artifact, list, artifactRepository);
         }
     }
     public void resolveAlways(Artifact artifact, List list, ArtifactRepository artifactRepository) throws ArtifactResolutionException, ArtifactNotFoundException {
-        try {
-            super.resolveAlways(artifact, list, artifactRepository);
-        } catch (ArtifactResolutionException exc) {
-            if (exc.getCause() instanceof IOException) {
-                // DefaultArtifactResolver:193 when having snapshots something gets copied and fails
-                // when the wagon manager just pretends to download something..
-                System.out.println("exc=" + exc.getCause().getMessage());
-                return;
+        if (artifact.getScope() == null && "pom".equals(artifact.getType())) {
+            //the condition is meant to mean.. "if we look for parent pom", not sure it's close enough..
+            try {
+                letArtifactGo(artifact);
+                super.resolveAlways(artifact, list, artifactRepository);
+            } catch (ArtifactResolutionException exc) {
+                if (exc.getCause() instanceof IOException) {
+                    // DefaultArtifactResolver:193 when having snapshots something gets copied and fails
+                    // when the wagon manager just pretends to download something..
+                    System.out.println("exc=" + exc.getCause().getMessage());
+                    return;
+                }
+                throw exc;
+            } finally {
+                cleanLetGone(artifact);
             }
-            throw exc;
+        } else {
+            super.resolveAlways(artifact, list, artifactRepository);
+            
         }
     }
 
@@ -143,6 +170,38 @@ public class NbArtifactResolver extends DefaultArtifactResolver {
             newListeners.add(listener);
         }
         return super.resolveTransitively(set, artifact, map, artifactRepository, list, artifactMetadataSource, artifactFilter, newListeners);
+    }
+
+    private void cleanLetGone(Artifact artifact) {
+        if (wagonMan != null) {
+            try {
+                Object manObj = wagonMan.get(this);
+                if (manObj instanceof NbWagonManager) {
+                    NbWagonManager manager = (NbWagonManager)manObj;
+                    manager.cleanLetGone(artifact);
+                }
+            } catch (IllegalArgumentException ex) {
+                ex.printStackTrace();
+            } catch (IllegalAccessException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private void letArtifactGo(Artifact artifact) {
+        if (wagonMan != null) {
+            try {
+                Object manObj = wagonMan.get(this);
+                if (manObj instanceof NbWagonManager) {
+                    NbWagonManager manager = (NbWagonManager)manObj;
+                    manager.letGoThrough(artifact);
+                }
+            } catch (IllegalArgumentException ex) {
+                ex.printStackTrace();
+            } catch (IllegalAccessException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     
