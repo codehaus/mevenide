0f8d555443a5084a9fb3fa0a2a37a889254f199f
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2006-07-29 13:33:19 +0000

    merging fixes from NB_2.2 branch

--- trunk/mevenide2/netbeans/ide-mojos/netbeans-deploy-plugin/src/main/java/org/codehaus/mevenide/plugin/deploy/DeployMojo.java
+++ trunk/mevenide2/netbeans/ide-mojos/netbeans-deploy-plugin/src/main/java/org/codehaus/mevenide/plugin/deploy/DeployMojo.java
@@ -81,7 +81,6 @@ public class DeployMojo extends AbstractMojo {
         if (!allowedPackagings.contains(project.getPackaging())) {
             throw new MojoFailureException("Can only deploy war, ear or ejb packaged projects");
         }
-        getLog().info("about to lookup mavendeployment..");
         MavenDeployment deployment = (MavenDeployment)Lookup.getDefault().lookup(MavenDeployment.class);
         if (deployment == null) {
             getLog().error("Cannot lookup the Maven-Netbeans bridge for deployment.");
