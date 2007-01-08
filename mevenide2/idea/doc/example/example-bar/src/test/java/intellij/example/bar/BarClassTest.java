5e388f1b3410f1cc8e1b8fd80c2cfc4a043249af
jvanzyl <jvanzyl@c6c22482-0310-0410-b13f-f1d71ac210c7>
2007-01-08 04:09:06 +0000

    o adding IDEA code

---
+++ mevenide2/idea/doc/example/example-bar/src/test/java/intellij/example/bar/BarClassTest.java
@@ -0,0 +1,21 @@
+
+package intellij.example.bar;
+
+
+import junit.framework.TestCase;
+import junit.framework.Assert;
+
+
+public class BarClassTest extends TestCase {
+
+    public void setUp() {
+    }
+
+
+    public void testDoIt() {
+
+        Assert.assertTrue(true);
+    }
+
+}
+
