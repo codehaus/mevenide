0f9acb39c62a23a37550da4a565d1d14e83622ac
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2007-01-20 18:11:36 +0000

    relax the permitted packagings condition to allow custom lifecycles to be able to use the plugin. MEVENIDE-483

--- trunk/mevenide2/netbeans/ide-mojos/netbeans-deploy-plugin/src/main/java/org/codehaus/mevenide/plugin/deploy/DeployMojo.java
+++ trunk/mevenide2/netbeans/ide-mojos/netbeans-deploy-plugin/src/main/java/org/codehaus/mevenide/plugin/deploy/DeployMojo.java
@@ -79,7 +79,7 @@ public class DeployMojo extends AbstractMojo {
         allowedPackagings.add("ejb");
         allowedPackagings.add("ear");
         if (!allowedPackagings.contains(project.getPackaging())) {
-            throw new MojoFailureException("Can only deploy war, ear or ejb packaged projects");
+            getLog().warn("You attempt to run run the plugin on a project that might not be of war, ear or ejb kind (packaging). The plugin might possibly fail.");
         }
         MavenDeployment deployment = (MavenDeployment)Lookup.getDefault().lookup(MavenDeployment.class);
         if (deployment == null) {
