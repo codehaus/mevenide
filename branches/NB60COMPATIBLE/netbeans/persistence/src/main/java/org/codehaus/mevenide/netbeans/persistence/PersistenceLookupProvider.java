8d369814ef5a62ee1b156e0a9b0204cc4d14b175
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2007-02-20 20:46:40 +0000

    copy persistence module to 60branch.

---
+++ branches/NB60COMPATIBLE/netbeans/persistence/src/main/java/org/codehaus/mevenide/netbeans/persistence/PersistenceLookupProvider.java
@@ -0,0 +1,44 @@
+/* ==========================================================================
+ * Copyright 2006 Mevenide Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * =========================================================================
+ */
+package org.codehaus.mevenide.netbeans.persistence;
+
+import org.codehaus.mevenide.netbeans.NbMavenProject;
+import org.netbeans.spi.project.LookupProvider;
+import org.openide.util.Lookup;
+import org.openide.util.lookup.Lookups;
+
+/**
+ * extending the default maven project lookup.
+ * @author  Milos Kleint (mkleint@codehaus.org)
+ */
+
+public class PersistenceLookupProvider implements LookupProvider {
+    
+    /** Creates a new instance of J2eeLookupProvider */
+    public PersistenceLookupProvider() {
+    }
+    
+    public Lookup createAdditionalLookup(Lookup context) {
+//        // if there's more items later, just do a proxy..
+        NbMavenProject prj = context.lookup(NbMavenProject.class);
+        assert prj != null;
+        return Lookups.fixed(new Object[] {
+            new MavenPersistenceProvider(prj)
+        });
+    }
+
+}
---
+++ branches/NB60COMPATIBLE/netbeans/persistence/src/main/java/org/codehaus/mevenide/netbeans/persistence/PersistenceScopeImpl.java
@@ -0,0 +1,71 @@
+/* ==========================================================================
+ * Copyright 2006 Mevenide Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * =========================================================================
+ */
+package org.codehaus.mevenide.netbeans.persistence;
+
+import org.netbeans.api.java.classpath.ClassPath;
+import org.netbeans.modules.j2ee.persistence.spi.PersistenceClassPathProvider;
+import org.netbeans.modules.j2ee.persistence.spi.PersistenceLocationProvider;
+import org.netbeans.modules.j2ee.persistence.spi.PersistenceScopeImplementation;
+import org.openide.filesystems.FileObject;
+
+/**
+ * Maven2 Implementation of 
+ * <CODE>org.netbeans.modules.j2ee.persistence.spi.PersistenceScopeImplementation</CODE> 
+ * @author Daniel Mohni
+ */
+public class PersistenceScopeImpl implements PersistenceScopeImplementation
+{
+    private PersistenceLocationProvider  locationProvider  = null;
+    private PersistenceClassPathProvider classpathProvider = null;
+    
+    /**
+     * Creates a new instance of PersistenceScopeImpl
+     * @param locProvider the PersistenceLocationProvider instance to use for lookups
+     * @param cpProvider the PersistenceClasspathProvider instance to use for lookups
+     */
+    public PersistenceScopeImpl(PersistenceLocationProvider locProvider,
+            PersistenceClassPathProvider cpProvider)
+    {
+        this.locationProvider  = locProvider;
+        this.classpathProvider = cpProvider;
+    }
+    
+    /**
+     * property access to the project's persistence.xml
+     * @return the persistence.xml file used in this project or null
+     */
+    public FileObject getPersistenceXml()
+    {
+        FileObject location = locationProvider.getLocation();
+        if (location == null)
+        {
+            return null;
+        }
+        return location.getFileObject("persistence.xml"); // NOI18N
+        
+    }
+    
+    /**
+     * property access to the persistence project classpath
+     * @return the classpath provided by the PersistenceClasspathProvider
+     */
+    public ClassPath getClassPath()
+    {
+        return classpathProvider.getClassPath();
+    }
+    
+}
