022fd78ee786f214b18cd5100e029cab14620ecc
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2007-09-04 17:05:55 +0000

    no longer necessary to return a special, fake auxiliary configuration to satisfy junit.

--- trunk/mevenide2/netbeans/nb-project/src/main/java/org/codehaus/mevenide/netbeans/M2AuxilaryConfigImpl.java
+++ trunk/mevenide2/netbeans/nb-project/src/main/java/org/codehaus/mevenide/netbeans/M2AuxilaryConfigImpl.java
@@ -19,20 +19,10 @@ package org.codehaus.mevenide.netbeans;
 
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.List;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.model.Dependency;
-import org.apache.maven.model.Model;
-import org.codehaus.mevenide.netbeans.api.PluginPropertyUtils;
-import org.codehaus.mevenide.netbeans.embedder.writer.WriterUtils;
 import org.codehaus.plexus.util.StringOutputStream;
 import org.netbeans.api.project.ProjectManager;
 import org.netbeans.spi.project.AuxiliaryConfiguration;
 import org.openide.ErrorManager;
-import org.openide.filesystems.FileObject;
-import org.openide.util.Exceptions;
 import org.openide.util.Mutex;
 import org.openide.xml.XMLUtil;
 import org.w3c.dom.Document;
@@ -48,21 +38,6 @@ import org.xml.sax.SAXException;
  */
 public class M2AuxilaryConfigImpl implements AuxiliaryConfiguration {
 
-    //copied from junit module..
-    /** */
-    private static final String JUNIT_NAMESPACE_URI
-            = "http://www.netbeans.org/ns/junit/1"; //NOI18N
-    /** */
-    private static final String JUNIT_VERSION_ELEM_NAME = "junit-version"; //NOI18N
-    /** */
-    private static final String JUNIT_VERSION_ATTR_NAME = "value"; //NOI18N
-    
-    private static final String JUNIT3 = "junit3"; //NOI18N
-    private static final String JUNIT4 = "junit4"; //NOI18N
-    
-    private static final String JUNIT_GR_ART = "junit"; //NOI18N
-
-    
     private static final String AUX_CONFIG = "AuxilaryConfiguration"; //NOI18N
     private NbMavenProject project;
     
@@ -73,30 +48,6 @@ public class M2AuxilaryConfigImpl implements AuxiliaryConfiguration {
     
     public Element getConfigurationFragment(final String elementName, final String namespace, boolean shared) {
         if (shared) {
-            //somewhat hack... generate what the junit module expects based on the pom dependency section.
-            if (JUNIT_NAMESPACE_URI.equals(namespace) && JUNIT_VERSION_ELEM_NAME.equals(elementName)) {
-                List<Artifact> arts = project.getOriginalMavenProject().getTestArtifacts();
-                Artifact artifact = null;
-                for (Artifact art : arts) {
-                    if (JUNIT_GR_ART.equals(art.getGroupId()) && JUNIT_GR_ART.equals(art.getArtifactId())) {
-                        artifact = art;
-                        break;
-                    }
-                }
-                if (artifact != null) {
-                    String version = artifact.getVersion();
-                    if (version.startsWith("3.")) { //NOI18N
-                        return createJUnitElement(JUNIT3);
-                    } 
-                    else if (version.startsWith("4.")) {
-                        return createJUnitElement(JUNIT4); //NOI18N
-                    } else {
-                        ErrorManager.getDefault().log("AuxilaryConfiguration: Unknown version of junit artifact found in project:" + version); //NOI18N
-                    }
-                }
-                // do not print the error message now, just prent we'return just not set.
-                return null;
-            } 
             ErrorManager.getDefault().log("Maven2 support doesn't support shared custom configurations. Element was:" + elementName + " , namespace:" + namespace); //NOI18N
             return null;
         }
@@ -120,40 +71,6 @@ public class M2AuxilaryConfigImpl implements AuxiliaryConfiguration {
     
     public void putConfigurationFragment(final Element fragment, boolean shared) throws IllegalArgumentException {
         if (shared) {
-            if (JUNIT_NAMESPACE_URI.equals(fragment.getNamespaceURI())) {
-                //somewhat hack... update pom based on the junit module data.
-                String attr = fragment.getAttribute(JUNIT_VERSION_ATTR_NAME);
-                List<Artifact> arts = project.getOriginalMavenProject().getTestArtifacts();
-                Artifact artifact = null;
-                for (Artifact art : arts) {
-                    if (JUNIT_GR_ART.equals(art.getGroupId()) && JUNIT_GR_ART.equals(art.getArtifactId())) {
-                        artifact = art;
-                        break;
-                    }
-                }
-                if (artifact != null) {
-                    String ver = artifact.getVersion();
-                    if ((attr.equals(JUNIT4) && ver.startsWith("4.")) || //NOI18N
-                        (attr.equals(JUNIT3) && ver.startsWith("3."))) { //NOI18N
-                        //don't update anything, somewhere the stuff is correctly set.
-                        return;
-                    }
-                }
-                FileObject fo = project.getProjectDirectory().getFileObject("pom.xml"); //NOI18N
-                Model model = WriterUtils.loadModel(fo); //NOI18N
-                if (model != null) {
-                    Dependency dep = PluginPropertyUtils.checkModelDependency(model,JUNIT_GR_ART,JUNIT_GR_ART, true);
-                    dep.setVersion(attr.equals(JUNIT4) ? "4.1" : "3.8.2"); //NOI18N
-                    dep.setScope("test"); //NOI18N
-                    //TODO how to upgrade always to a version that is correct in terms of junit support?
-                    try {
-                        WriterUtils.writePomModel(fo, model);
-                    } catch (IOException ex) {
-                        Exceptions.printStackTrace(ex);
-                    }
-                }                
-                return;
-            } 
             ErrorManager.getDefault().log("Maven2 support doesn't support shared custom configurations. Element was:" + fragment.getNodeName()); //NOI18N
             return;
         }
@@ -250,33 +167,5 @@ public class M2AuxilaryConfigImpl implements AuxiliaryConfiguration {
             }
         }
         return result;
-    }
-    
-    private Element createJUnitElement(String version) {
-        Element toRet = null;
-        Document doc = createXmlDocument();
-        if (doc != null) {
-            toRet = doc.createElementNS(
-                    JUNIT_NAMESPACE_URI,
-                    JUNIT_VERSION_ELEM_NAME);
-            toRet.setAttribute(JUNIT_VERSION_ATTR_NAME,
-                    version);
-        }
-        return toRet;
-    }
-    
-    /**
-     * Creates a new DOM document.
-     *
-     * @return  created document, or {@code null} if the document
-     *          could not be created
-     */
-    private Document createXmlDocument() {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        try {
-            return factory.newDocumentBuilder().newDocument();
-        } catch (ParserConfigurationException ex) {
-            return null;
-        }
     }
 }
