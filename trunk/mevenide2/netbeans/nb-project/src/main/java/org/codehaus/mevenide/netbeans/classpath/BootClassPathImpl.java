ce8b4087d78fd36a63c2b90f4e0895af10dc5426
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2007-10-02 08:24:09 +0000

    #104974 workaround: add "netbeans.hint.jdkPlatform" property to maven pom to designate the NetBeans Java Platform definition to use in editor. Will not influence the Maven build itself.

--- trunk/mevenide2/netbeans/nb-project/src/main/java/org/codehaus/mevenide/netbeans/classpath/BootClassPathImpl.java
+++ trunk/mevenide2/netbeans/nb-project/src/main/java/org/codehaus/mevenide/netbeans/classpath/BootClassPathImpl.java
@@ -17,33 +17,45 @@
 
 package org.codehaus.mevenide.netbeans.classpath;
 
+import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import org.codehaus.mevenide.netbeans.NbMavenProject;
+import org.codehaus.mevenide.netbeans.api.Constants;
+import org.codehaus.mevenide.netbeans.api.ProjectURLWatcher;
 import org.netbeans.spi.java.classpath.ClassPathImplementation;
 import org.netbeans.spi.java.classpath.support.ClassPathSupport;
 
 import org.netbeans.api.java.platform.JavaPlatform;
 import org.netbeans.api.java.platform.JavaPlatformManager;
 import org.netbeans.api.java.classpath.ClassPath;
+import org.netbeans.api.java.platform.Specification;
 import org.netbeans.spi.java.classpath.PathResourceImplementation;
+import org.openide.util.WeakListeners;
 
 /**
  *
  * @author  Milos Kleint (mkleint@codehaus.org)
  */
-final class BootClassPathImpl implements ClassPathImplementation {
+public final class BootClassPathImpl implements ClassPathImplementation, PropertyChangeListener {
 
     private List<? extends PathResourceImplementation> resourcesCache;
     private PropertyChangeSupport support = new PropertyChangeSupport(this);
+    private NbMavenProject project;
+    private String lastHintValue = null;
+    private boolean activePlatformValid = true;
+    private JavaPlatformManager platformManager;
     
 //    private String lastNonDefault = null;
 //    private String lastNonDefaultPlatform = null;
 
-    public BootClassPathImpl () {
+
+    BootClassPathImpl(NbMavenProject project) {
+        this.project = project;
     }
 
     public synchronized List<? extends PathResourceImplementation> getResources() {
@@ -73,10 +85,89 @@ final class BootClassPathImpl implements ClassPathImplementation {
     }
 
     private JavaPlatform findActivePlatform () {
-        JavaPlatformManager pm = JavaPlatformManager.getDefault();
+        activePlatformValid = true;
+        if (platformManager == null) {
+            platformManager = JavaPlatformManager.getDefault();
+            platformManager.addPropertyChangeListener(WeakListeners.propertyChange(this, platformManager));
+            ProjectURLWatcher watch = project.getLookup().lookup(ProjectURLWatcher.class);
+            watch.addPropertyChangeListener(this);
+            
+        }                
+        String val = project.getOriginalMavenProject().getProperties().getProperty(Constants.HINT_JDK_PLATFORM);
+        lastHintValue = val;
+        JavaPlatform plat = getActivePlatform(val);
+        if (plat == null) {
+            //TODO report how?
+            plat = platformManager.getDefaultPlatform();
+            activePlatformValid = false;
+        }
         //TODO how to find custom compiler in m2?
         //Invalid platform ID or default platform
-        return pm.getDefaultPlatform();
+        return plat;
     }
+    
+    /**
+     * Returns the active platform used by the project or null if the active
+     * project platform is broken.
+     * @param activePlatformId the name of platform used by Ant script or null
+     * for default platform.
+     * @return active {@link JavaPlatform} or null if the project's platform
+     * is broken
+     */
+    public static JavaPlatform getActivePlatform (final String activePlatformId) {
+        final JavaPlatformManager pm = JavaPlatformManager.getDefault();
+        if (activePlatformId == null) {
+            return pm.getDefaultPlatform();
+        }
+        else {
+            JavaPlatform[] installedPlatforms = pm.getPlatforms(null, new Specification ("j2se",null));   //NOI18N
+            for (int i=0; i<installedPlatforms.length; i++) {
+                String antName = (String) installedPlatforms[i].getProperties().get("platform.ant.name");        //NOI18N
+                if (antName != null && antName.equals(activePlatformId)) {
+                    return installedPlatforms[i];
+                }
+            }
+            return null;
+        }
+    }
+
+    public void propertyChange(PropertyChangeEvent evt) {
+        String newVal = project.getOriginalMavenProject().getProperties().getProperty(Constants.HINT_JDK_PLATFORM);
+        if (evt.getSource() == project && evt.getPropertyName().equals(NbMavenProject.PROP_PROJECT)) {
+            //Active platform was changed
+            if ( (newVal == null && lastHintValue != null) || (newVal != null && !newVal.equals(lastHintValue))) {
+                resetCache ();
+            }
+        }
+        else if (evt.getSource() == platformManager && 
+                JavaPlatformManager.PROP_INSTALLED_PLATFORMS.equals(evt.getPropertyName()) 
+                && lastHintValue != null) {
+            lastHintValue = newVal;
+            //Platform definitions were changed, check if the platform was not resolved or deleted
+            if (activePlatformValid) {
+                if (getActivePlatform (lastHintValue) == null) {
+                    //the platform was removed
+                    resetCache();
+                }
+            }
+            else {
+                if (getActivePlatform (lastHintValue) != null) {
+                    //platform was added
+                    resetCache();
+                }
+            }
+        }
+    }
+    
+    /**
+     * Resets the cache and firesPropertyChange
+     */
+    private void resetCache () {
+        synchronized (this) {
+            resourcesCache = null;
+        }
+        support.firePropertyChange(PROP_RESOURCES, null, null);
+    }
+    
     
 }
