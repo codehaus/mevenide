d1f2a946bb98bf253da99e6eebbc744b01ac3b6e
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2007-10-05 08:39:38 +0000

    #117869 react to changes in icon annotations on project root folder.

--- mevenide2/netbeans/nb-project/src/main/java/org/codehaus/mevenide/netbeans/nodes/MavenProjectNode.java
+++ mevenide2/netbeans/nb-project/src/main/java/org/codehaus/mevenide/netbeans/nodes/MavenProjectNode.java
@@ -22,7 +22,10 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
 import javax.swing.AbstractAction;
@@ -54,6 +57,10 @@ import org.openide.actions.FindAction;
 import org.openide.actions.ToolsAction;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileStateInvalidException;
+import org.openide.filesystems.FileStatusEvent;
+import org.openide.filesystems.FileStatusListener;
+import org.openide.filesystems.FileSystem;
+import org.openide.filesystems.FileUtil;
 import org.openide.filesystems.Repository;
 import org.openide.loaders.DataFolder;
 import org.openide.loaders.DataObject;
@@ -62,20 +69,30 @@ import org.openide.loaders.FolderLookup;
 import org.openide.nodes.AbstractNode;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
+import org.openide.util.RequestProcessor;
 import org.openide.util.Utilities;
 import org.openide.util.actions.SystemAction;
-import org.openide.util.lookup.Lookups;
 
 
 /** A node to represent project root.
  *
  * @author Milos Kleint (mkleint@codehaus.org)
  */
-public class MavenProjectNode extends AbstractNode {
+public class MavenProjectNode extends AbstractNode implements FileStatusListener, Runnable {
      
      private NbMavenProject project;
      private ProjectInformation info;
      private ProblemReporter reporter;
+
+
+        private Set<FileObject> files;
+        private Map<FileSystem,FileStatusListener> fileSystemListeners;
+        private RequestProcessor.Task task;
+        private final Object privateLock = new Object();
+        private boolean iconChange;
+        private boolean nameChange;
+
+
      public MavenProjectNode(Lookup lookup, NbMavenProject proj) {
         super(NodeFactorySupport.createCompositeChildren(proj, "Projects/org-codehaus-mevenide-netbeans/Nodes"), lookup); //NOI18N
         this.project = proj;
@@ -101,8 +118,83 @@ public class MavenProjectNode extends AbstractNode {
                 });
             }
         });
+        setFiles(Collections.<FileObject>singleton(proj.getProjectDirectory()));
     }
-    
+
+//----------------------------------------------------
+// icon annotation change related, copied from j2se project.
+// eventually this should end up in a sort of SPI and be shared across project types
+
+        
+        protected final void setFiles(Set<FileObject> files) {
+            if (fileSystemListeners != null) {
+                for (Map.Entry<FileSystem,FileStatusListener> e : fileSystemListeners.entrySet()) {
+                    e.getKey().removeFileStatusListener(e.getValue());
+                }
+            }
+            
+            fileSystemListeners = new HashMap<FileSystem,FileStatusListener>();
+            this.files = files;
+            if (files == null) {
+                return;
+            }
+            
+            Set<FileSystem> hookedFileSystems = new HashSet<FileSystem>();
+            for (FileObject fo : files) {
+                try {
+                    FileSystem fs = fo.getFileSystem();
+                    if (hookedFileSystems.contains(fs)) {
+                        continue;
+                    }
+                    hookedFileSystems.add(fs);
+                    FileStatusListener fsl = FileUtil.weakFileStatusListener(this, fs);
+                    fs.addFileStatusListener(fsl);
+                    fileSystemListeners.put(fs, fsl);
+                } catch (FileStateInvalidException e) {
+                    ErrorManager err = ErrorManager.getDefault();
+                    err.annotate(e, ErrorManager.UNKNOWN, "Cannot get " + fo + " filesystem, ignoring...", null, null, null); // NO18N
+                    err.notify(ErrorManager.INFORMATIONAL, e);
+                }
+            }
+        }
+
+        public void run() {
+            boolean fireIcon;
+            boolean fireName;
+            synchronized (privateLock) {
+                fireIcon = iconChange;
+                fireName = nameChange;
+                iconChange = false;
+                nameChange = false;
+            }
+            if (fireIcon) {
+                fireIconChange();
+                fireOpenedIconChange();
+            }
+            if (fireName) {
+                fireDisplayNameChange(null, null);
+            }
+        }
+        
+        public void annotationChanged(FileStatusEvent event) {
+            if (task == null) {
+                task = RequestProcessor.getDefault().create(this);
+            }
+            
+            synchronized (privateLock) {
+                if ((iconChange == false && event.isIconChange()) || (nameChange == false && event.isNameChange())) {
+                    for (FileObject fo : files) {
+                        if (event.hasChanged(fo)) {
+                            iconChange |= event.isIconChange();
+                            nameChange |= event.isNameChange();
+                        }
+                    }
+                }
+            }
+            
+            task.schedule(50); // batch by 50 ms
+        }
+//----------------------------------------------------
     
     @Override
     public String getDisplayName() {
@@ -274,40 +366,6 @@ public class MavenProjectNode extends AbstractNode {
             OpenProjects.getDefault().close(arr);
         }
     }
-
-//    private class LifecycleMapTest extends AbstractAction {
-//        public LifecycleMapTest() {
-//            putValue(Action.NAME, "Lifecycle map test.");
-//        }
-//
-//        public void actionPerformed(ActionEvent e) {
-//            try {
-//                Map map = EmbedderFactory.getOnlineEmbedder().getLifecycleMappings(
-//                        EmbedderFactory.getOnlineEmbedder().readProject(project.getPOMFile()), "default", "install");
-//                Iterator it = map.entrySet().iterator();
-//                while (it.hasNext()) {
-//                    Map.Entry elem = (Map.Entry) it.next();
-//                    System.out.println("elem=" + elem.getKey());
-//                    List mojos = (List)elem.getValue();
-//                    Iterator it2 = mojos.iterator();
-//                    while (it2.hasNext()) {
-//                        MojoExecution exec = (MojoExecution) it2.next();
-//                        System.out.println(" value=" + exec.getExecutionId() + " " + exec.getMojoDescriptor().getFullGoalName());
-//                        System.out.println(" config =" + exec.getConfiguration());
-//                    }
-//                }
-//            } catch (PluginNotFoundException ex) {
-//                ex.printStackTrace();
-//            } catch (BuildFailureException ex) {
-//                ex.printStackTrace();
-//            } catch (ProjectBuildingException ex) {
-//                ex.printStackTrace();
-//            } catch (LifecycleExecutionException ex) {
-//                ex.printStackTrace();
-//            }
-//            
-//        }
-//    }
     
     private class ShowProblemsAction extends AbstractAction {
         
