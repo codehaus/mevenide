7be0b1def9afdf16ffcd74cd691cead56ab0529f
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2008-01-30 09:51:43 +0000

    rewrite repo utils to use nexus rather then archiva. ExtractPluginInfo not working yet.

--- trunk/mevenide2/netbeans/maven-repo-utils/src/main/java/org/codehaus/mevenide/repoutils/IndexRepository.java
+++ trunk/mevenide2/netbeans/maven-repo-utils/src/main/java/org/codehaus/mevenide/repoutils/IndexRepository.java
@@ -10,24 +10,7 @@
 package org.codehaus.mevenide.repoutils;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import org.apache.maven.archiva.discoverer.ArtifactDiscoverer;
-import org.apache.maven.archiva.discoverer.filter.AcceptAllArtifactFilter;
-import org.apache.maven.archiva.indexer.RepositoryArtifactIndex;
-import org.apache.maven.archiva.indexer.RepositoryArtifactIndexFactory;
-import org.apache.maven.archiva.indexer.record.RepositoryIndexRecordFactory;
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.InvalidArtifactRTException;
-import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.repository.DefaultArtifactRepository;
-import org.apache.maven.artifact.repository.layout.DefaultRepositoryLayout;
-import org.apache.maven.artifact.resolver.filter.ArtifactFilter;
-import org.codehaus.classworlds.ClassWorld;
-import org.codehaus.plexus.embed.Embedder;
+import org.codehaus.mevenide.indexer.NexusRepositoryIndexserImpl;
 
 
 /**
@@ -40,65 +23,22 @@ public class IndexRepository {
      * @param args
      * 0 - repository index directory
      * 1 - repository root directory
+     * 2 - repository id
      */
     public static void main(String[] args) {
-
-        try {
-            Embedder embedder = new Embedder();
-            RepositoryArtifactIndexFactory indexFactory;
-            RepositoryArtifactIndex defaultIndex;
-            RepositoryIndexRecordFactory recordFactory;
-            ArtifactDiscoverer discoverer;
-            ArtifactFactory artifactFactory;
-
-            embedder.start();
-            indexFactory = (RepositoryArtifactIndexFactory) embedder.lookup(RepositoryArtifactIndexFactory.ROLE, "lucene"); //NOI18N
-            discoverer = (ArtifactDiscoverer) embedder.lookup(ArtifactDiscoverer.ROLE, "default" ); //NOI18N
-            recordFactory = (RepositoryIndexRecordFactory) embedder.lookup(RepositoryIndexRecordFactory.ROLE, "standard"); //NOI18N
-            artifactFactory = (ArtifactFactory) embedder.lookup(ArtifactFactory.ROLE);
+            if (args.length < 2) {
+                throw new IllegalArgumentException("Must have 2 parameters [repo index directory] [repo root directory] [repo id]");
+            }
             File basedir = new File(args[0]);
             if (basedir.exists()) {
                 throw new IllegalArgumentException(args[0] + " folder already exist");
             }
             basedir.mkdirs();
-            defaultIndex = indexFactory.createStandardIndex(new File(basedir, ".index")); //NOI18N
             File repodir = new File(args[1]);
             if (!repodir.exists()) {
                 throw new IllegalArgumentException(args[1] + " folder doesn't exist");
             }
-
-            //discover artifacts
-            ArtifactFilter filter = new AcceptAllArtifactFilter();
-            ArtifactRepository repository = new DefaultArtifactRepository("central", "file://" + repodir.getAbsolutePath(), new DefaultRepositoryLayout());
-            final List artifacts = discoverer.discoverArtifacts(repository, null, filter);
-
-            //now index..
-            int size = artifacts.size();
-            System.out.println("Number of Artifacts: " + size);
-            int count = 0;
-            Collection records = new ArrayList(250);
-            for (Iterator i = artifacts.iterator(); i.hasNext();) {
-                Artifact artifact = (Artifact) i.next();
-                i.remove();
-                count++;
-                try {
-                    records.add(recordFactory.createRecord(artifact));
-                } catch (InvalidArtifactRTException e) {
-                    //TODO.. some better handling..
-                    e.printStackTrace();
-                }
-                if (count % 200 == 0) {
-                    defaultIndex.indexRecords(records);
-                    records.clear();
-                    System.out.println("... processed " + count);
-                }
-                if (count % 800 == 0) {
-                    System.gc();
-                }
-            }
-            defaultIndex.indexRecords(records);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            NexusRepositoryIndexserImpl index = new NexusRepositoryIndexserImpl();
+            index.indexRepo(args[2], repodir, basedir);
     }
 }
