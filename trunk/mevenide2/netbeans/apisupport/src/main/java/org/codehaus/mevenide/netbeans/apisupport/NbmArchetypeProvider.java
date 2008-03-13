f28a27d9e7c3e6eb3658b6cf538cab42b8c49971
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2008-03-13 20:18:15 +0000

    use latest version of archetypes

--- trunk/mevenide2/netbeans/apisupport/src/main/java/org/codehaus/mevenide/netbeans/apisupport/NbmArchetypeProvider.java
+++ trunk/mevenide2/netbeans/apisupport/src/main/java/org/codehaus/mevenide/netbeans/apisupport/NbmArchetypeProvider.java
@@ -34,14 +34,14 @@ public class NbmArchetypeProvider implements ArchetypeProvider {
         Archetype a = new Archetype(false, true);
         a.setGroupId("org.codehaus.mojo.archetypes"); //NOI18N
         a.setArtifactId("nbm-archetype"); //NOI18N
-        a.setVersion("1.0"); //NOI18N
+        a.setVersion("1.0.1"); //NOI18N
         a.setName(NbBundle.getMessage(NbmArchetypeProvider.class, "TIT_NBM_Archetype"));
         a.setDescription(NbBundle.getMessage(NbmArchetypeProvider.class, "DESC_NBM_Archetype"));
         lst.add(a);
         a = new Archetype(false, true);
         a.setGroupId("org.codehaus.mojo.archetypes"); //NOI18N
         a.setArtifactId("netbeans-platform-app-archetype"); //NOI18N
-        a.setVersion("1.0"); //NOI18N
+        a.setVersion("1.0.1"); //NOI18N
         a.setName(NbBundle.getMessage(NbmArchetypeProvider.class, "TIT_Platform_Application_Archetype"));
         a.setDescription(NbBundle.getMessage(NbmArchetypeProvider.class, "DESC_Platform_Application_Archetype"));
         lst.add(a);
