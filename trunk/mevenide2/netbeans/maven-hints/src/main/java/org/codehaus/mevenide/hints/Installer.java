e23cc7c00d6d89a7191cdb98658e495cb94436b9
anuradha <anuradha@c6c22482-0310-0410-b13f-f1d71ac210c7>
2008-03-17 07:11:08 +0000

    new maven hints module stub

---
+++ trunk/mevenide2/netbeans/maven-hints/src/main/java/org/codehaus/mevenide/hints/Installer.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.codehaus.mevenide.hints;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Set;
+import org.openide.modules.ModuleInstall;
+
+/**
+ * Manages a module's lifecycle. Remember that an installer is optional and
+ * often not needed at all.
+ */
+public class Installer extends ModuleInstall {
+
+    /**
+     * screw friend dependency.
+     */ 
+    @Override
+    public void validate() throws IllegalStateException {
+        try {
+            java.lang.Class main = java.lang.Class.forName("org.netbeans.core.startup.Main", false,  //NOI18N
+                    Thread.currentThread().getContextClassLoader());
+            Method meth = main.getMethod("getModuleSystem", new Class[0]); //NOI18N
+            Object moduleSystem = meth.invoke(null, new Object[0]);
+            meth = moduleSystem.getClass().getMethod("getManager", new Class[0]); //NOI18N
+            Object mm = meth.invoke(moduleSystem, new Object[0]);
+            Method moduleMeth = mm.getClass().getMethod("get", new Class[] {String.class}); //NOI18N
+            Object persistence = moduleMeth.invoke(mm, "org.netbeans.core.startup"); //NOI18N
+            if (persistence != null) {
+                Field frField = persistence.getClass().getSuperclass().getDeclaredField("friendNames"); //NOI18N
+                frField.setAccessible(true);
+                Set friends = (Set)frField.get(persistence);
+                friends.add("org.codehaus.mevenide.hints"); //NOI18N
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            new IllegalStateException("Cannot fix dependencies for org.codehaus.mevenide.hints. " + //NOI18N
+                    "Please log a report at http://jira.codehaus.org/browse/MEVENIDE"); //NOI18N
+        }
+    }
+}
