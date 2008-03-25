91f4515478348176a182539c827773cd5c980e71
anuradha <anuradha@c6c22482-0310-0410-b13f-f1d71ac210c7>
2008-03-25 08:11:50 +0000

    fixing license header

--- mevenide2/netbeans/lifecycle-build-plan/src/main/java/org/codehaus/mevenide/buildplan/BuildPlanView.java
+++ mevenide2/netbeans/lifecycle-build-plan/src/main/java/org/codehaus/mevenide/buildplan/BuildPlanView.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008 Anuradha.
+ *  Copyright 2008 Mevenide Team.
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
