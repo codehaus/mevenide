d390805700bb20b84e03930b133f6821c9bf52a8
anuradha <anuradha@c6c22482-0310-0410-b13f-f1d71ac210c7>
2008-03-27 10:11:39 +0000

    using Model to load Profiles

--- trunk/mevenide2/netbeans/nb-project/src/main/java/org/codehaus/mevenide/netbeans/api/ModelUtils.java
+++ trunk/mevenide2/netbeans/nb-project/src/main/java/org/codehaus/mevenide/netbeans/api/ModelUtils.java
@@ -16,17 +16,19 @@
  */
 package org.codehaus.mevenide.netbeans.api;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import org.apache.maven.execution.DefaultMavenExecutionRequest;
-import org.apache.maven.execution.MavenExecutionRequest;
-import org.apache.maven.execution.MavenExecutionResult;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.Profile;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.mevenide.netbeans.embedder.EmbedderFactory;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.Exceptions;
 
 /**
  * Various maven model related utilities.
@@ -42,29 +44,12 @@ public final class ModelUtils {
      * @return
      */
     public static List<String> retrieveAllProfiles(MavenProject mavenProject) {
-        Set<String> values = new HashSet<String>();
+        Set<String> profileIds = new HashSet<String>();
         MavenProject root = getRootMavenProject(mavenProject);
 
-        /** BEGIN
-         * Sometimes Need load project again  to get getCollectedProjects correctly 
-         * may be project.getParent() related. to: Milos any Idea ?
-         */
-        MavenExecutionRequest req = new DefaultMavenExecutionRequest();
-        req.setPomFile(root.getFile().getAbsolutePath());
-        MavenExecutionResult res = EmbedderFactory.getProjectEmbedder().readProjectWithDependencies(req);
-        root = res.getProject();
-        /*END*************************************************************/
+        exteactProfiles(profileIds, root.getBasedir(), root.getModel());
 
-        List<MavenProject> mps = root.getCollectedProjects();
-        for (MavenProject mp : mps) {
-
-            Model model = mp.getModel();
-            List<Profile> profiles = model.getProfiles();
-            for (Profile profile : profiles) {
-                values.add(profile.getId());
-            }
-        }
-        return new ArrayList<String>(values);
+        return new ArrayList<String>(profileIds);
     }
 
     private static MavenProject getRootMavenProject(MavenProject mavenProject) {
@@ -74,5 +59,30 @@ public final class ModelUtils {
         }
 
         return mavenProject;
+    }
+
+    private static void exteactProfiles(Set<String> profileIds, File file, Model model) {
+
+        List<Profile> profiles = model.getProfiles();
+        for (Profile profile : profiles) {
+            profileIds.add(profile.getId());
+        }
+
+        List<String> modules = model.getModules();
+        for (String name : modules) {
+            File dir = FileUtil.normalizeFile(new File(file, name));
+            File pom = FileUtil.normalizeFile(new File(dir, "pom.xml"));//NOI18N
+
+            try {
+
+                Model readModel = EmbedderFactory.getProjectEmbedder().readModel(pom);
+                exteactProfiles(profileIds, dir, readModel);
+            } catch (XmlPullParserException ex) {
+                Exceptions.printStackTrace(ex);
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+        }
+
     }
 }
