7db8551162a6be2b838d27473b9cc7ed0b65d567
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2008-04-03 12:42:29 +0000

    optimize performance and make sure the run.single, debug.single actions are enabled for web pages only.

--- mevenide2/netbeans/j2ee/src/main/java/org/codehaus/mevenide/netbeans/j2ee/J2eeActionsProvider.java
+++ mevenide2/netbeans/j2ee/src/main/java/org/codehaus/mevenide/netbeans/j2ee/J2eeActionsProvider.java
@@ -18,16 +18,30 @@
 package org.codehaus.mevenide.netbeans.j2ee;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import org.codehaus.mevenide.netbeans.NbMavenProject;
+import org.codehaus.mevenide.netbeans.api.ProjectURLWatcher;
 import org.codehaus.mevenide.netbeans.execute.AbstractActionGoalProvider;
+import org.netbeans.api.project.SourceGroup;
+import org.netbeans.api.project.Sources;
+import org.netbeans.spi.project.ActionProvider;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.Lookup;
 
 /**
  * j2ee specific defaults for project running and debugging..
  * @author mkleint
  */
 public class J2eeActionsProvider extends AbstractActionGoalProvider {
-    
+
+    private ArrayList<String> supported;
     /** Creates a new instance of J2eeActionsProvider */
     public J2eeActionsProvider() {
+        supported = new ArrayList<String>();
+        supported.add(ProjectURLWatcher.TYPE_WAR);
+        supported.add(ProjectURLWatcher.TYPE_EAR);
+        supported.add(ProjectURLWatcher.TYPE_EJB);
     }
     
     
@@ -37,5 +51,30 @@ public class J2eeActionsProvider extends AbstractActionGoalProvider {
         assert in != null : "no instream for " + path;  //NOI18N
         return in;
     }
-    
+
+    @Override
+    public boolean isActionEnable(String action, NbMavenProject project, Lookup lookup) {
+        if (ActionProvider.COMMAND_RUN_SINGLE.equals(action) || 
+            ActionProvider.COMMAND_DEBUG_SINGLE.equals(action)) {
+            //only enable for doc root fileobjects..
+            FileObject[] fos = extractFileObjectsfromLookup(lookup);
+            if (fos != null) {
+                Sources srcs = project.getLookup().lookup(Sources.class);
+                SourceGroup[] grp = srcs.getSourceGroups("doc_root"); //NOI18N J2EE
+                for (int i = 0; i < grp.length; i++) {
+                    String relPath = FileUtil.getRelativePath(grp[i].getRootFolder(), fos[0]);
+                    if (relPath != null) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        } else if (ActionProvider.COMMAND_RUN.equals(action) || 
+                   ActionProvider.COMMAND_DEBUG.equals(action)) {
+            //performance, don't read the xml file to figure enablement..
+            return supported.contains(project.getProjectWatcher().getPackagingType());
+        } else {
+            return false;
+        }
+    }
 }
