87d6b994cc4d9366fbefe3a1dcb2425232412c39
anuradha <anuradha@c6c22482-0310-0410-b13f-f1d71ac210c7>
2008-04-21 06:43:46 +0000

    result grouping

--- trunk/mevenide2/netbeans/nb-repo-browser/src/main/java/org/codehaus/mevenide/repository/FindResultsPanel.java
+++ trunk/mevenide2/netbeans/nb-repo-browser/src/main/java/org/codehaus/mevenide/repository/FindResultsPanel.java
@@ -17,14 +17,25 @@
 package org.codehaus.mevenide.repository;
 
 import java.awt.BorderLayout;
+import java.awt.Image;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import javax.swing.Action;
 import javax.swing.SwingUtilities;
+import org.apache.maven.artifact.Artifact;
 import org.codehaus.mevenide.indexer.api.NBVersionInfo;
 import org.codehaus.mevenide.indexer.api.QueryField;
 import org.codehaus.mevenide.indexer.api.RepositoryInfo;
 import org.codehaus.mevenide.indexer.api.RepositoryPreferences;
 import org.codehaus.mevenide.indexer.api.RepositoryQueries;
+import org.codehaus.mevenide.indexer.api.RepositoryUtil;
+import org.codehaus.mevenide.netbeans.api.CommonArtifactActions;
+import org.codehaus.mevenide.repository.dependency.AddAsDependencyAction;
 import org.openide.DialogDescriptor;
 import org.openide.DialogDisplayer;
 import org.openide.explorer.ExplorerManager;
@@ -33,6 +44,7 @@ import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
 import org.openide.nodes.Node;
 import org.openide.util.RequestProcessor;
+import org.openide.util.Utilities;
 
 /**
  *
@@ -61,11 +73,9 @@ public class FindResultsPanel extends javax.swing.JPanel implements ExplorerMana
         dd = d;
     }
 
-   
-
     void find(final List<QueryField> fields) {
         Node loadingNode = GroupListChildren.createLoadingNode();
-        Children.Array array=new Children.Array();
+        Children.Array array = new Children.Array();
         array.add(new Node[]{loadingNode});
         manager.setRootContext(new AbstractNode(array));
         RequestProcessor.getDefault().post(new Runnable() {
@@ -75,7 +85,7 @@ public class FindResultsPanel extends javax.swing.JPanel implements ExplorerMana
                 SwingUtilities.invokeLater(new Runnable() {
 
                     public void run() {
-                         manager.setRootContext(createRootNode(infos));
+                        manager.setRootContext(createRootNode(infos));
                     }
                 });
             }
@@ -143,14 +153,156 @@ private void btnModifyActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIR
     }
 
     private Node createRootNode(List<NBVersionInfo> infos) {
-        Children.Array array = new Children.Array();
-        for (NBVersionInfo i : infos) {
-            RepositoryInfo ri = RepositoryPreferences.getInstance().getRepositoryInfoById(i.getRepoId());
-            array.add(new Node[]{
-                        new VersionNode(ri, i, i.isJavadocExists(), i.isSourcesExists(), false)
-                    });
+        Node node = null;
+        Map<String, List<NBVersionInfo>> map = new HashMap<String, List<NBVersionInfo>>();
+
+        for (NBVersionInfo nbvi : infos) {
+            String key = nbvi.getGroupId() + " : " + nbvi.getArtifactId();
+            List<NBVersionInfo> get = map.get(key);
+            if (get == null) {
+                get = new ArrayList<NBVersionInfo>();
+                map.put(key, get);
+            }
+            get.add(nbvi);
         }
-        return new AbstractNode(array);
+        Set<String> keySet = map.keySet();
+        if (keySet.size() > 0) {
+            Children.Array array = new Children.Array();
+
+            List<String> keyList = new ArrayList<String>(keySet);
+            Collections.sort(keyList);
+            node = new AbstractNode(array);
+            for (String key : keyList) {
+                array.add(new Node[]{new ArtifactNode(key, map.get(key))});
+            }
+        }
+        return node;
+
+    }
+
+    private static class ArtifactNode extends AbstractNode {
+
+        private List<NBVersionInfo> versionInfos;
+
+        public ArtifactNode(String name, final List<NBVersionInfo> list) {
+            super(new Children.Keys<NBVersionInfo>() {
+
+                @Override
+                protected Node[] createNodes(NBVersionInfo arg0) {
+
+
+                    return new Node[]{new VersionNode(arg0, arg0.isJavadocExists(),
+                                arg0.isSourcesExists())
+                            };
+                }
+
+                @Override
+                protected void addNotify() {
+
+                    setKeys(list);
+                }
+            });
+            this.versionInfos = list;
+            setName(name);
+            setDisplayName(name);
+        }
+
+        @Override
+        public Image getIcon(int arg0) {
+            Image badge = Utilities.loadImage("org/codehaus/mevenide/repository/ArtifactBadge.png", true); //NOI18N
+
+            return badge;
+        }
+
+        @Override
+        public Image getOpenedIcon(int arg0) {
+            return getIcon(arg0);
+        }
+
+        public List<NBVersionInfo> getVersionInfos() {
+            return new ArrayList<NBVersionInfo>(versionInfos);
+        }
+    }
+
+    private static class VersionNode extends AbstractNode {
+
+        private NBVersionInfo nbvi;
+        private boolean hasJavadoc;
+        private boolean hasSources;
+
+        /** Creates a new instance of VersionNode */
+        public VersionNode(NBVersionInfo versionInfo, boolean javadoc, boolean source) {
+            super(Children.LEAF);
+
+            hasJavadoc = javadoc;
+            hasSources = source;
+            this.nbvi = versionInfo;
+
+            setName(versionInfo.getVersion());
+            setDisplayName(versionInfo.getVersion() + " [ " + versionInfo.getType() + (versionInfo.getClassifier() != null ? ("," + versionInfo.getClassifier()) : "") + " ] " + " - " + versionInfo.getRepoId());
+
+            setIconBaseWithExtension("org/codehaus/mevenide/repository/DependencyJar.gif"); //NOI18N
+
+        }
+
+        @Override
+        public Action[] getActions(boolean context) {
+            Artifact artifact = RepositoryUtil.createArtifact(nbvi);
+            Action[] retValue;
+            RepositoryInfo info = RepositoryPreferences.getInstance().getRepositoryInfoById(nbvi.getRepoId());
+            if (info != null && info.isRemoteDownloadable()) {
+                retValue = new Action[]{
+                            new AddAsDependencyAction(artifact),
+                            CommonArtifactActions.createFindUsages(artifact),
+                            null,
+                            CommonArtifactActions.createViewProjectHomeAction(artifact),
+                            CommonArtifactActions.createViewBugTrackerAction(artifact),
+                            CommonArtifactActions.createSCMActions(artifact)
+                        };
+
+            } else {
+
+
+                retValue = new Action[]{
+                            new AddAsDependencyAction(artifact),
+                            null,
+                            CommonArtifactActions.createFindUsages(artifact),
+                            null,
+                            CommonArtifactActions.createViewJavadocAction(artifact),
+                            CommonArtifactActions.createViewProjectHomeAction(artifact),
+                            CommonArtifactActions.createViewBugTrackerAction(artifact),
+                            CommonArtifactActions.createSCMActions(artifact)
+                        };
+            }
+            return retValue;
+        }
+
+        @Override
+        public java.awt.Image getIcon(int param) {
+            java.awt.Image retValue = super.getIcon(param);
+            if (hasJavadoc) {
+                retValue = Utilities.mergeImages(retValue,
+                        Utilities.loadImage("org/codehaus/mevenide/repository/DependencyJavadocIncluded.png"),//NOI18N
+                        12, 12);
+            }
+            if (hasSources) {
+                retValue = Utilities.mergeImages(retValue,
+                        Utilities.loadImage("org/codehaus/mevenide/repository/DependencySrcIncluded.png"),//NOI18N
+                        12, 8);
+            }
+            return retValue;
+
+        }
+
+        public NBVersionInfo getNBVersionInfo() {
+            return nbvi;
+        }
+
+        @Override
+        public String getShortDescription() {
+
+            return nbvi.toString();
+        }
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnClose;
