b4c6bdac08311357be6ac24e93bb466a068bb00e
anuradha <anuradha@c6c22482-0310-0410-b13f-f1d71ac210c7>
2008-05-08 05:45:34 +0000

    @Override

--- trunk/mevenide2/netbeans/nb-project/src/test/java/org/codehaus/mevenide/netbeans/execute/OutputHandlerTest.java
+++ trunk/mevenide2/netbeans/nb-project/src/test/java/org/codehaus/mevenide/netbeans/execute/OutputHandlerTest.java
@@ -14,7 +14,6 @@
  *  limitations under the License.
  * =========================================================================
  */
-
 package org.codehaus.mevenide.netbeans.execute;
 
 import java.io.StringWriter;
@@ -32,44 +31,45 @@ import org.openide.windows.OutputWriter;
  * @author mkleint
  */
 public class OutputHandlerTest extends TestCase {
-    
+
     public OutputHandlerTest(String testName) {
         super(testName);
     }
 
     public static Test suite() {
         TestSuite suite = new TestSuite(OutputHandlerTest.class);
-        
+
         return suite;
     }
 
-   public void testSequence() {
-       HashMap procs = new HashMap();
-       HashSet set = new HashSet();
-       TestProcessor proc = new TestProcessor();
-       set.add(proc);
-       procs.put("mojo-execute#test:test", set);
-       OutputHandler handler = new OutputHandler();
-       handler.setup(procs, new NullOutputWriter(), new NullOutputWriter());
-       assertFalse(proc.processing);
-       handler.startEvent("mojo-execute", "test:xxx", 0);
-       assertFalse(proc.processing);
-       handler.startEvent("mojo-execute", "test:test", 0);
-       assertTrue(proc.processing);
-       handler.error("xxx");
-       handler.endEvent("mojo-execute", "test:test", 0);
-       assertFalse(proc.processing);
-       handler.error("xxx");
+    public void testSequence() {
+        HashMap procs = new HashMap();
+        HashSet set = new HashSet();
+        TestProcessor proc = new TestProcessor();
+        set.add(proc);
+        procs.put("mojo-execute#test:test", set);
+        OutputHandler handler = new OutputHandler();
+        handler.setup(procs, new NullOutputWriter(), new NullOutputWriter());
+        assertFalse(proc.processing);
+        handler.startEvent("mojo-execute", "test:xxx", 0);
+        assertFalse(proc.processing);
+        handler.startEvent("mojo-execute", "test:test", 0);
+        assertTrue(proc.processing);
+        handler.error("xxx");
+        handler.endEvent("mojo-execute", "test:test", 0);
+        assertFalse(proc.processing);
+        handler.error("xxx");
 //       fail();
-   } 
-   
-   
-   private class TestProcessor implements OutputProcessor {
-       boolean processing = false;
+    }
+
+    private class TestProcessor implements OutputProcessor {
+
+        boolean processing = false;
+
         public String[] getRegisteredOutputSequences() {
-            return new String[] {
-                "mojo-execute#test:test"
-            };
+            return new String[]{
+                        "mojo-execute#test:test"
+                    };
         }
 
         public void processLine(String line, OutputVisitor visitor) {
@@ -89,94 +89,122 @@ public class OutputHandlerTest extends TestCase {
         public void sequenceFail(String sequenceId, OutputVisitor visitor) {
             processing = false;
         }
-       
-   }
-   
-    
-   private class NullOutputWriter extends OutputWriter {
-       
-       NullOutputWriter() {
-           super(new StringWriter());
-       }
-       
-        public void println(String string, OutputListener outputListener) throws IOException {}
+    }
 
-        public void reset() throws IOException {}
+    private class NullOutputWriter extends OutputWriter {
 
-        public void print(Object obj) {}
+        NullOutputWriter() {
+            super(new StringWriter());
+        }
 
-        public void println(Object x) {}
+        public void println(String string, OutputListener outputListener) throws IOException {
+        }
 
-        public void println(boolean x) {}
+        public void reset() throws IOException {
+        }
 
+        @Override
+        public void print(Object obj) {
+        }
+
+        @Override
+        public void println(Object x) {
+        }
+
+        @Override
+        public void println(boolean x) {
+        }
+
+        @Override
         public void print(boolean b) {
         }
 
+        @Override
         public void print(double d) {
         }
 
+        @Override
         public void println(double x) {
         }
 
+        @Override
         public void println(char x) {
         }
 
+        @Override
         public void print(char c) {
         }
 
+        @Override
         public void write(char[] buf, int off, int len) {
         }
 
+        @Override
         public void print(float f) {
         }
 
+        @Override
         public void println(float x) {
         }
 
+        @Override
         public void print(String s) {
         }
 
+        @Override
         public void println(String x) {
         }
 
+        @Override
         public void write(String s) {
         }
 
+        @Override
         public void print(int i) {
         }
 
+        @Override
         public void println(int x) {
         }
 
+        @Override
         public void write(int c) {
         }
 
+        @Override
         public void println(String s, OutputListener l, boolean important) throws IOException {
         }
 
+        @Override
         public void print(long l) {
         }
 
+        @Override
         public void println(long x) {
         }
 
+        @Override
         public void println(char[] x) {
         }
 
+        @Override
         public void print(char[] s) {
         }
 
+        @Override
         public void write(char[] buf) {
         }
 
+        @Override
         public void write(String s, int off, int len) {
         }
 
+        @Override
         public void println() {
         }
 
+        @Override
         protected void setError() {
         }
-   }
-    
+    }
 }
