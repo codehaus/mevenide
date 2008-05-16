fc9630356b96cb63bce1b6796114b5bf5b9d6050
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2008-05-16 20:08:56 +0000

    no need to convert to array, just iterate the list

--- trunk/mevenide2/netbeans/nb-project/src/test/java/org/codehaus/mevenide/netbeans/output/MEVENIDE637Test.java
+++ trunk/mevenide2/netbeans/nb-project/src/test/java/org/codehaus/mevenide/netbeans/output/MEVENIDE637Test.java
@@ -16,6 +16,7 @@
  */
 package org.codehaus.mevenide.netbeans.output;
 
+import java.util.List;
 import junit.framework.*;
 import org.codehaus.mevenide.netbeans.execute.AbstractOutputHandler;
 
@@ -41,31 +42,31 @@ public class MEVENIDE637Test extends TestCase {
     public void testSeparatorSplit() {
          String aString="Some text \n a and some more .. \n and more..";//linux and unix
          
-         String[] strs = AbstractOutputHandler.splitMultiLine(aString);
+         List<String> strs = AbstractOutputHandler.splitMultiLine(aString);
          
-         assertEquals(strs.length, 3);
-         assertEquals("Some text ", strs[0]);
-         assertEquals(" a and some more .. ", strs[1]);
-         assertEquals(" and more..", strs[2]);
+         assertEquals(strs.size(), 3);
+         assertEquals("Some text ", strs.get(0));
+         assertEquals(" a and some more .. ", strs.get(1));
+         assertEquals(" and more..", strs.get(2));
          
          aString="Some text \r a and some more .. \r and more..";//Mac
          
          strs = AbstractOutputHandler.splitMultiLine(aString);
          
-         assertEquals(strs.length, 3);
+         assertEquals(strs.size(), 3);
 
-         assertEquals("Some text ", strs[0]);
-         assertEquals(" a and some more .. ", strs[1]);
-         assertEquals(" and more..", strs[2]);
+         assertEquals("Some text ", strs.get(0));
+         assertEquals(" a and some more .. ", strs.get(1));
+         assertEquals(" and more..", strs.get(2));
          
          aString="Some text \r\n a and some more .. \r\n and more..";//Windows
          
          strs = AbstractOutputHandler.splitMultiLine(aString);
 
-         assertEquals(strs.length, 3);
-         assertEquals("Some text ", strs[0]);
-         assertEquals(" a and some more .. ", strs[1]);
-         assertEquals(" and more..", strs[2]);
+         assertEquals(strs.size(), 3);
+         assertEquals("Some text ", strs.get(0));
+         assertEquals(" a and some more .. ", strs.get(1));
+         assertEquals(" and more..", strs.get(2));
          
          //MEVENIDE-637
          aString="\r\n\nMojo: \n\n  org.apache.maven.plugins:maven-compiler-plugin:2.0.2:compile" +
@@ -75,9 +76,9 @@ public class MEVENIDE637Test extends TestCase {
                  "\n\nsymbol  : class MyObject\n\nlocation: class example.App";
          strs = AbstractOutputHandler.splitMultiLine(aString);
 
-         assertEquals(strs.length, 8);
+         assertEquals(strs.size(), 8);
          
-         assertEquals("Mojo: ", strs[0]);
+         assertEquals("Mojo: ", strs.get(0));
          
     }
  
