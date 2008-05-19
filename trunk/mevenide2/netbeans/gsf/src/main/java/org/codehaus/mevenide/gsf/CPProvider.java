05f9be56f0a233bcd1da13b64e27a01276e9060f
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2008-05-19 06:29:41 +0000

    adding skeleton of GSF module. shall be used for adding extra langauges support to maven projects.

---
+++ trunk/mevenide2/netbeans/gsf/src/main/java/org/codehaus/mevenide/gsf/CPProvider.java
@@ -0,0 +1,34 @@
+/*
+ *  Copyright 2008 mkleint.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package org.codehaus.mevenide.gsf;
+
+import org.netbeans.modules.gsfpath.api.classpath.ClassPath;
+import org.netbeans.modules.gsfpath.spi.classpath.ClassPathProvider;
+import org.openide.filesystems.FileObject;
+
+/**
+ *
+ * @author mkleint
+ */
+public class CPProvider implements ClassPathProvider {
+
+    public ClassPath findClassPath(FileObject file, String type) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
