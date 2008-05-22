1f267bae8a09742bb89bd6410be395a7cef55ca8
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2008-05-22 09:59:54 +0000

    update implementation

--- mevenide2/netbeans/repoqueries-bridge/src/main/java/org/codehaus/mevenide/repoqueries/bridge/Impl.java
+++ mevenide2/netbeans/repoqueries-bridge/src/main/java/org/codehaus/mevenide/repoqueries/bridge/Impl.java
@@ -14,14 +14,35 @@
  *  limitations under the License.
  *  under the License.
  */
-
 package org.codehaus.mevenide.repoqueries.bridge;
 
 import java.io.File;
 import java.util.List;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.UnknownRepositoryLayoutException;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.manager.CredentialsDataSourceException;
+import org.apache.maven.artifact.manager.WagonConfigurationException;
+import org.apache.maven.artifact.manager.WagonManager;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
+import org.apache.maven.embedder.MavenEmbedder;
+import org.apache.maven.wagon.ConnectionException;
+import org.apache.maven.wagon.ResourceDoesNotExistException;
+import org.apache.maven.wagon.TransferFailedException;
+import org.apache.maven.wagon.UnsupportedProtocolException;
+import org.apache.maven.wagon.Wagon;
+import org.apache.maven.wagon.authentication.AuthenticationException;
+import org.apache.maven.wagon.authorization.AuthorizationException;
+import org.apache.maven.wagon.repository.Repository;
 import org.codehaus.mevenide.indexer.api.NBVersionInfo;
+import org.codehaus.mevenide.indexer.api.RepositoryInfo;
+import org.codehaus.mevenide.indexer.api.RepositoryPreferences;
 import org.codehaus.mevenide.indexer.api.RepositoryQueries;
+import org.codehaus.mevenide.netbeans.embedder.EmbedderFactory;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
 import org.netbeans.modules.repoqueries.spi.RepositoryQueryImplementation;
+import org.openide.util.Exceptions;
 
 /**
  *
@@ -36,12 +57,15 @@ public class Impl implements RepositoryQueryImplementation {
         }
         return null;
     }
-    
+
     private class Res implements RepositoryQueryImplementation.Result {
+
         private NBVersionInfo info;
+
         private Res(NBVersionInfo info) {
             this.info = info;
         }
+
         public boolean hasJavadoc() {
             return info.isJavadocExists();
         }
@@ -50,23 +74,87 @@ public class Impl implements RepositoryQueryImplementation {
             return info.isSourcesExists();
         }
 
-        public void downloadArtifact(File newLocation) {
-            throw new UnsupportedOperationException("Not supported yet.");
+        public boolean downloadArtifact(File newLocation) {
+            try {
+                MavenEmbedder emb = EmbedderFactory.createOnlineEmbedder();
+                ArtifactFactory af = (ArtifactFactory) emb.getPlexusContainer().lookup(ArtifactFactory.ROLE);
+                Artifact art = af.createArtifactWithClassifier(info.getGroupId(), info.getArtifactId(), info.getVersion(), info.getType(), null);
+                return downloadArtifact(art, emb, newLocation);
+            } catch (ComponentLookupException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+            return false;
         }
 
-        public void downloadJavadoc(File newLocation) {
-            throw new UnsupportedOperationException("Not supported yet.");
+        public boolean downloadJavadoc(File newLocation) {
+            try {
+                MavenEmbedder emb = EmbedderFactory.createOnlineEmbedder();
+                ArtifactFactory af = (ArtifactFactory) emb.getPlexusContainer().lookup(ArtifactFactory.ROLE);
+                Artifact art = af.createArtifactWithClassifier(info.getGroupId(), info.getArtifactId(), info.getVersion(), info.getType(), "javadoc");
+                return downloadArtifact(art, emb, newLocation);
+            } catch (ComponentLookupException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+            return false;
+        }
+        public boolean downloadSources(File newLocation) {
+            try {
+                MavenEmbedder emb = EmbedderFactory.createOnlineEmbedder();
+                ArtifactFactory af = (ArtifactFactory) emb.getPlexusContainer().lookup(ArtifactFactory.ROLE);
+                Artifact art = af.createArtifactWithClassifier(info.getGroupId(), info.getArtifactId(), info.getVersion(), info.getType(), "sources");
+                return downloadArtifact(art, emb, newLocation);
+            } catch (ComponentLookupException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+            return false;
         }
 
-        public void downloadSources(File newLocation) {
-            throw new UnsupportedOperationException("Not supported yet.");
+        public boolean downloadArtifact(Artifact art, MavenEmbedder emb, File newLocation) throws ComponentLookupException {
+            try {
+                WagonManager wagon = (WagonManager) emb.getPlexusContainer().lookup(WagonManager.ROLE);
+                System.out.println("art=" + art.getId());
+                RepositoryInfo repoinfo = RepositoryPreferences.getInstance().getRepositoryInfoById(info.getRepoId());
+                ArtifactRepositoryFactory arf = (ArtifactRepositoryFactory) emb.getPlexusContainer().lookup(ArtifactRepositoryFactory.ROLE);
+                ArtifactRepository artRepo = arf.createArtifactRepository(repoinfo.getId(), repoinfo.getRepositoryUrl(), arf.getLayout(arf.DEFAULT_LAYOUT_ID), null, null);
+                System.out.println("path=" + artRepo.pathOf(art));
+                Repository repository = new Repository(repoinfo.getId(), repoinfo.getRepositoryUrl());
+                Wagon wag = wagon.getWagon(repository);
+                if (wag != null) {
+                    try {
+                        wag.connect(repository, wagon.getAuthenticationInfo(repoinfo.getId()));
+                        wag.get(artRepo.pathOf(art), newLocation);
+                    } catch (ConnectionException ex) {
+                        Exceptions.printStackTrace(ex);
+                    } catch (AuthenticationException ex) {
+                        Exceptions.printStackTrace(ex);
+                    } catch (CredentialsDataSourceException ex) {
+                        Exceptions.printStackTrace(ex);
+                    } catch (TransferFailedException ex) {
+                        Exceptions.printStackTrace(ex);
+                    } catch (ResourceDoesNotExistException ex) {
+                        Exceptions.printStackTrace(ex);
+                    } catch (AuthorizationException ex) {
+                        Exceptions.printStackTrace(ex);
+                    } finally {
+                        try {
+                            wag.disconnect();
+                        } catch (ConnectionException ex) {
+                            Exceptions.printStackTrace(ex);
+                        }
+                    }
+                }
+            } catch (UnsupportedProtocolException ex) {
+                Exceptions.printStackTrace(ex);
+            } catch (WagonConfigurationException ex) {
+                Exceptions.printStackTrace(ex);
+            } catch (UnknownRepositoryLayoutException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+            return false;
         }
 
         public String getMavenId() {
             return info.getGroupId() + ":" + info.getArtifactId() + ":" + info.getVersion() + ":" + info.getClassifier() + ":" + info.getType();
         }
-        
     }
-            
-
 }
