c85660dca4489a6afe956634f0e7f83443f3e28a
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2008-07-14 12:48:54 +0000

    have the UI component for command line execution check the new properties location as well. fix some issues with file saving optimization of the axusliaryproperties file

--- branches/NETBEANS_ORG2/main/maven/src/org/netbeans/modules/maven/MavenProjectPropsImpl.java
+++ branches/NETBEANS_ORG2/main/maven/src/org/netbeans/modules/maven/MavenProjectPropsImpl.java
@@ -19,6 +19,8 @@ package org.netbeans.modules.maven;
 
 import java.util.HashMap;
 import java.util.Properties;
+import java.util.TreeMap;
+import java.util.logging.Logger;
 import org.netbeans.api.project.Project;
 import org.netbeans.modules.maven.api.NbMavenProject;
 import org.netbeans.spi.project.AuxiliaryConfiguration;
@@ -39,20 +41,44 @@ public class MavenProjectPropsImpl implements AuxiliaryProperties {
     static String ROOT = "properties"; //NOI18N
 
     private Project prj;
+    private boolean transaction = false;
+    private TreeMap<String, String> transPropsShared;
+    private TreeMap<String, String> transPropsPrivate;
+    private AuxiliaryConfiguration aux;
+    private boolean sharedChanged;
 
     public MavenProjectPropsImpl(Project project) {
         prj = project;
     }
 
-
-    public String get(String key, boolean shared) {
-        HashMap<String, String> props = readProperties(prj.getLookup().lookup(AuxiliaryConfiguration.class), shared);
-        //TODO optimize
-        String ret =  props.get(key);
-        if (ret != null) {
-            return ret;
+    private AuxiliaryConfiguration getAuxConf() {
+        if (aux == null) {
+            aux = prj.getLookup().lookup(AuxiliaryConfiguration.class);
         }
-        if (shared) {
+        return aux;
+    }
+
+    public synchronized String get(String key, boolean shared) {
+        return get(key, shared, true);
+    }
+
+    public synchronized String get(String key, boolean shared, boolean usePom) {
+        if (transaction) {
+            if (shared && transPropsShared.containsKey(key)) {
+                return transPropsShared.get(key);
+            }
+            if (!shared && transPropsPrivate.containsKey(key)) {
+                return transPropsPrivate.get(key);
+            }
+        } else {
+            TreeMap<String, String> props = readProperties(getAuxConf(), shared);
+            //TODO optimize
+            String ret =  props.get(key);
+            if (ret != null) {
+                return ret;
+            }
+        }
+        if (shared && usePom) {
             String val = prj.getLookup().lookup(NbMavenProject.class).getMavenProject().getProperties().getProperty(key);
             if (val != null) {
                 return val;
@@ -61,15 +87,24 @@ public class MavenProjectPropsImpl implements AuxiliaryProperties {
         return null;
     }
 
-    public void put(String key, String value, boolean shared) {
+    public synchronized void put(String key, String value, boolean shared) {
         if (shared) {
             //TODO put props to project.. shall we actually do it here?
         }
-        writeAuxiliaryData(prj.getLookup().lookup(AuxiliaryConfiguration.class), key, value, shared);
+        if (transaction) {
+            if (shared) {
+                transPropsShared.put(key, value);
+                sharedChanged = true;
+            } else {
+                transPropsPrivate.put(key, value);
+            }
+        } else {
+            writeAuxiliaryData(getAuxConf(), key, value, shared);
+        }
     }
 
-    public Iterable<String> listKeys(boolean shared) {
-        HashMap<String, String> props = readProperties(prj.getLookup().lookup(AuxiliaryConfiguration.class), shared);
+    public synchronized Iterable<String> listKeys(boolean shared) {
+        TreeMap<String, String> props = readProperties(getAuxConf(), shared);
         if (shared) {
             Properties mvnprops =  prj.getLookup().lookup(NbMavenProject.class).getMavenProject().getProperties();
             for (Object prop: mvnprops.keySet()) {
@@ -79,16 +114,8 @@ public class MavenProjectPropsImpl implements AuxiliaryProperties {
         return props.keySet();
     }
 
-    public static void writeAuxiliaryData(AuxiliaryConfiguration conf, String property, String value, boolean shared) {
-        Element el = conf.getConfigurationFragment(ROOT, NAMESPACE, shared);
-        if (el == null) {
-            el = XMLUtil.createDocument(ROOT, NAMESPACE, null, null).getDocumentElement();
-            Comment comment = el.getOwnerDocument().createComment("\nProperties that influence various parts of the IDE, especially code formatting and the like. \n" + //NOI18N
-                    "You can copy and paste the single properties, into the pom.xml file and the IDE will pick them up.\n" + //NOI18N
-                    "That way multiple projects can share the same settings (useful for formatting rules for example).\n" + //NOI18N
-                    "Any value defined here will override the pom.xml file value but is only applicable to the current project.\n"); //NOI18N
-            el.appendChild(comment);
-        }
+    private void writeAuxiliaryData(AuxiliaryConfiguration conf, String property, String value, boolean shared) {
+        Element el = getOrCreateRootElement(conf, shared);
         Element enEl;
         NodeList list = el.getElementsByTagNameNS(NAMESPACE, property);
         if (list.getLength() > 0) {
@@ -105,8 +132,43 @@ public class MavenProjectPropsImpl implements AuxiliaryProperties {
         conf.putConfigurationFragment(el, shared);
     }
 
-    private HashMap<String, String> readProperties(AuxiliaryConfiguration aux, boolean shared) {
-        HashMap<String, String> props = new HashMap<String, String>();
+    private void writeAuxiliaryData(AuxiliaryConfiguration conf, TreeMap<String, String> props, boolean shared) {
+        Element el = getOrCreateRootElement(conf, shared);
+        Element enEl;
+        for (String key : props.keySet()) {
+            NodeList list = el.getElementsByTagNameNS(NAMESPACE, key);
+            if (list.getLength() > 0) {
+                enEl = (Element)list.item(0);
+            } else {
+                enEl = el.getOwnerDocument().createElementNS(NAMESPACE, key);
+                el.appendChild(enEl);
+            }
+            String value = props.get(key);
+            if (value != null) {
+                enEl.setTextContent(value);
+            } else {
+                el.removeChild(enEl);
+            }
+        }
+        conf.putConfigurationFragment(el, shared);
+    }
+
+    private Element getOrCreateRootElement(AuxiliaryConfiguration conf, boolean shared) {
+        Element el = conf.getConfigurationFragment(ROOT, NAMESPACE, shared);
+        if (el == null) {
+            el = XMLUtil.createDocument(ROOT, NAMESPACE, null, null).getDocumentElement();
+            Comment comment = el.getOwnerDocument().createComment("\nProperties that influence various parts of the IDE, especially code formatting and the like. \n" + //NOI18N
+                    "You can copy and paste the single properties, into the pom.xml file and the IDE will pick them up.\n" + //NOI18N
+                    "That way multiple projects can share the same settings (useful for formatting rules for example).\n" + //NOI18N
+                    "Any value defined here will override the pom.xml file value but is only applicable to the current project.\n"); //NOI18N
+            el.appendChild(comment);
+        }
+        return el;
+    }
+
+
+    private TreeMap<String, String> readProperties(AuxiliaryConfiguration aux, boolean shared) {
+        TreeMap<String, String> props = new TreeMap<String, String>();
         Element el = aux.getConfigurationFragment(ROOT, NAMESPACE, shared);
         if (el != null) {
             NodeList list = el.getChildNodes();
@@ -115,11 +177,44 @@ public class MavenProjectPropsImpl implements AuxiliaryProperties {
                     Node nd = list.item(i);
                     if (nd instanceof Element) {
                         Element enEl = (Element)nd;
-                        props.put(enEl.getNodeName(), enEl.getNodeValue());
+                        props.put(enEl.getNodeName(), enEl.getTextContent());
                     }
                 }
             }
         }
         return props;
     }
+
+    public synchronized TreeMap<String, String> getRawProperties(boolean shared) {
+        return readProperties(getAuxConf(), shared);
+    }
+
+    public synchronized void startTransaction() {
+        transaction = true;
+        transPropsShared = getRawProperties(true);
+        transPropsPrivate = getRawProperties(false);
+        sharedChanged = false;
+
+    }
+
+    public synchronized void commitTransaction() {
+        transaction = false;
+        if (transPropsShared == null) {
+            Logger.getLogger(MavenProjectPropsImpl.class.getName()).info("Commiting a transaction that was cancelled.");
+            return;
+        }
+        if (sharedChanged) {
+            writeAuxiliaryData(getAuxConf(), transPropsShared, true);
+        }
+        writeAuxiliaryData(getAuxConf(), transPropsPrivate, false);
+        transPropsPrivate = null;
+        transPropsShared = null;
+    }
+
+    public synchronized void cancelTransaction() {
+        transaction = false;
+        transPropsPrivate = null;
+        transPropsShared = null;
+    }
+
 }
