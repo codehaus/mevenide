07fca6eacbc48525caa00006f59b3774db55e463
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2008-08-01 10:40:12 +0000

    upon installing, attempt to find old mevenide modules and disable them.

--- main/maven/src/org/netbeans/modules/maven/ModInstall.java
+++ main/maven/src/org/netbeans/modules/maven/ModInstall.java
@@ -18,35 +18,69 @@ package org.netbeans.modules.maven;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+import javax.swing.JOptionPane;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.netbeans.modules.maven.indexer.api.RepositoryIndexer;
 import org.netbeans.modules.maven.indexer.api.RepositoryInfo;
 import org.netbeans.modules.maven.indexer.api.RepositoryPreferences;
 import org.netbeans.api.project.Project;
 import org.netbeans.api.project.ui.OpenProjects;
+import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
+import org.openide.ErrorManager;
+import org.openide.NotifyDescriptor;
+import org.openide.filesystems.FileLock;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileSystem;
+import org.openide.filesystems.Repository;
+import org.openide.modules.ModuleInfo;
 import org.openide.modules.ModuleInstall;
+import org.openide.util.Exceptions;
+import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
+import org.openide.util.NbPreferences;
 import org.openide.util.RequestProcessor;
+import org.openide.xml.XMLUtil;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 /**
  * Module install that checks if the local repo index shall be refreshed.
  * @author mkleint
  */
-public class ModInstall extends ModuleInstall {
+public class ModInstall extends ModuleInstall implements ErrorHandler, EntityResolver {
 
     private static int MILIS_IN_SEC = 1000;
     private static int MILIS_IN_MIN = MILIS_IN_SEC * 60;
     private transient PropertyChangeListener projectsListener;
 
-     /*logger*/
-    private static final Logger LOGGER = 
+    /*logger*/
+    private static final Logger LOGGER =
             Logger.getLogger("org.netbeans.modules.maven.ModuleInstall");//NOI18N
-    
+
     /** Creates a new instance of ModInstall */
     public ModInstall() {
     }
@@ -54,12 +88,21 @@ public class ModInstall extends ModuleInstall {
     @Override
     public void restored() {
         super.restored();
+        try {
+            Preferences prfs = NbPreferences.root().node("/org/netbeans/modules/maven/touchme");
+            prfs.put("touch", "me");
+            prfs.flush();
+        } catch (BackingStoreException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        disableOldModules();
 
         projectsListener = new OpenProjectsListener();
         OpenProjects.getDefault().addPropertyChangeListener(projectsListener);
         final int freq = RepositoryPreferences.getInstance().getIndexUpdateFrequency();
         //#138102
         RequestProcessor.getDefault().post(new Runnable() {
+
             public void run() {
                 List<RepositoryInfo> ris = RepositoryPreferences.getInstance().getRepositoryInfos();
                 for (final RepositoryInfo ri : ris) {
@@ -70,16 +113,16 @@ public class ModInstall extends ModuleInstall {
                     if (freq != RepositoryPreferences.FREQ_NEVER) {
                         boolean run = false;
                         if (freq == RepositoryPreferences.FREQ_STARTUP) {
-                            LOGGER.finer("Index At Startup :"+ri.getId());//NOI18N
+                            LOGGER.finer("Index At Startup :" + ri.getId());//NOI18N
                             run = true;
-                        } else if (freq == RepositoryPreferences.FREQ_ONCE_DAY && checkDiff(ri.getId(),86400000L)) {
-                            LOGGER.finer("Index Once a Day :"+ri.getId());//NOI18N
+                        } else if (freq == RepositoryPreferences.FREQ_ONCE_DAY && checkDiff(ri.getId(), 86400000L)) {
+                            LOGGER.finer("Index Once a Day :" + ri.getId());//NOI18N
                             run = true;
-                        } else if (freq == RepositoryPreferences.FREQ_ONCE_WEEK && checkDiff(ri.getId(),604800000L)) {
-                            LOGGER.finer("Index once a Week :"+ri.getId());//NOI18N
+                        } else if (freq == RepositoryPreferences.FREQ_ONCE_WEEK && checkDiff(ri.getId(), 604800000L)) {
+                            LOGGER.finer("Index once a Week :" + ri.getId());//NOI18N
                             run = true;
                         }
-                        if (run  && ri.getIndexUpdateUrl() != null) {
+                        if (run && ri.getIndexUpdateUrl() != null) {
                             RepositoryIndexer.indexRepo(ri);
                         }
                     }
@@ -88,14 +131,14 @@ public class ModInstall extends ModuleInstall {
         }, MILIS_IN_MIN * 2);
     }
 
-    private boolean checkDiff(String repoid,long amount) {
+    private boolean checkDiff(String repoid, long amount) {
         Date date = RepositoryPreferences.getInstance().getLastIndexUpdate(repoid);
         Date now = new Date();
-        LOGGER.finer("Check Date Diff :"+repoid);//NOI18N
-        LOGGER.finer("Last Indexed Date :"+SimpleDateFormat.getInstance().format(date));//NOI18N
-        LOGGER.finer("Now :"+SimpleDateFormat.getInstance().format(now));//NOI18N
+        LOGGER.finer("Check Date Diff :" + repoid);//NOI18N
+        LOGGER.finer("Last Indexed Date :" + SimpleDateFormat.getInstance().format(date));//NOI18N
+        LOGGER.finer("Now :" + SimpleDateFormat.getInstance().format(now));//NOI18N
         long diff = now.getTime() - date.getTime();
-        LOGGER.finer("Diff :"+diff);//NOI18N
+        LOGGER.finer("Diff :" + diff);//NOI18N
         return (diff < 0 || diff > amount);
     }
 
@@ -122,7 +165,7 @@ public class ModInstall extends ModuleInstall {
                             if (RepositoryPreferences.getInstance().
                                     getRepositoryInfoById(rep.getId()) == null) {
                                 RepositoryInfo ri = new RepositoryInfo(rep.getId(),
-                                        RepositoryPreferences.TYPE_NEXUS, 
+                                        RepositoryPreferences.TYPE_NEXUS,
                                         rep.getId() + " " + NbBundle.getMessage(ModInstall.class, "LBL_REPOSITORY"),//NOI18N
                                         null, rep.getUrl(), null);
                                 RepositoryPreferences.getInstance().addOrModifyRepositoryInfo(ri);
@@ -133,5 +176,110 @@ public class ModInstall extends ModuleInstall {
             }
 
         }
+    }
+
+    private void disableOldModules() {
+        Runnable runnable = new Runnable() {
+
+            public void run() {
+                final List<String> moduleNames = new ArrayList<String>();
+                Collection<? extends ModuleInfo> all = Lookup.getDefault().lookupAll(ModuleInfo.class);
+                for (ModuleInfo info : all) {
+                    if (info.getCodeNameBase().startsWith("org.codehaus.mevenide")) { //NOI18N
+                        moduleNames.add(info.getCodeNameBase());
+                    }
+                }
+                try {
+                    Repository.getDefault().getDefaultFileSystem().runAtomicAction(new FileSystem.AtomicAction() {
+
+                        public void run() throws IOException {
+                            boolean notified = false;
+                            outter:
+                            for (String newModule : moduleNames) {
+                                FileLock lock = null;
+                                OutputStream os = null;
+                                try {
+                                    String newModuleXML = "Modules/" + newModule.replace('.', '-') + ".xml"; // NOI18N
+                                    FileObject fo = Repository.getDefault().getDefaultFileSystem().findResource(newModuleXML);
+                                    if (fo == null) {
+                                        continue;
+                                    }
+                                    Document document = readModuleDocument(fo);
+                                    NodeList list = document.getDocumentElement().getElementsByTagName("param"); // NOI18N
+                                    int n = list.getLength();
+                                    for (int j = 0; j < n; j++) {
+                                        Element node = (Element) list.item(j);
+                                        if ("enabled".equals(node.getAttribute("name"))) {
+                                            // NOI18N
+                                            Text text = (Text) node.getChildNodes().item(0);
+                                            String value = text.getNodeValue();
+                                            if ("true".equals(value)) {
+                                                // NOI18N
+                                                text.setNodeValue("false"); // NOI18N
+                                                break;
+                                            } else {
+                                                continue outter;
+                                            }
+                                        }
+                                    }
+                                    if (!notified) {
+                                        NotifyDescriptor nd = new NotifyDescriptor.Message(NbBundle.getMessage(ModInstall.class, "MSG_Install_Warning"));
+                                        nd.setTitle(NbBundle.getMessage(ModInstall.class, "MSG_Install_Warning_Title"));
+                                        DialogDisplayer.getDefault().notify(nd);
+                                        notified = true;
+                                    }
+                                    lock = fo.lock();
+                                    os = fo.getOutputStream(lock);
+                                    XMLUtil.write(document, os, "UTF-8"); // NOI18N
+                                } catch (Exception e) {
+                                    ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, e);
+                                } finally {
+                                    if (os != null) {
+                                        try {
+                                            os.close();
+                                        } catch (IOException ex) {
+                                        }
+                                    }
+                                    if (lock != null) {
+                                        lock.releaseLock();
+                                    }
+                                }
+                            }
+                        }
+                    });
+                } catch (IOException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+            }
+        };
+        RequestProcessor.getDefault().post(runnable);
+    }
+
+    private Document readModuleDocument(FileObject fo) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setValidating(false);
+        DocumentBuilder parser = dbf.newDocumentBuilder();
+        parser.setEntityResolver(this);
+        parser.setErrorHandler(this);
+        InputStream is = fo.getInputStream();
+        Document document = parser.parse(is);
+        is.close();
+        return document;
+    }
+
+    public InputSource resolveEntity(String publicId, String systemId) {
+        return new InputSource(new ByteArrayInputStream(new byte[0]));
+    }
+
+    public void error(SAXParseException exception) {
+        ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, exception);
+    }
+
+    public void fatalError(SAXParseException exception) {
+        ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, exception);
+    }
+
+    public void warning(SAXParseException exception) {
+        ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, exception);
     }
 }
