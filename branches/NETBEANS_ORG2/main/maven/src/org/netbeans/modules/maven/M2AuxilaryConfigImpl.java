8d21909ac8d03f68d28fdae57182352a6b0a4007
mkleint <mkleint@c6c22482-0310-0410-b13f-f1d71ac210c7>
2008-08-15 06:29:44 +0000

    prevent Project mutex related deadlock. the auxconfiguration in netbeans shall not require a mutex, it's the only one reading and writing the file.

--- branches/NETBEANS_ORG2/main/maven/src/org/netbeans/modules/maven/M2AuxilaryConfigImpl.java
+++ branches/NETBEANS_ORG2/main/maven/src/org/netbeans/modules/maven/M2AuxilaryConfigImpl.java
@@ -37,7 +37,7 @@ import org.xml.sax.SAXException;
 
 /**
  * implementation of AuxiliaryConfiguration that relies on FileObject's attributes
- * for the non shared elements and on ${basedir}/.nb-configuration file for share ones.
+ * for the non shared elements and on ${basedir}/nb-configuration file for share ones.
  * @author mkleint
  */
 public class M2AuxilaryConfigImpl implements AuxiliaryConfiguration {
@@ -51,216 +51,196 @@ public class M2AuxilaryConfigImpl implements AuxiliaryConfiguration {
         this.project = proj;
     }
 
-    public Element getConfigurationFragment(final String elementName, final String namespace, boolean shared) {
+    public synchronized Element getConfigurationFragment(final String elementName, final String namespace, boolean shared) {
         if (shared) {
             final FileObject config = project.getProjectDirectory().getFileObject(CONFIG_FILE_NAME);
             if (config != null) {
-                return (Element) ProjectManager.mutex().readAccess(new Mutex.Action() {
-
-                    public Object run() {
-                        Document doc;
-                        InputStream in = null;
+                Document doc;
+                InputStream in = null;
+                try {
+                    in = config.getInputStream();
+                    doc = XMLUtil.parse(new InputSource(in), false, true, null, null);
+                    return findElement(doc.getDocumentElement(), elementName, namespace);
+                } catch (SAXException ex) {
+                    Exceptions.printStackTrace(ex);
+                } catch (IOException ex) {
+                    Exceptions.printStackTrace(ex);
+                } finally {
+                    if (in != null) {
                         try {
-                            in = config.getInputStream();
-                            doc = XMLUtil.parse(new InputSource(in), false, true, null, null);
-                            return findElement(doc.getDocumentElement(), elementName, namespace);
-                        } catch (SAXException ex) {
-                            Exceptions.printStackTrace(ex);
+                            in.close();
                         } catch (IOException ex) {
                             Exceptions.printStackTrace(ex);
-                        } finally {
-                            if (in != null) {
-                                try {
-                                    in.close();
-                                } catch (IOException ex) {
-                                    Exceptions.printStackTrace(ex);
-                                }
-                            }
                         }
-                        return null;
                     }
-                });
+                }
+                return null;
+            }
+            return null;
+        } else {
+            String str = (String) project.getProjectDirectory().getAttribute(AUX_CONFIG);
+            if (str != null) {
+                Document doc;
+                try {
+                    doc = XMLUtil.parse(new InputSource(new StringReader(str)), false, true, null, null);
+                    return findElement(doc.getDocumentElement(), elementName, namespace);
+                } catch (SAXException ex) {
+                    ex.printStackTrace();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
             return null;
         }
-        return (Element) ProjectManager.mutex().readAccess(new Mutex.Action() {
-
-            public Object run() {
-                String str = (String) project.getProjectDirectory().getAttribute(AUX_CONFIG);
-                if (str != null) {
-                    Document doc;
-                    try {
-                        doc = XMLUtil.parse(new InputSource(new StringReader(str)), false, true, null, null);
-                        return findElement(doc.getDocumentElement(), elementName, namespace);
-                    } catch (SAXException ex) {
-                        ex.printStackTrace();
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
-                }
-                return null;
-            }
-        });
     }
 
-    public void putConfigurationFragment(final Element fragment, final boolean shared) throws IllegalArgumentException {
-        ProjectManager.mutex().writeAccess(new Mutex.Action() {
-
-            public Object run() {
-                Document doc = null;
-                FileObject config = project.getProjectDirectory().getFileObject(CONFIG_FILE_NAME);
-                if (shared) {
-                    if (config != null) {
-                        try {
-                            doc = XMLUtil.parse(new InputSource(config.getInputStream()), false, true, null, null);
-                        } catch (SAXException ex) {
-                            ex.printStackTrace();
-                        } catch (IOException ex) {
-                            ex.printStackTrace();
-                        }
-                    } else {
-                        String element = "project-shared-configuration"; // NOI18N
-                        doc = XMLUtil.createDocument(element, null, null, null);
-                        doc.getDocumentElement().appendChild(doc.createComment(
-                                "\nThis file contains additional configuration written by modules in the NetBeans IDE.\n" + //NOI18N will be part of a file on disk, don't translate
-                                "The configuration is intended to be shared among all the users of project and\n" +//NOI18N
-                                "therefore it is assumed to be part of version control checkout.\n" +//NOI18N
-                                "Without this configuration present, some functionality in the IDE may be limited or fail altogether.\n"));//NOI18N
-                    }
-                } else {
-                    String str = (String) project.getProjectDirectory().getAttribute(AUX_CONFIG);
-                    if (str != null) {
-                        try {
-                            doc = XMLUtil.parse(new InputSource(new StringReader(str)), false, true, null, null);
-                        } catch (SAXException ex) {
-                            ex.printStackTrace();
-                        } catch (IOException ex) {
-                            ex.printStackTrace();
-                        }
-                    } 
-                    if (doc == null) {
-                        String element = "project-private"; // NOI18N
-                        doc = XMLUtil.createDocument(element, null, null, null);
-                    }
+    public synchronized void putConfigurationFragment(final Element fragment, final boolean shared) throws IllegalArgumentException {
+        Document doc = null;
+        FileObject config = project.getProjectDirectory().getFileObject(CONFIG_FILE_NAME);
+        if (shared) {
+            if (config != null) {
+                try {
+                    doc = XMLUtil.parse(new InputSource(config.getInputStream()), false, true, null, null);
+                } catch (SAXException ex) {
+                    ex.printStackTrace();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
                 }
-                if (doc != null) {
-                    Element el = findElement(doc.getDocumentElement(), fragment.getNodeName(), fragment.getNamespaceURI());
-                    if (el != null) {
-                        doc.getDocumentElement().removeChild(el);
-                    }
-                    doc.getDocumentElement().appendChild(doc.importNode(fragment, true));
+            } else {
+                String element = "project-shared-configuration"; // NOI18N
+                doc = XMLUtil.createDocument(element, null, null, null);
+                doc.getDocumentElement().appendChild(doc.createComment(
+                        "\nThis file contains additional configuration written by modules in the NetBeans IDE.\n" + //NOI18N will be part of a file on disk, don't translate
+                        "The configuration is intended to be shared among all the users of project and\n" +//NOI18N
+                        "therefore it is assumed to be part of version control checkout.\n" +//NOI18N
+                        "Without this configuration present, some functionality in the IDE may be limited or fail altogether.\n"));//NOI18N
+            }
+        } else {
+            String str = (String) project.getProjectDirectory().getAttribute(AUX_CONFIG);
+            if (str != null) {
+                try {
+                    doc = XMLUtil.parse(new InputSource(new StringReader(str)), false, true, null, null);
+                } catch (SAXException ex) {
+                    ex.printStackTrace();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
                 }
-                if (shared) {
-                    FileLock lck = null;
-                    OutputStream out = null;
+            }
+            if (doc == null) {
+                String element = "project-private"; // NOI18N
+                doc = XMLUtil.createDocument(element, null, null, null);
+            }
+        }
+        if (doc != null) {
+            Element el = findElement(doc.getDocumentElement(), fragment.getNodeName(), fragment.getNamespaceURI());
+            if (el != null) {
+                doc.getDocumentElement().removeChild(el);
+            }
+            doc.getDocumentElement().appendChild(doc.importNode(fragment, true));
+        }
+        if (shared) {
+            FileLock lck = null;
+            OutputStream out = null;
+            try {
+                if (config == null) {
+                    config = project.getProjectDirectory().createData(CONFIG_FILE_NAME);
+                }
+                lck = config.lock();
+                out = config.getOutputStream(lck);
+                XMLUtil.write(doc, out, "UTF-8"); //NOI18N
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            } finally {
+                if (out != null) {
                     try {
-                        if (config == null) {
-                            config = project.getProjectDirectory().createData(CONFIG_FILE_NAME);
-                        }
-                        lck = config.lock();
-                        out = config.getOutputStream(lck);
-                        XMLUtil.write(doc, out, "UTF-8"); //NOI18N
+                        out.close();
                     } catch (IOException ex) {
                         Exceptions.printStackTrace(ex);
-                    } finally {
-                        if (out != null) {
-                            try {
-                                out.close();
-                            } catch (IOException ex) {
-                                Exceptions.printStackTrace(ex);
-                            }
-                        }
-                        if (lck != null) {
-                            lck.releaseLock();
-                        }
-                    }
-                } else {
-                    try {
-                        StringOutputStream wr = new StringOutputStream();
-                        XMLUtil.write(doc, wr, "UTF-8"); //NOI18N
-                        project.getProjectDirectory().setAttribute(AUX_CONFIG, wr.toString());
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
                     }
                 }
-                return null;
+                if (lck != null) {
+                    lck.releaseLock();
+                }
             }
-        });
+        } else {
+            try {
+                StringOutputStream wr = new StringOutputStream();
+                XMLUtil.write(doc, wr, "UTF-8"); //NOI18N
+                project.getProjectDirectory().setAttribute(AUX_CONFIG, wr.toString());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
 
     }
 
-    public boolean removeConfigurationFragment(final String elementName, final String namespace, final boolean shared) throws IllegalArgumentException {
-        return ((Boolean) ProjectManager.mutex().writeAccess(new Mutex.Action() {
-
-            public Object run() {
-                Document doc = null;
-                FileObject config = project.getProjectDirectory().getFileObject(CONFIG_FILE_NAME);
-                if (shared) {
-                    if (config != null) {
-                        try {
-                            doc = XMLUtil.parse(new InputSource(config.getInputStream()), false, true, null, null);
-                        } catch (SAXException ex) {
-                            Exceptions.printStackTrace(ex);
-                        } catch (IOException ex) {
-                            Exceptions.printStackTrace(ex);
-                        }
-                    } else {
-                        return Boolean.FALSE;
-                    }
-                    
-                } else {
-                    String str = (String) project.getProjectDirectory().getAttribute(AUX_CONFIG);
-                    if (str != null) {
-                        try {
-                            doc = XMLUtil.parse(new InputSource(new StringReader(str)), false, true, null, null);
-                        } catch (SAXException ex) {
-                            Exceptions.printStackTrace(ex);
-                        } catch (IOException ex) {
-                            Exceptions.printStackTrace(ex);
-                        }
-                    } else {
-                        return Boolean.FALSE;
-                    }
+    public synchronized boolean removeConfigurationFragment(final String elementName, final String namespace, final boolean shared) throws IllegalArgumentException {
+        Document doc = null;
+        FileObject config = project.getProjectDirectory().getFileObject(CONFIG_FILE_NAME);
+        if (shared) {
+            if (config != null) {
+                try {
+                    doc = XMLUtil.parse(new InputSource(config.getInputStream()), false, true, null, null);
+                } catch (SAXException ex) {
+                    Exceptions.printStackTrace(ex);
+                } catch (IOException ex) {
+                    Exceptions.printStackTrace(ex);
                 }
-                if (doc != null) {
-                    Element el = findElement(doc.getDocumentElement(), elementName, namespace);
-                    if (el != null) {
-                        doc.getDocumentElement().removeChild(el);
-                    }
-                }
-                if (shared) {
-                    FileLock lck = null;
-                    OutputStream out = null;
-                    try {
-                        lck = config.lock();
-                        out = config.getOutputStream(lck);
-                        XMLUtil.write(doc, out, "UTF-8"); //NOI18N
-                    } catch (IOException ex) {
-                        Exceptions.printStackTrace(ex);
-                    } finally {
-                        if (out != null) {
-                            try {
-                                out.close();
-                            } catch (IOException ex) {
-                                Exceptions.printStackTrace(ex);
-                            }
-                        }
-                        if (lck != null) {
-                            lck.releaseLock();
-                        }
-                    }
-                } else {
-                    try {
-                        StringOutputStream wr = new StringOutputStream();
-                        XMLUtil.write(doc, wr, "UTF-8"); //NOI18N
-                        project.getProjectDirectory().setAttribute(AUX_CONFIG, wr.toString());
-                    } catch (IOException ex) {
-                        Exceptions.printStackTrace(ex);
-                    }
-                }
-                return Boolean.TRUE;
+            } else {
+                return false;
             }
-        })).booleanValue();
+
+        } else {
+            String str = (String) project.getProjectDirectory().getAttribute(AUX_CONFIG);
+            if (str != null) {
+                try {
+                    doc = XMLUtil.parse(new InputSource(new StringReader(str)), false, true, null, null);
+                } catch (SAXException ex) {
+                    Exceptions.printStackTrace(ex);
+                } catch (IOException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+            } else {
+                return false;
+            }
+        }
+        if (doc != null) {
+            Element el = findElement(doc.getDocumentElement(), elementName, namespace);
+            if (el != null) {
+                doc.getDocumentElement().removeChild(el);
+            }
+        }
+        if (shared) {
+            FileLock lck = null;
+            OutputStream out = null;
+            try {
+                lck = config.lock();
+                out = config.getOutputStream(lck);
+                XMLUtil.write(doc, out, "UTF-8"); //NOI18N
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            } finally {
+                if (out != null) {
+                    try {
+                        out.close();
+                    } catch (IOException ex) {
+                        Exceptions.printStackTrace(ex);
+                    }
+                }
+                if (lck != null) {
+                    lck.releaseLock();
+                }
+            }
+        } else {
+            try {
+                StringOutputStream wr = new StringOutputStream();
+                XMLUtil.write(doc, wr, "UTF-8"); //NOI18N
+                project.getProjectDirectory().setAttribute(AUX_CONFIG, wr.toString());
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+        }
+        return true;
     }
 
     private static Element findElement(Element parent, String name, String namespace) {
@@ -270,8 +250,7 @@ public class M2AuxilaryConfigImpl implements AuxiliaryConfiguration {
         for (int i = 0; i < len; i++) {
             if (l.item(i).getNodeType() == Node.ELEMENT_NODE) {
                 Element el = (Element) l.item(i);
-                if (name.equals(el.getLocalName()) && ((namespace==el.getNamespaceURI()) /*check both namespaces are null*/
-                        ||(namespace!=null && namespace.equals(el.getNamespaceURI())))) {
+                if (name.equals(el.getLocalName()) && ((namespace == el.getNamespaceURI()) /*check both namespaces are null*/ || (namespace != null && namespace.equals(el.getNamespaceURI())))) {
                     if (result == null) {
                         result = el;
                     } else {
